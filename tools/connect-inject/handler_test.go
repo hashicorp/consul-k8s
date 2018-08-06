1d189bda36b6e02d6a99a820de95b233d2f5e708
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-06 16:57:29 -0700

    tools/connect-inject: continuing work on test foundations

--- tools/connect-inject/handler_test.go
+++ tools/connect-inject/handler_test.go
@@ -1,25 +1,50 @@
 package connectinject
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestHandlerHandle(t *testing.T) {
 	cases := []struct {
 		Name    string
 		Handler Handler
-		Req     v1beta1.AdmissionReview
+		Req     v1beta1.AdmissionRequest
 		Err     string // expected error string, not exact
-		Status  int    // expected status, defaults to 200
-	}{}
+	}{
+		{
+			"kube-system namespace",
+			Handler{},
+			v1beta1.AdmissionRequest{
+				Object: encodeRaw(t, &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: metav1.NamespaceSystem,
+					},
+				}),
+			},
+			"",
+		},
+	}
 
 	for _, tt := range cases {
 		t.Run(tt.Name, func(t *testing.T) {
+			require := require.New(t)
+			resp := tt.Handler.Mutate(&tt.Req)
+			if (tt.Err == "") != resp.Allowed {
+				t.Fatalf("allowed: %v, expected err: %v", resp.Allowed, tt.Err)
+			}
+			if tt.Err != "" {
+				require.Contains(resp.Result.Message, tt.Err)
+				return
+			}
 		})
 	}
 }
@@ -48,4 +73,11 @@ func TestHandlerHandle_noBody(t *testing.T) {
 	h.Handle(rec, req)
 	require.Equal(t, http.StatusBadRequest, rec.Code)
 	require.Contains(t, rec.Body.String(), "body")
+}
+
+// encodeRaw is a helper to encode some data into a RawExtension.
+func encodeRaw(t *testing.T, input interface{}) runtime.RawExtension {
+	data, err := json.Marshal(input)
+	require.NoError(t, err)
+	return runtime.RawExtension{Raw: data}
 }
