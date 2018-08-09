72df75a9cebcfe03419f2c5a519dc159bbddd56a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-08 18:58:35 -0700

    cmd: support TLS files for the command

--- cmd/main.go
+++ cmd/main.go
@@ -9,10 +9,11 @@ import (
 )
 
 func main() {
+	ui := &cli.BasicUi{Writer: os.Stdout, ErrorWriter: os.Stderr}
 	c := cli.NewCLI("consul-k8s", "0.1.0")
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
-		"inject": func() (cli.Command, error) { return &subcommand.Inject{}, nil },
+		"inject": func() (cli.Command, error) { return &subcommand.Inject{UI: ui}, nil },
 	}
 
 	exitStatus, err := c.Run()
--- cmd/subcommand/inject.go
+++ cmd/subcommand/inject.go
@@ -1,21 +1,44 @@
 package subcommand
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/hashicorp/consul-k8s/tools/connect-inject"
+	"github.com/hashicorp/consul/command/flags"
 	"github.com/mitchellh/cli"
 )
 
 type Inject struct {
 	UI cli.Ui
+
+	flagCertFile string // TLS cert for listening (PEM)
+	flagKeyFile  string // TLS cert private key (PEM)
+	flagSet      *flag.FlagSet
+
+	once sync.Once
+	help string
+}
+
+func (c *Inject) init() {
+	c.flagSet = flag.NewFlagSet("", flag.ContinueOnError)
+	c.flagSet.StringVar(&c.flagCertFile, "tls-cert-file", "", "PEM-encoded TLS certificate to serve")
+	c.flagSet.StringVar(&c.flagKeyFile, "tls-key-file", "", "PEM-encoded TLS private key to serve")
+	c.help = flags.Usage(help, c.flagSet)
 }
 
 func (c *Inject) Run(args []string) int {
-	flags := flag.NewFlagSet("", flag.ContinueOnError)
-	if err := flags.Parse(args); err != nil {
+	c.once.Do(c.init)
+	if err := c.flagSet.Parse(args); err != nil {
+		return 1
+	}
+
+	cert, err := tls.LoadX509KeyPair(c.flagCertFile, c.flagKeyFile)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error loading TLS keypair: %s", err))
 		return 1
 	}
 
@@ -23,11 +46,12 @@ func (c *Inject) Run(args []string) int {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/mutate", h.Handle)
 	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+		Addr:      ":8080",
+		Handler:   mux,
+		TLSConfig: &tls.Config{Certificates: []tls.Certificate{cert}},
 	}
 
-	if err := server.ListenAndServe(); err != nil {
+	if err := server.ListenAndServeTLS("", ""); err != nil {
 		c.UI.Error(fmt.Sprintf("Error listening: %s", err))
 		return 1
 	}
@@ -36,7 +60,10 @@ func (c *Inject) Run(args []string) int {
 }
 
 func (c *Inject) Synopsis() string { return synopsis }
-func (c *Inject) Help() string     { return help }
+func (c *Inject) Help() string {
+	c.once.Do(c.init)
+	return c.help
+}
 
 const synopsis = "Inject Connect proxy sidecar."
 const help = `
