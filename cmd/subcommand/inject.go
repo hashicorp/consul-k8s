c7db31d2efdbd1eb5f97356223cb2d376c3b5c82
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-10 09:41:41 -0700

    cmd: support all the new cert autogen

--- cmd/subcommand/inject.go
+++ cmd/subcommand/inject.go
@@ -1,36 +1,49 @@
 package subcommand
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/gorilla/handlers"
-	"github.com/hashicorp/consul-k8s/tools/connect-inject"
+	"github.com/hashicorp/consul-k8s/cmd/helper/cert"
+	"github.com/hashicorp/consul-k8s/cmd/helper/connect-inject"
 	"github.com/hashicorp/consul/command/flags"
 	"github.com/mitchellh/cli"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type Inject struct {
 	UI cli.Ui
 
 	flagListen   string
+	flagAutoName string // MutatingWebhookConfiguration for updating
 	flagCertFile string // TLS cert for listening (PEM)
 	flagKeyFile  string // TLS cert private key (PEM)
 	flagSet      *flag.FlagSet
 
 	once sync.Once
 	help string
+	cert atomic.Value
 }
 
 func (c *Inject) init() {
 	c.flagSet = flag.NewFlagSet("", flag.ContinueOnError)
-	c.flagSet.StringVar(&c.flagListen, "listen", ":8080", "Addrss to bind listener to.")
-	c.flagSet.StringVar(&c.flagCertFile, "tls-cert-file", "", "PEM-encoded TLS certificate to serve")
-	c.flagSet.StringVar(&c.flagKeyFile, "tls-key-file", "", "PEM-encoded TLS private key to serve")
+	c.flagSet.StringVar(&c.flagListen, "listen", ":8080", "Address to bind listener to.")
+	c.flagSet.StringVar(&c.flagAutoName, "tls-auto", "",
+		"MutatingWebhookConfiguration name. If specified, will auto generate cert bundle.")
+	c.flagSet.StringVar(&c.flagCertFile, "tls-cert-file", "",
+		"PEM-encoded TLS certificate to serve. If blank, will generate random cert.")
+	c.flagSet.StringVar(&c.flagKeyFile, "tls-key-file", "",
+		"PEM-encoded TLS private key to serve. If blank, will generate random cert.")
 	c.help = flags.Usage(help, c.flagSet)
 }
 
@@ -40,22 +53,47 @@ func (c *Inject) Run(args []string) int {
 		return 1
 	}
 
-	cert, err := tls.LoadX509KeyPair(c.flagCertFile, c.flagKeyFile)
+	// We must have an in-cluster K8S client
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		c.UI.Error(fmt.Sprintf("Error loading TLS keypair: %s", err))
+		c.UI.Error(fmt.Sprintf("Error loading in-cluster K8S config: %s", err))
+		return 1
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating K8S client: %s", err))
 		return 1
 	}
 
+	// Determine where to source the certificates from
+	var certSource cert.Source = &cert.GenSource{Name: "Connect Inject"}
+	if c.flagCertFile != "" {
+		certSource = &cert.DiskSource{
+			CertPath: c.flagCertFile,
+			KeyPath:  c.flagKeyFile,
+		}
+	}
+
+	// Create the certificate notifier so we can update for certificates,
+	// then start all the background routines for updating certificates.
+	certCh := make(chan cert.Bundle)
+	certNotify := &cert.Notify{Ch: certCh, Source: certSource}
+	defer certNotify.Stop()
+	go certNotify.Start(context.Background())
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
+	go c.certWatcher(ctx, certCh, clientset)
+
+	// Build the HTTP handler and server
 	var injector connectinject.Handler
 	mux := http.NewServeMux()
 	mux.HandleFunc("/mutate", injector.Handle)
-
 	var handler http.Handler = mux
 	handler = handlers.LoggingHandler(os.Stdout, handler)
 	server := &http.Server{
 		Addr:      c.flagListen,
 		Handler:   handler,
-		TLSConfig: &tls.Config{Certificates: []tls.Certificate{cert}},
+		TLSConfig: &tls.Config{GetCertificate: c.getCertificate},
 	}
 
 	c.UI.Info(fmt.Sprintf("Listening on %q...", c.flagListen))
@@ -65,6 +103,63 @@ func (c *Inject) Run(args []string) int {
 	}
 
 	return 0
+}
+
+func (c *Inject) getCertificate(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	certRaw := c.cert.Load()
+	if certRaw == nil {
+		return nil, fmt.Errorf("No certificate available.")
+	}
+
+	return certRaw.(*tls.Certificate), nil
+}
+
+func (c *Inject) certWatcher(ctx context.Context, ch <-chan cert.Bundle, clientset *kubernetes.Clientset) {
+	var bundle cert.Bundle
+	for {
+		select {
+		case bundle = <-ch:
+			c.UI.Output("Updated certificate bundle received. Updating certs...")
+			// Bundle is updated, set it up
+
+		case <-time.After(1 * time.Second):
+			// This forces the mutating webhook config to remain updated
+			// fairly quickly. This is a jank way to do this and we should
+			// look to improve it in the future. Since we use Patch requests
+			// it is pretty cheap to do, though.
+
+		case <-ctx.Done():
+			// Quit
+			return
+		}
+
+		cert, err := tls.X509KeyPair(bundle.Cert, bundle.Key)
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error loading TLS keypair: %s", err))
+			continue
+		}
+
+		// If there is a MWC name set, then update the CA bundle.
+		if c.flagAutoName != "" && len(bundle.CACert) > 0 {
+			_, err := clientset.Admissionregistration().
+				MutatingWebhookConfigurations().
+				Patch(c.flagAutoName, types.JSONPatchType, []byte(fmt.Sprintf(
+					`{
+						"op": "replace",
+						"path": "/webhooks/0/clientConfig/caBundle",
+						"value": %q,
+					}`, string(bundle.CACert))))
+			if err != nil {
+				c.UI.Error(fmt.Sprintf(
+					"Error updating MutatingWebhookConfiguration: %s",
+					err))
+				continue
+			}
+		}
+
+		// Update the certificate
+		c.cert.Store(&cert)
+	}
 }
 
 func (c *Inject) Synopsis() string { return synopsis }
