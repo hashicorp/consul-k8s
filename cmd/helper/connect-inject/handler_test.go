964e01f3c8d9a5aca7f2a5166fdab22e4179703c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-11 14:50:30 -0700

    cmd/helper/connect-inject: tests for containerSidecar

--- cmd/helper/connect-inject/handler_test.go
+++ cmd/helper/connect-inject/handler_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/mattbaird/jsonpatch"
@@ -289,6 +290,83 @@ func TestHandlerDefaultAnnotations(t *testing.T) {
 				actual = nil
 			}
 			require.Equal(actual, tt.Expected)
+		})
+	}
+}
+
+func TestHandlerContainerSidecar(t *testing.T) {
+	minimal := func() *corev1.Pod {
+		return &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					annotationService: "foo",
+				},
+			},
+
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					corev1.Container{
+						Name: "web",
+					},
+
+					corev1.Container{
+						Name: "web-side",
+					},
+				},
+			},
+		}
+	}
+
+	cases := []struct {
+		Name   string
+		Pod    func(*corev1.Pod) *corev1.Pod
+		Cmd    string // Strings.Contains test
+		CmdNot string // Not contains
+	}{
+		{
+			"Only service",
+			func(pod *corev1.Pod) *corev1.Pod {
+				pod.Annotations[annotationService] = "web"
+				return pod
+			},
+			"-service=web",
+			"-register",
+		},
+
+		{
+			"Service port specified",
+			func(pod *corev1.Pod) *corev1.Pod {
+				pod.Annotations[annotationService] = "web"
+				pod.Annotations[annotationPort] = "1234"
+				return pod
+			},
+			"-service-addr=127.0.0.1:1234",
+			"",
+		},
+
+		{
+			"Upstream",
+			func(pod *corev1.Pod) *corev1.Pod {
+				pod.Annotations[annotationService] = "web"
+				pod.Annotations[annotationUpstreams] = "db:1234"
+				return pod
+			},
+			"-upstream=db:1234",
+			"",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.Name, func(t *testing.T) {
+			require := require.New(t)
+
+			var h Handler
+			container := h.containerSidecar(tt.Pod(minimal()))
+			actual := strings.Join(container.Command, " ")
+			require.Contains(actual, tt.Cmd)
+			if tt.CmdNot != "" {
+				require.NotContains(actual, tt.CmdNot)
+			}
 		})
 	}
 }
