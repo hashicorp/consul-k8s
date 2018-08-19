42e6dd65b10e39c1e62cd4229af5012847df6d09
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-18 18:13:33 -0700

    Set version to human version

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"os"
 
 	"github.com/hashicorp/consul-k8s/subcommand"
+	"github.com/hashicorp/consul-k8s/version"
 	"github.com/mitchellh/cli"
 )
 
 func main() {
 	ui := &cli.BasicUi{Writer: os.Stdout, ErrorWriter: os.Stderr}
-	c := cli.NewCLI("consul-k8s", "0.1.0")
+	c := cli.NewCLI("consul-k8s", version.GetHumanVersion())
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
 		"inject": func() (cli.Command, error) { return &subcommand.Inject{UI: ui}, nil },
