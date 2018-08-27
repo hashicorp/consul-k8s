49a32dce9fc46f12fc82fe0d7b6cc09cdadb85c8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-27 13:25:31 -0700

    Reorganizing a bit to prepare for new commands

--- main.go
+++ main.go
@@ -4,18 +4,14 @@ import (
 	"log"
 	"os"
 
-	"github.com/hashicorp/consul-k8s/subcommand"
 	"github.com/hashicorp/consul-k8s/version"
 	"github.com/mitchellh/cli"
 )
 
 func main() {
-	ui := &cli.BasicUi{Writer: os.Stdout, ErrorWriter: os.Stderr}
 	c := cli.NewCLI("consul-k8s", version.GetHumanVersion())
 	c.Args = os.Args[1:]
-	c.Commands = map[string]cli.CommandFactory{
-		"inject": func() (cli.Command, error) { return &subcommand.Inject{UI: ui}, nil },
-	}
+	c.Commands = Commands
 
 	exitStatus, err := c.Run()
 	if err != nil {
