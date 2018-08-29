b0d45e3d96f7927f17b8c7413b9e5f26e3a877b3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-28 20:01:11 -0700

    command/sync-catalog

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/hashicorp/consul-k8s/subcommand"
+	cmdSyncCatalog "github.com/hashicorp/consul-k8s/subcommand/sync-catalog"
 	"github.com/mitchellh/cli"
 )
 
@@ -16,6 +17,10 @@ func init() {
 	Commands = map[string]cli.CommandFactory{
 		"inject": func() (cli.Command, error) {
 			return &subcommand.Inject{UI: ui}, nil
+		},
+
+		"sync-catalog": func() (cli.Command, error) {
+			return &cmdSyncCatalog.Command{UI: ui}, nil
 		},
 	}
 }
