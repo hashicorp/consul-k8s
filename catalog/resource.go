b3c167d9c3591ccb1819b49020f504afacb1c127
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-29 09:33:21 -0700

    catalog: basic load balancer => registration generation works

--- catalog/resource.go
+++ catalog/resource.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/hashicorp/consul-k8s/helper/controller"
+	consulapi "github.com/hashicorp/consul/api"
 	"github.com/hashicorp/go-hclog"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,13 +17,19 @@ import (
 // ServiceResource implements controller.Resource to sync Service resource
 // types from K8S.
 type ServiceResource struct {
-	Client kubernetes.Interface
 	Log    hclog.Logger
+	Client kubernetes.Interface
+	Syncer Syncer
 
 	// serviceMap is a mapping of unique key (given by controller) to
-	// the service structure.
-	serviceMap  map[string]*apiv1.Service
-	serviceLock sync.RWMutex
+	// the service structure. endpointsMap is the mapping of the same
+	// uniqueKey to a set of endpoints.
+	//
+	// serviceLock must be held for any read/write to these maps.
+	serviceLock  sync.RWMutex
+	serviceMap   map[string]*apiv1.Service
+	endpointsMap map[string]*apiv1.Endpoints
+	consulMap    map[string][]*consulapi.CatalogRegistration
 }
 
 // Informer implements the controller.Resource interface.
@@ -54,13 +61,21 @@ func (t *ServiceResource) Upsert(key string, raw interface{}) error {
 
 	t.serviceLock.Lock()
 	defer t.serviceLock.Unlock()
+	t.Log.Info("upsert", "key", key)
 
+	// TODO(mitchellh): check if syncing is enabled for this service
+
+	// Syncing is enabled, let's keep track of this service.
 	if t.serviceMap == nil {
 		t.serviceMap = make(map[string]*apiv1.Service)
 	}
 	t.serviceMap[key] = service
 
-	t.Log.Info("upsert", "raw", raw)
+	// TODO(mitchellh): on initial load populate the endpoints
+
+	// Update the registration and trigger a sync
+	t.generateRegistrations(key)
+	t.sync()
 	return nil
 }
 
@@ -69,6 +84,8 @@ func (t *ServiceResource) Delete(key string) error {
 	t.serviceLock.Lock()
 	defer t.serviceLock.Unlock()
 	delete(t.serviceMap, key)
+	delete(t.endpointsMap, key)
+	delete(t.consulMap, key)
 
 	t.Log.Info("delete", "key", key)
 	return nil
@@ -81,6 +98,92 @@ func (t *ServiceResource) Run(ch <-chan struct{}) {
 		Log:      t.Log,
 		Resource: &serviceEndpointsResource{Service: t},
 	}).Run(ch)
+}
+
+// generateRegistrations generates the necessary Consul registrations for
+// the given key. This is best effort: if there isn't enough information
+// yet to register a service, then no registration will be generated.
+//
+// Precondition: the lock t.lock is held.
+func (t *ServiceResource) generateRegistrations(key string) {
+	// Get the service. If it doesn't exist, then we can't generate.
+	svc, ok := t.serviceMap[key]
+	if !ok {
+		return
+	}
+
+	// Initialize our consul service map here if it isn't already.
+	if t.consulMap == nil {
+		t.consulMap = make(map[string][]*consulapi.CatalogRegistration)
+	}
+
+	// baseNode and baseService are the base that should be modified with
+	// service-type specific changes. These are not pointers, they should be
+	// shallow copied for each instance.
+	baseNode := consulapi.CatalogRegistration{
+		SkipNodeUpdate: true,
+		Node:           "k8s-sync",
+		Address:        "127.0.0.1",
+		NodeMeta: map[string]string{
+			"consul-source": "k8s",
+		},
+	}
+
+	baseService := consulapi.AgentService{
+		Service: svc.Name,
+		Tags:    []string{"k8s"},
+		Meta: map[string]string{
+			"consul-source":  "k8s",
+			"consul-k8s-key": key,
+		},
+	}
+
+	// TODO(mitchellh): read annotations and modify the base here
+
+	switch svc.Spec.Type {
+	// For LoadBalancer type services, we create a service instance for
+	// each LoadBalancer entry. We only support entries that have an IP
+	// address assigned (not hostnames).
+	case apiv1.ServiceTypeLoadBalancer:
+		t.consulMap[key] = []*consulapi.CatalogRegistration{}
+		for _, ingress := range svc.Status.LoadBalancer.Ingress {
+			addr := ingress.IP
+			if addr == "" {
+				addr = ingress.Hostname
+			}
+			if addr == "" {
+				continue
+			}
+
+			r := baseNode
+			rs := baseService
+			r.Service = &rs
+			r.Service.Address = addr
+			t.consulMap[key] = append(t.consulMap[key], &r)
+		}
+
+		t.Log.Debug("generated registration",
+			"key", key,
+			"service", baseService.Service,
+			"instances", len(t.consulMap[key]))
+	}
+}
+
+// sync calls the Syncer.Sync function from the generated registrations.
+//
+// Precondition: lock must be held
+func (t *ServiceResource) sync() {
+	// NOTE(mitchellh): This isn't the most efficient way to do this and
+	// the times that sync are called are also not the most efficient. All
+	// of these are implementation details so lets improve this later when
+	// it becomes a performance issue and just do the easy thing first.
+	rs := make([]*consulapi.CatalogRegistration, 0, len(t.consulMap)*4)
+	for _, set := range t.consulMap {
+		rs = append(rs, set...)
+	}
+
+	// Sync, which should be non-blocking in real-world cases
+	t.Syncer.Sync(rs)
 }
 
 // serviceEndpointsResource implements controller.Resource and starts
---
+++ catalog/testing.go
@@ -0,0 +1,21 @@
+package catalog
+
+import (
+	"sync"
+
+	"github.com/hashicorp/consul/api"
+)
+
+// TestSyncer implements Syncer for tests, giving easy access to the
+// set of registrations.
+type TestSyncer struct {
+	sync.Mutex    // Lock should be held while accessing Registrations
+	Registrations []*api.CatalogRegistration
+}
+
+// Sync implements Syncer
+func (s *TestSyncer) Sync(rs []*api.CatalogRegistration) {
+	s.Lock()
+	defer s.Unlock()
+	s.Registrations = rs
+}
