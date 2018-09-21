bed30a9571909731c22f2af3f77da0b9578d3cba
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-09-21 12:49:55 -0700

    subcommand/sync-catalog: support Consul to Kube sync

--- subcommand/sync-catalog/command.go
+++ subcommand/sync-catalog/command.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 	"time"
 
+	catalogFromConsul "github.com/hashicorp/consul-k8s/catalog/from-consul"
 	catalogFromK8S "github.com/hashicorp/consul-k8s/catalog/from-k8s"
 	"github.com/hashicorp/consul-k8s/helper/controller"
 	"github.com/hashicorp/consul-k8s/subcommand"
@@ -26,12 +27,16 @@ import (
 type Command struct {
 	UI cli.Ui
 
-	flags          *flag.FlagSet
-	http           *flags.HTTPFlags
-	k8s            *k8sflags.K8SFlags
-	flagDefault    bool
-	flagNamespace  string
-	flagSyncPeriod flags.DurationValue
+	flags                  *flag.FlagSet
+	http                   *flags.HTTPFlags
+	k8s                    *k8sflags.K8SFlags
+	flagToConsul           bool
+	flagToK8S              bool
+	flagConsulDomain       string
+	flagK8SDefault         bool
+	flagK8SSourceNamespace string
+	flagK8SWriteNamespace  string
+	flagConsulWritePeriod  flags.DurationValue
 
 	once sync.Once
 	help string
@@ -39,17 +44,27 @@ type Command struct {
 
 func (c *Command) init() {
 	c.flags = flag.NewFlagSet("", flag.ContinueOnError)
-	c.flags.BoolVar(&c.flagDefault, "default-sync", true,
-		"If true, all valid services are synced by default. If false, "+
+	c.flags.BoolVar(&c.flagToConsul, "to-consul", true,
+		"If true, K8S services will be synced to Consul.")
+	c.flags.BoolVar(&c.flagToK8S, "to-k8s", true,
+		"If true, Consul services will be synced to Kubernetes.")
+	c.flags.BoolVar(&c.flagK8SDefault, "k8s-default-sync", true,
+		"If true, all valid services in K8S are synced by default. If false, "+
 			"the service must be annotated properly to sync. In either case "+
 			"an annotation can override the default")
-	c.flags.StringVar(&c.flagNamespace, "-k8s-namespace", metav1.NamespaceAll,
-		"The Kubernetes namespace to watch for service changes and sync. "+
+	c.flags.StringVar(&c.flagK8SSourceNamespace, "k8s-source-namespace", metav1.NamespaceAll,
+		"The Kubernetes namespace to watch for service changes and sync to Consul. "+
 			"If this is not set then it will default to all namespaces.")
-	c.flags.Var(&c.flagSyncPeriod, "-sync-interval",
-		"The interval to perform syncing operations. All changes are "+
-			"merged and write calls are only made on this interval. Defaults "+
-			"to 30 seconds.")
+	c.flags.StringVar(&c.flagK8SWriteNamespace, "k8s-write-namespace", metav1.NamespaceDefault,
+		"The Kubernetes namespace to write to for services from Consul. "+
+			"If this is not set then it will default to the default namespace.")
+	c.flags.StringVar(&c.flagConsulDomain, "consul-domain", "consul",
+		"The domain for Consul services to use when writing services to "+
+			"Kubernetes. Defaults to consul.")
+	c.flags.Var(&c.flagConsulWritePeriod, "consul-write-interval",
+		"The interval to perform syncing operations creating Consul services. "+
+			"All changes are merged and write calls are only made on this "+
+			"interval. Defaults to 30 seconds.")
 
 	c.http = &flags.HTTPFlags{}
 	c.k8s = &k8sflags.K8SFlags{}
@@ -91,46 +106,98 @@ func (c *Command) Run(args []string) int {
 
 	// Get the sync interval
 	var syncInterval time.Duration
-	c.flagSyncPeriod.Merge(&syncInterval)
+	c.flagConsulWritePeriod.Merge(&syncInterval)
 
 	// Create the context we'll use to cancel everything
 	ctx, cancelF := context.WithCancel(context.Background())
 
-	// Build the Consul sync and start it
-	syncer := &catalogFromK8S.ConsulSyncer{
-		Client:            consulClient,
-		Log:               hclog.Default().Named("syncer/consul"),
-		Namespace:         c.flagNamespace,
-		SyncPeriod:        syncInterval,
-		ServicePollPeriod: syncInterval * 2,
-	}
-	go syncer.Run(ctx)
+	// Start the K8S-to-Consul syncer
+	toConsulCh := make(chan struct{})
+	close(toConsulCh)
+	if c.flagToConsul {
+		// Build the Consul sync and start it
+		syncer := &catalogFromK8S.ConsulSyncer{
+			Client:            consulClient,
+			Log:               hclog.Default().Named("to-consul/sink"),
+			Namespace:         c.flagK8SSourceNamespace,
+			SyncPeriod:        syncInterval,
+			ServicePollPeriod: syncInterval * 2,
+		}
+		go syncer.Run(ctx)
 
-	// Build the controller and start it
-	ctl := &controller.Controller{
-		Log: hclog.Default().Named("controller/service"),
-		Resource: &catalogFromK8S.ServiceResource{
-			Log:            hclog.Default().Named("controller/service"),
-			Client:         clientset,
-			Syncer:         syncer,
-			Namespace:      c.flagNamespace,
-			ExplicitEnable: !c.flagDefault,
-		},
+		// Build the controller and start it
+		ctl := &controller.Controller{
+			Log: hclog.Default().Named("to-consul/controller"),
+			Resource: &catalogFromK8S.ServiceResource{
+				Log:            hclog.Default().Named("to-consul/source"),
+				Client:         clientset,
+				Syncer:         syncer,
+				Namespace:      c.flagK8SSourceNamespace,
+				ExplicitEnable: !c.flagK8SDefault,
+			},
+		}
+
+		toConsulCh = make(chan struct{})
+		go func() {
+			defer close(toConsulCh)
+			ctl.Run(ctx.Done())
+		}()
 	}
 
-	doneCh := make(chan struct{})
-	go func() {
-		defer close(doneCh)
-		ctl.Run(ctx.Done())
-	}()
+	// Start Consul-to-K8S sync
+	toK8SCh := make(chan struct{})
+	close(toK8SCh)
+	if c.flagToK8S {
+		sink := &catalogFromConsul.K8SSink{
+			Client:    clientset,
+			Namespace: c.flagK8SWriteNamespace,
+			Log:       hclog.Default().Named("to-k8s/sink"),
+		}
+
+		source := &catalogFromConsul.Source{
+			Client: consulClient,
+			Domain: c.flagConsulDomain,
+			Sink:   sink,
+			Log:    hclog.Default().Named("to-k8s/source"),
+		}
+		go source.Run(ctx)
+
+		// Build the controller and start it
+		ctl := &controller.Controller{
+			Log:      hclog.Default().Named("to-k8s/controller"),
+			Resource: sink,
+		}
+
+		toK8SCh = make(chan struct{})
+		go func() {
+			defer close(toK8SCh)
+			ctl.Run(ctx.Done())
+		}()
+	}
 
 	// Wait on an interrupt to exit
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
-	<-sigCh
-	cancelF()
-	<-doneCh
-	return 0
+	select {
+	// Unexpected exit
+	case <-toConsulCh:
+		cancelF()
+		<-toK8SCh
+		return 1
+
+	// Unexpected exit
+	case <-toK8SCh:
+		cancelF()
+		<-toConsulCh
+		return 1
+
+	// Interrupted, gracefully exit
+	case <-sigCh:
+		cancelF()
+		<-toConsulCh
+		<-toK8SCh
+		return 0
+	}
 }
 
 func (c *Command) Synopsis() string { return synopsis }
