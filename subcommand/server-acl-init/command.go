6c063aab7ae092ada73ede92b4638f73e578a1a3
Rebecca Zanzig <rzazzl@hotmail.com>
2019-04-03 16:49:32 -0700

    Support ACL bootstrapping
    This creates two new commands for consul-k8s:
    
    1) server-acl-init
    This functions as a post-install script that connects to a
    server pod, bootstraps ACLs, creates server tokens, passes
    these out to all Consul servers and reloads their config. It
    then creates a client token and (optional) catalog sync token
    that are written out into Kubernetes secrets that will be
    consumed by those pod's init containers.
    
    2) acl-init
    This is run by the init container of clients and (optionally)
    the catalog sync pod. It waits for a Kubernetes secret to be
    populated with an ACL token that will be consumed and applied
    to the pod.
    
    Because of change to Consul's packaging, this also required
    updating dependencies. Details of that process:
    update to consul, consul/api, and consul/sdk master tip versions
    
    1. update testutil/retry => sdk/testutil/retry
    2. go get github.com/hashicorp/consul@master
    3. go get github.com/hashicorp/consul/sdk@master
    4. go get github.com/hashicorp/consul/api@master
    5. go mod tidy

---
+++ subcommand/server-acl-init/command.go
@@ -0,0 +1,350 @@
+package serveraclinit
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/hashicorp/consul-k8s/subcommand"
+	k8sflags "github.com/hashicorp/consul-k8s/subcommand/flags"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/command/flags"
+	"github.com/hashicorp/go-hclog"
+	"github.com/mitchellh/cli"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type Command struct {
+	UI cli.Ui
+
+	flags               *flag.FlagSet
+	k8s                 *k8sflags.K8SFlags
+	flagReleaseName     string
+	flagReplicas        int
+	flagNamespace       string
+	flagAllowDNS        bool
+	flagCreateSyncToken bool
+	flagLogLevel        string
+
+	once sync.Once
+	help string
+}
+
+func (c *Command) init() {
+	c.flags = flag.NewFlagSet("", flag.ContinueOnError)
+	c.flags.StringVar(&c.flagReleaseName, "release-name", "",
+		"Name of Consul Helm release")
+	c.flags.IntVar(&c.flagReplicas, "expected-replicas", 1,
+		"Number of expected Consul server replicas")
+	c.flags.StringVar(&c.flagNamespace, "k8s-namespace", "",
+		"Name of Kubernetes namespace where the servers are deployed")
+	c.flags.BoolVar(&c.flagAllowDNS, "allow-dns", false,
+		"Toggle for updating the anonymous token to allow DNS queries to work")
+	c.flags.BoolVar(&c.flagCreateSyncToken, "create-sync-token", false,
+		"Toggle for creating a catalog sync token")
+	c.flags.StringVar(&c.flagLogLevel, "log-level", "info",
+		"Log verbosity level. Supported values (in order of detail) are \"trace\", "+
+			"\"debug\", \"info\", \"warn\", and \"error\".")
+
+	c.k8s = &k8sflags.K8SFlags{}
+	flags.Merge(c.flags, c.k8s.Flags())
+	c.help = flags.Usage(help, c.flags)
+}
+
+func (c *Command) Run(args []string) int {
+	c.once.Do(c.init)
+	if err := c.flags.Parse(args); err != nil {
+		return 1
+	}
+	if len(c.flags.Args()) > 0 {
+		c.UI.Error(fmt.Sprintf("Should have no non-flag arguments."))
+		return 1
+	}
+
+	config, err := subcommand.K8SConfig(c.k8s.KubeConfig())
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error retrieving Kubernetes auth: %s", err))
+		return 1
+	}
+
+	// Create the Kubernetes clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error initializing Kubernetes client: %s", err))
+		return 1
+	}
+
+	level := hclog.LevelFromString(c.flagLogLevel)
+	if level == hclog.NoLevel {
+		c.UI.Error(fmt.Sprintf("Unknown log level: %s", c.flagLogLevel))
+		return 1
+	}
+
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:  level,
+		Output: os.Stderr,
+	})
+
+	// Use the client to get statefulset pods
+	labelSelector := fmt.Sprintf("component=server, app=consul, release=%s", c.flagReleaseName)
+
+	serverPods, err := clientset.CoreV1().Pods(c.flagNamespace).List(metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		logger.Error(err.Error())
+		return 1
+	}
+
+	if len(serverPods.Items) == 0 {
+		logger.Error("No pods were found")
+		return 1
+	}
+
+	// Pull the addresses out of each pod
+	var podAddresses []string
+	for _, pod := range serverPods.Items {
+		address := fmt.Sprintf("%s:8500", pod.Status.PodIP)
+		if pod.Status.PodIP != "" {
+			podAddresses = append(podAddresses, address)
+			logger.Info(address)
+		}
+	}
+
+	if len(podAddresses) < c.flagReplicas {
+		logger.Error(fmt.Sprintf("Not enough pod addresses were found: %d", len(podAddresses)))
+		return 1
+	}
+
+	// Pick the first pod to connect to for bootstrapping & set up connection
+	consulConfig := api.Config{
+		Address: podAddresses[0],
+		Scheme:  "http",
+	}
+
+	consulClient, err := api.NewClient(&consulConfig)
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error connecting to Consul agent: %s", err))
+		return 1
+	}
+
+	// Bootstrap the ACLs
+	bootstrapToken, _, err := consulClient.ACL().Bootstrap()
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error bootstrapping Consul agent: %s", err))
+		return 1
+	}
+
+	// Write bootstrap token to a Kubernetes secret
+	_, err = clientset.CoreV1().Secrets(c.flagNamespace).Create(&apiv1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%s-consul-bootstrap-acl-token", c.flagReleaseName),
+		},
+		StringData: map[string]string{
+			"token": bootstrapToken.SecretID,
+		},
+	})
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating bootstrap token secret: %s", err))
+		return 1
+	}
+
+	// Create agent policy
+	agentRules := `node_prefix "" {
+   policy = "write"
+}
+service_prefix "" {
+   policy = "read"
+}`
+
+	agentPolicy := api.ACLPolicy{
+		Name:        "agent-token",
+		Description: "Agent Token Policy",
+		Rules:       agentRules,
+	}
+
+	aclAgentPolicy, _, err := consulClient.ACL().PolicyCreate(&agentPolicy, &api.WriteOptions{Token: bootstrapToken.SecretID})
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating agent policy: %s", err))
+		return 1
+	}
+
+	// Create agent token for each agent
+	var serverTokens []api.ACLToken
+
+	for i := 0; i < len(podAddresses); i++ {
+		// Include the pod name into the token description
+		token := api.ACLToken{
+			Description: fmt.Sprintf("Server Agent Token for %s", serverPods.Items[i].Name),
+			Policies:    []*api.ACLTokenPolicyLink{&api.ACLTokenPolicyLink{Name: aclAgentPolicy.Name}},
+		}
+
+		newToken, _, err := consulClient.ACL().TokenCreate(&token, &api.WriteOptions{Token: bootstrapToken.SecretID})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error creating agent token %v: %s", i, err))
+			return 1
+		}
+		serverTokens = append(serverTokens, *newToken)
+	}
+
+	// Pass out agent tokens and restart the servers
+	for i := 0; i < len(podAddresses); i++ {
+		// Connect to other pods
+		consulConfig := api.Config{
+			Address: podAddresses[i],
+			Scheme:  "http",
+			Token:   bootstrapToken.SecretID,
+		}
+
+		consulClient, err = api.NewClient(&consulConfig)
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error connecting to Consul agent %v: %s", i, err))
+			return 1
+		}
+
+		// Apply token
+		_, err = consulClient.Agent().UpdateAgentACLToken(serverTokens[i].SecretID, &api.WriteOptions{})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error applying agent token %v: %s", i, err))
+			return 1
+		}
+
+		// Restart
+		err = consulClient.Agent().Reload()
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error restarting agent %v: %s", i, err))
+			return 1
+		}
+	}
+
+	// Create client agent token
+	token := api.ACLToken{
+		Description: "Client Agent Token",
+		Policies:    []*api.ACLTokenPolicyLink{&api.ACLTokenPolicyLink{Name: aclAgentPolicy.Name}},
+	}
+
+	clientToken, _, err := consulClient.ACL().TokenCreate(&token, &api.WriteOptions{Token: bootstrapToken.SecretID})
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating client agent token: %s", err))
+		return 1
+	}
+
+	// Write client agent token to a Kubernetes secret
+	_, err = clientset.CoreV1().Secrets(c.flagNamespace).Create(&apiv1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("%s-consul-client-acl-token", c.flagReleaseName),
+		},
+		StringData: map[string]string{
+			"token": clientToken.SecretID,
+		},
+	})
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error creating client token secret: %s", err))
+		return 1
+	}
+
+	// Update anonymous token to allow DNS to work
+	if c.flagAllowDNS {
+		// Create policy for the anonymous token
+		dnsRules := `node_prefix "" {
+   policy = "read"
+}
+service_prefix "" {
+   policy = "read"
+}`
+
+		dnsPolicy := api.ACLPolicy{
+			Name:        "dns-policy",
+			Description: "DNS Policy",
+			Rules:       dnsRules,
+		}
+
+		aclDNSPolicy, _, err := consulClient.ACL().PolicyCreate(&dnsPolicy, &api.WriteOptions{})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error creating dns policy: %s", err))
+			return 1
+		}
+
+		// Create token to get sent to TokenUpdate
+		aToken := api.ACLToken{
+			AccessorID: "00000000-0000-0000-0000-000000000002",
+			Policies:   []*api.ACLTokenPolicyLink{&api.ACLTokenPolicyLink{Name: aclDNSPolicy.Name}},
+		}
+
+		// Update anonymous token to include this policy
+		_, _, err = consulClient.ACL().TokenUpdate(&aToken, &api.WriteOptions{})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error updating anonymous token: %s", err))
+			return 1
+		}
+	}
+
+	// Create catalog sync token if necessary
+	if c.flagCreateSyncToken {
+		// Create agent policy
+		syncRules := `node_prefix "" {
+   policy = "read"
+}
+node "k8s-sync" {
+	policy = "write"
+}
+service_prefix "" {
+   policy = "write"
+}`
+
+		syncPolicy := api.ACLPolicy{
+			Name:        "catalog-sync-token",
+			Description: "Catalog Sync Token Policy",
+			Rules:       syncRules,
+		}
+
+		aclSyncPolicy, _, err := consulClient.ACL().PolicyCreate(&syncPolicy, &api.WriteOptions{})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error creating catalog sync policy: %s", err))
+			return 1
+		}
+
+		sToken := api.ACLToken{
+			Description: "Catalog Sync Token",
+			Policies:    []*api.ACLTokenPolicyLink{&api.ACLTokenPolicyLink{Name: aclSyncPolicy.Name}},
+		}
+
+		// Create catalog sync token
+		syncToken, _, err := consulClient.ACL().TokenCreate(&sToken, &api.WriteOptions{})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error creating client agent token: %s", err))
+			return 1
+		}
+
+		// Write catalog sync token to a Kubernetes secret
+		_, err = clientset.CoreV1().Secrets(c.flagNamespace).Create(&apiv1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: fmt.Sprintf("%s-consul-catalog-sync-acl-token", c.flagReleaseName),
+			},
+			StringData: map[string]string{
+				"token": syncToken.SecretID,
+			},
+		})
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error creating catalog sync token secret: %s", err))
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func (c *Command) Synopsis() string { return synopsis }
+func (c *Command) Help() string {
+	c.once.Do(c.init)
+	return c.help
+}
+
+const synopsis = "Initialize ACLs on Consul servers."
+const help = `
+Usage: consul-k8s server-acl-init [options]
+
+  Bootstraps servers with ACLs
+
+`
