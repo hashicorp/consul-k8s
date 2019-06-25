39241fd31172616b6de4f8a871260b1a0859552b
Paul Bouwer <paul.bouwer@gmail.com>
2019-06-25 16:51:42 +1000

    Ensure kubernetes.default.svc can be found when consul deployed into another namespace.

--- subcommand/server-acl-init/command.go
+++ subcommand/server-acl-init/command.go
@@ -302,8 +302,8 @@ func (c *Command) Run(args []string) int {
 
 	// Support ConnectInject using Kubernetes as an auth method
 	if c.flagCreateInjectAuthMethod {
-		// Get the Kubernetes service IP address
-		k8sService, err := c.clientset.CoreV1().Services(c.flagNamespace).Get("kubernetes", metav1.GetOptions{})
+		// Get the Kubernetes service IP address - kubernetes.default.svc
+		k8sService, err := c.clientset.CoreV1().Services("default").Get("kubernetes", metav1.GetOptions{})
 		if err != nil {
 			c.UI.Error(fmt.Sprintf("Error getting kubernetes service: %s", err))
 			return 1
