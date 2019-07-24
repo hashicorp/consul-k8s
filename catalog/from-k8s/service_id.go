addbb930551c46b6bab5b46f27f2ac15a8dee682
Nikita Massalitin <massalitine@mail.ru>
2019-07-24 14:09:24 +0300

    Update service_id.go
    Fix issue when you have multiple services endpoints under one service.
    For example:
     postgres-db --
                              postgres-db-sessions
                              postgres-db-users
    with different consul.hashicorp.com/service-meta-XXX, but with the same ip address.
    Before hash calculated only by ip address, now I added some random component

--- catalog/from-k8s/service_id.go
+++ catalog/from-k8s/service_id.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // serviceID generates a unique ID for a service. This ID is not meant
@@ -12,6 +14,7 @@ func serviceID(name, addr string) string {
 	// sha1 is fine because we're doing this for uniqueness, not any
 	// cryptographic strength. We then take only the first 12 because its
 	// _probably_ unique and makes it easier to read.
-	sum := sha1.Sum([]byte(fmt.Sprintf("%s-%s", name, addr)))
+	rand.Seed(time.Now().UnixNano())
+	sum := sha1.Sum([]byte(fmt.Sprintf("%s-%s", name, addr, rand.Int())))
 	return fmt.Sprintf("%s-%s", name, hex.EncodeToString(sum[:])[:12])
 }
