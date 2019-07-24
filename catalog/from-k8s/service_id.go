a82518a242b5dced07cced35da86a44bb4c1c652
Nikita Massalitin <massalitine@mail.ru>
2019-07-24 14:29:33 +0300

    Update service_id.go
    fix

--- catalog/from-k8s/service_id.go
+++ catalog/from-k8s/service_id.go
@@ -15,6 +15,6 @@ func serviceID(name, addr string) string {
 	// cryptographic strength. We then take only the first 12 because its
 	// _probably_ unique and makes it easier to read.
 	rand.Seed(time.Now().UnixNano())
-	sum := sha1.Sum([]byte(fmt.Sprintf("%s-%s", name, addr, rand.Int())))
+	sum := sha1.Sum([]byte(fmt.Sprintf("%s-%s-%d", name, addr, rand.Int())))
 	return fmt.Sprintf("%s-%s", name, hex.EncodeToString(sum[:])[:12])
 }
