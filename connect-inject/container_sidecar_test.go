678e6e8f37d44877e6b27e90f5fc2a1cc967396a
Kevin Jing Qiu <kevin@idempotent.ca>
2019-12-11 18:18:33 -0500

    Write failing tests

---
+++ connect-inject/container_sidecar_test.go
@@ -0,0 +1,46 @@
+package connectinject
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestContainerSidecarCommand(t *testing.T) {
+	cases := []struct {
+		name                     string
+		extraEnvoyArgs           string
+		expectedContainerCommand []string
+	}{
+		{
+			name:           "no extra args provided",
+			extraEnvoyArgs: "",
+			expectedContainerCommand: []string{
+				"envoy", "--max-obj-name-len", "256",
+				"--config-path", "/consul/connect-inject/envoy-bootstrap.yaml",
+			},
+		},
+		{
+			name:           "extra loglevel args",
+			extraEnvoyArgs: "--log-level debug",
+			expectedContainerCommand: []string{
+				"envoy", "--max-obj-name-len", "256",
+				"--config-path", "/consul/connect-inject/envoy-bootstrap.yaml",
+				"--log-level", "debug",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			h := Handler{
+				ImageConsul:    "hashicorp/consul:latest",
+				ImageEnvoy:     "hashicorp/consul-k8s:latest",
+				ExtraEnvoyArgs: tc.extraEnvoyArgs,
+			}
+
+			c, err := h.containerSidecar(nil)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedContainerCommand, c.Command)
+		})
+	}
+}
