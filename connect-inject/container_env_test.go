c3f03bf808dbb72b9a8667a21049bd15d283e8e6
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2020-06-02 21:35:52 -0400

    Simplified unit test.

--- connect-inject/container_env_test.go
+++ connect-inject/container_env_test.go
@@ -1,8 +1,6 @@
 package connectinject
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,36 +9,18 @@ import (
 )
 
 func TestContainerEnvVars(t *testing.T) {
-	serviceHost := "static-server"
-	servicePort := "7890"
-	upstreamAnnotation := fmt.Sprintf("%s:%s:dc2", serviceHost, servicePort)
-
-	minimal := func() *corev1.Pod {
-		return &corev1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{
-					annotationService: "foo",
-				},
-			},
-		}
-	}
 
 	cases := []struct {
-		Name        string
-		Pod         func(*corev1.Pod) *corev1.Pod
-		ServiceHost string // Service host name used to craft env var names
-		ServicePort string // Service port that should be set to env var
+		Name     string
+		Upstream string
 	}{
-		// The test checks that upstream annotated pods with a datacenter segment
-		// can be properly parsed into serviceHost and port environment variables.
 		{
 			"Upstream with datacenter",
-			func(pod *corev1.Pod) *corev1.Pod {
-				pod.Annotations[annotationUpstreams] = upstreamAnnotation
-				return pod
-			},
-			serviceHost,
-			servicePort,
+			"static-server:7890:dc1",
+		},
+		{
+			"Upstream without datacenter",
+			"static-server:7890",
 		},
 	}
 
@@ -49,30 +29,24 @@ func TestContainerEnvVars(t *testing.T) {
 			require := require.New(t)
 
 			var h Handler
-			envVars := h.containerEnvVars(tt.Pod(minimal()))
+			envVars := h.containerEnvVars(&corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						annotationService:   "foo",
+						annotationUpstreams: tt.Upstream,
+					},
+				},
+			})
 
-			hostEntryFound := false
-			portEntryFound := false
-			portCorrectlySet := false
-			// TODO: Consider extracting functions for the format calls to ensure no
-			// implementation drift between tests and function under test.
-			formattedServiceHost := strings.ToUpper(strings.Replace(tt.ServiceHost, "-", "_", -1))
-
-			for i := range envVars {
-				if envVars[i].Name == fmt.Sprintf("%s_CONNECT_SERVICE_HOST", formattedServiceHost) {
-					hostEntryFound = true
-				}
-				if envVars[i].Name == fmt.Sprintf("%s_CONNECT_SERVICE_PORT", formattedServiceHost) {
-					portEntryFound = true
-					if envVars[i].Value == tt.ServicePort {
-						portCorrectlySet = true
-					}
-				}
-			}
-
-			require.True(hostEntryFound)
-			require.True(portEntryFound)
-			require.True(portCorrectlySet)
+			require.ElementsMatch(envVars, []corev1.EnvVar{
+				{
+					Name:  "STATIC_SERVER_CONNECT_SERVICE_HOST",
+					Value: "127.0.0.1",
+				}, {
+					Name:  "STATIC_SERVER_CONNECT_SERVICE_PORT",
+					Value: "7890",
+				},
+			})
 		})
 	}
 }
