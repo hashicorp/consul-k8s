33142b857f53568420cecc480d4d9823e56ac1ee
Kyle Schochenmaier <kschoche@gmail.com>
2020-07-06 23:40:20 -0500

    Fix inject-connect tests

--- subcommand/inject-connect/command_test.go
+++ subcommand/inject-connect/command_test.go
@@ -9,47 +9,59 @@ import (
 )
 
 func TestRun_FlagValidation(t *testing.T) {
+	mandatoryResourceFlags := []string{
+		"-init-copy-container-memory-limit=125M",
+		"-init-copy-container-memory-request=25M",
+		"-init-copy-container-cpu-limit=50m",
+		"-init-copy-container-cpu-request=50m",
+		"-lifecycle-sidecar-memory-limit=25Mi",
+		"-lifecycle-sidecar-memory-request=25Mi",
+		"-lifecycle-sidecar-cpu-limit=20m",
+		"-lifecycle-sidecar-cpu-request=20m",
+	}
+
 	cases := []struct {
 		name   string
 		flags  []string
 		expErr string
 	}{
 		{
-			flags:  []string{},
+			//flags:  []string{} + mandatoryResourceFlags,
+			flags:  mandatoryResourceFlags,
 			expErr: "-consul-k8s-image must be set",
 		},
 		{
-			flags:  []string{"-consul-k8s-image", "foo", "-ca-file", "bar"},
+			flags:  append([]string{"-consul-k8s-image", "foo", "-ca-file", "bar"},  mandatoryResourceFlags...),
 			expErr: "Error reading Consul's CA cert file \"bar\"",
 		},
 		{
-			flags:  []string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-cpu-limit=unparseable"},
+			flags:  append([]string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-cpu-limit=unparseable"}, mandatoryResourceFlags...),
 			expErr: "-default-sidecar-proxy-cpu-limit is invalid",
 		},
 		{
-			flags:  []string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-cpu-request=unparseable"},
+			flags:  append([]string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-cpu-request=unparseable"}, mandatoryResourceFlags...),
 			expErr: "-default-sidecar-proxy-cpu-request is invalid",
 		},
 		{
-			flags:  []string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-memory-limit=unparseable"},
+			flags:  append([]string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-memory-limit=unparseable"}, mandatoryResourceFlags...),
 			expErr: "-default-sidecar-proxy-memory-limit is invalid",
 		},
 		{
-			flags:  []string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-memory-request=unparseable"},
+			flags:  append([]string{"-consul-k8s-image", "foo", "-default-sidecar-proxy-memory-request=unparseable"}, mandatoryResourceFlags...),
 			expErr: "-default-sidecar-proxy-memory-request is invalid",
 		},
 		{
-			flags: []string{"-consul-k8s-image", "foo",
+			flags: append([]string{"-consul-k8s-image", "foo",
 				"-default-sidecar-proxy-memory-request=50Mi",
 				"-default-sidecar-proxy-memory-limit=25Mi",
-			},
+			}, mandatoryResourceFlags...),
 			expErr: "request must be <= limit: -default-sidecar-proxy-memory-request value of \"50Mi\" is greater than the -default-sidecar-proxy-memory-limit value of \"25Mi\"",
 		},
 		{
-			flags: []string{"-consul-k8s-image", "foo",
+			flags: append([]string{"-consul-k8s-image", "foo",
 				"-default-sidecar-proxy-cpu-request=50m",
 				"-default-sidecar-proxy-cpu-limit=25m",
-			},
+			}, mandatoryResourceFlags...),
 			expErr: "request must be <= limit: -default-sidecar-proxy-cpu-request value of \"50m\" is greater than the -default-sidecar-proxy-cpu-limit value of \"25m\"",
 		},
 	}
