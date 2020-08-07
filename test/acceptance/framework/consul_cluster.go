487bccecf564a7f378371a9a68c30e0eb1c32544
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2020-08-07 16:27:35 -0700

    Acceptance tests for ingress gateways (#559)
    * Acceptance tests for ingress gateways
    
    Also implements helm upgrade.

--- test/acceptance/framework/consul_cluster.go
+++ test/acceptance/framework/consul_cluster.go
@@ -27,7 +27,10 @@ const helmChartPath = "../../../.."
 type Cluster interface {
 	Create(t *testing.T)
 	Destroy(t *testing.T)
-	Upgrade(t *testing.T)
+	// Upgrade runs helm upgrade. It will merge the helm values from the
+	// initial install with helmValues. Any keys that were previously set
+	// will be overridden by the helmValues keys.
+	Upgrade(t *testing.T, helmValues map[string]string)
 	SetupConsulClient(t *testing.T, secure bool) *api.Client
 }
 
@@ -117,7 +120,8 @@ func (h *HelmCluster) Destroy(t *testing.T) {
 	}
 }
 
-func (h *HelmCluster) Upgrade(t *testing.T) {
+func (h *HelmCluster) Upgrade(t *testing.T, helmValues map[string]string) {
+	mergeMaps(h.helmOptions.SetValues, helmValues)
 	helm.Upgrade(t, h.helmOptions, helmChartPath, h.releaseName)
 	helpers.WaitForAllPodsToBeReady(t, h.kubernetesClient, h.helmOptions.KubectlOptions.Namespace, fmt.Sprintf("release=%s", h.releaseName))
 }
@@ -186,7 +190,7 @@ func (h *HelmCluster) checkForPriorInstallations(t *testing.T) {
 	require.NoError(t, err)
 
 	for _, r := range installedReleases {
-		require.NotContains(t, r["chart"], "consul", fmt.Sprintf("detected an existing installation of Consul %s", r["chart"]))
+		require.NotContains(t, r["chart"], "consul", fmt.Sprintf("detected an existing installation of Consul %s, release name: %s", r["chart"], r["name"]))
 	}
 }
 
--- test/acceptance/helpers/helpers.go
+++ test/acceptance/helpers/helpers.go
@@ -28,7 +28,8 @@ func RandomName() string {
 func WaitForAllPodsToBeReady(t *testing.T, client kubernetes.Interface, namespace, podLabelSelector string) {
 	t.Helper()
 
-	counter := &retry.Counter{Count: 20, Wait: 5 * time.Second}
+	// Wait up to 3m.
+	counter := &retry.Counter{Count: 36, Wait: 5 * time.Second}
 	retry.RunWith(counter, t, func(r *retry.R) {
 		pods, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: podLabelSelector})
 		require.NoError(r, err)
