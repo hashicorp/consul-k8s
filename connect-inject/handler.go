bd2378269d1316f4c914858eb349bb4a5e4f95c9
Nitya Dhanushkodi <nitya@hashicorp.com>
2020-11-05 17:04:58 -0800

    Update Changelog

--- connect-inject/handler.go
+++ connect-inject/handler.go
@@ -87,9 +87,9 @@ const (
 
 	// annotationEnvoyExtraArgs is a space-separated list of arguments to be passed to the
 	// envoy binary. See list of args here: https://www.envoyproxy.io/docs/envoy/latest/operations/cli
-	// e.g. consul.hashicorp.com/envoy-extra-args:--log-level debug --disable-hot-restart
+	// e.g. consul.hashicorp.com/envoy-extra-args: "--log-level debug --disable-hot-restart"
 	// The arguments passed in via this annotation will take precendence over arguments
-	// passed via the global helm template value `global.envoyExtraArgs`
+	// passed via the helm template value `connectInject.envoyExtraArgs`
 	annotationEnvoyExtraArgs = "consul.hashicorp.com/envoy-extra-args"
 
 	// injected is used as the annotation value for annotationInjected
