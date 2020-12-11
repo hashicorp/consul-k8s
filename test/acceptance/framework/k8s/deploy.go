4480ad2590f9b970bbb0da11583534fd21c634d2
Nitya Dhanushkodi <nitya@hashicorp.com>
2020-12-11 13:26:09 -0800

    Add more time to verifyFederation and checking static server connection (#736)
    Adds more time to verifyFederation and checking the static server connection to see if it addresses flakes on EKS. Logs the amount of time it took to do those things.
    
    Co-authored-by: Luke Kysow <1034429+lkysow@users.noreply.github.com>

--- test/acceptance/framework/k8s/deploy.go
+++ test/acceptance/framework/k8s/deploy.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/hashicorp/consul-helm/test/acceptance/framework/helpers"
+	"github.com/hashicorp/consul-helm/test/acceptance/framework/logger"
 	"github.com/hashicorp/consul/sdk/testutil/retry"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/apps/v1"
@@ -131,7 +132,9 @@ func CheckStaticServerConnectionMultipleFailureMessages(
 // CheckStaticServerConnectionSuccessful is just like CheckStaticServerConnection
 // but it always expects a successful connection.
 func CheckStaticServerConnectionSuccessful(t *testing.T, options *k8s.KubectlOptions, deploymentName string, curlArgs ...string) {
+	start := time.Now()
 	CheckStaticServerConnection(t, options, true, deploymentName, "", curlArgs...)
+	logger.Logf(t, "Took %s to check if static server connection was successful", time.Since(start))
 }
 
 // CheckStaticServerConnectionSuccessful is just like CheckStaticServerConnection
