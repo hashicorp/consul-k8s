aee1263e8264de62137091529ae7de86b4d59038
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2020-12-17 13:13:47 -0800

    TerminatingGateway CRD (#408)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ BREAKING CHANGES:
 
 FEATURES:
 * CRDs: add new CRD `IngressGateway` for configuring Consul's [ingress-gateway](https://www.consul.io/docs/agent/config-entries/ingress-gateway) config entry. [[GH-407](https://github.com/hashicorp/consul-k8s/pull/407)]
+* CRDs: add new CRD `TerminatingGateway` for configuring Consul's [terminating-gateway](https://www.consul.io/docs/agent/config-entries/terminating-gateway) config entry. [[GH-408](https://github.com/hashicorp/consul-k8s/pull/408)]
 
 ## 0.21.0 (November 25, 2020)
 
---
+++ controller/terminatinggateway_controller.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	consulv1alpha1 "github.com/hashicorp/consul-k8s/api/v1alpha1"
+)
+
+// TerminatingGatewayController is the controller for TerminatingGateway resources.
+type TerminatingGatewayController struct {
+	client.Client
+	Log                   logr.Logger
+	Scheme                *runtime.Scheme
+	ConfigEntryController *ConfigEntryController
+}
+
+// +kubebuilder:rbac:groups=consul.hashicorp.com,resources=terminatinggateways,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=consul.hashicorp.com,resources=terminatinggateways/status,verbs=get;update;patch
+
+func (r *TerminatingGatewayController) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	return r.ConfigEntryController.ReconcileEntry(r, req, &consulv1alpha1.TerminatingGateway{})
+}
+
+func (r *TerminatingGatewayController) Logger(name types.NamespacedName) logr.Logger {
+	return r.Log.WithValues("request", name)
+}
+
+func (r *TerminatingGatewayController) UpdateStatus(ctx context.Context, obj runtime.Object, opts ...client.UpdateOption) error {
+	return r.Status().Update(ctx, obj, opts...)
+}
+
+func (r *TerminatingGatewayController) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&consulv1alpha1.TerminatingGateway{}).
+		Complete(r)
+}
