05070d23492dcb2e8f0597d09151f47e077add12
Iryna Shustava <ishustava@users.noreply.github.com>
2021-03-18 17:14:43 -0700

    Release 0.31.0 (#862)

--- test/acceptance/tests/metrics/metrics_test.go
+++ test/acceptance/tests/metrics/metrics_test.go
@@ -96,8 +96,8 @@ func TestAppMetrics(t *testing.T) {
 		// This image is required till Consul 1.10 is released and the CI config is updated.
 		// Note we need to set ent license to empty explicitly so that if tests have ent license
 		// globally provided this test won't try to apply it because this image is not an enterprise image.
-		// TODO: Remove this setting and ent license settings once Consul alpha is released.
-		"global.image":                        "docker.mirror.hashicorp.services/hashicorpdev/consul:latest",
+		// TODO: Remove this setting and ent license settings once the Helm chart with Consul 1.10 is released.
+		"global.image":                        "docker.mirror.hashicorp.services/hashicorp/consul:1.10.0-alpha",
 		"server.enterpriseLicense.secretName": "",
 		"server.enterpriseLicense.secretKey":  "",
 
