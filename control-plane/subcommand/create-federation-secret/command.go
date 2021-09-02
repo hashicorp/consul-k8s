c859755a234359c054ddb0b4a8303b52e7209f7b
Thomas Eckert <teckert@hashicorp.com>
2021-09-02 12:00:00 -0400

    Use ctx on create-federation-secret Command

--- control-plane/subcommand/create-federation-secret/command.go
+++ control-plane/subcommand/create-federation-secret/command.go
@@ -247,7 +247,7 @@ func (c *Command) Run(args []string) int {
 	_, err = c.k8sClient.CoreV1().Secrets(c.flagK8sNamespace).Create(c.ctx, federationSecret, metav1.CreateOptions{})
 	if k8serrors.IsAlreadyExists(err) {
 		logger.Info("Secret already exists, updating instead")
-		_, err = c.k8sClient.CoreV1().Secrets(c.flagK8sNamespace).Update(context.TODO(), federationSecret, metav1.UpdateOptions{})
+		_, err = c.k8sClient.CoreV1().Secrets(c.flagK8sNamespace).Update(c.ctx, federationSecret, metav1.UpdateOptions{})
 	}
 
 	if err != nil {
@@ -301,7 +301,7 @@ func (c *Command) replicationToken(logger hclog.Logger) ([]byte, error) {
 	// This will run forever but it's running as a Helm hook so Helm will timeout
 	// after a configurable time period.
 	err := backoff.Retry(func() error {
-		secret, err := c.k8sClient.CoreV1().Secrets(c.flagK8sNamespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+		secret, err := c.k8sClient.CoreV1().Secrets(c.flagK8sNamespace).Get(c.ctx, secretName, metav1.GetOptions{})
 		if k8serrors.IsNotFound(err) {
 			logger.Warn("secret not yet created, retrying", "secret", secretName, "ns", c.flagK8sNamespace)
 			return errors.New("")
