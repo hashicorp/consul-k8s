c40e732d2204ff5fe41e7f40dda2af017e14c6e7
Nitya Dhanushkodi <nitya@hashicorp.com>
2021-10-04 09:35:10 -0700

    add comment

--- charts/embed_chart.go
+++ charts/embed_chart.go
@@ -4,5 +4,12 @@ import (
 	"embed"
 )
 
+// ConsulHelmChart embeds the Consul Helm Chart files into an exported variable from this package. Changes to the chart
+// files referenced below will be reflected in the embedded templates in the CLI at CLI compile time.
+//
+// This is currently only meant to be used by the consul-k8s CLI within this repository. Importing this package from the
+// CLI allows us to embed the templates at compilation time. Since this is in a monorepo, we can directly reference this
+// charts module as relative to the CLI module (with a replace directive), which allows us to not need to bump the
+// charts module dependency manually or as part of a Makefile.
 //go:embed consul/Chart.yaml consul/values.yaml consul/templates consul/templates/_helpers.tpl
 var ConsulHelmChart embed.FS
