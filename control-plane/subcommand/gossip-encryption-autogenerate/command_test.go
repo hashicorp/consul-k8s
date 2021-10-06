fc4b912495b474e18e0541c2000185fa156f5091
Thomas Eckert <teckert@hashicorp.com>
2021-10-06 12:20:18 -0400

    Add test for flag validation

--- control-plane/subcommand/gossip-encryption-autogenerate/command_test.go
+++ control-plane/subcommand/gossip-encryption-autogenerate/command_test.go
@@ -1,1 +1,33 @@
 package gossipencryptionautogenerate
+
+import (
+	"testing"
+
+	"github.com/mitchellh/cli"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun_FlagValidation(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		flags  []string
+		expErr string
+	}{
+		{
+			flags:  []string{},
+			expErr: "-secret-name must be set",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.expErr, func(t *testing.T) {
+			ui := cli.NewMockUi()
+			cmd := Command{
+				UI: ui,
+			}
+			code := cmd.Run(c.flags)
+			require.Equal(t, 1, code)
+			require.Contains(t, ui.ErrorWriter.String(), c.expErr)
+		})
+	}
+}
