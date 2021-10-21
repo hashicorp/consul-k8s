3a0f5a3c4e34a9f9cc60fad7e00a4eafca123d35
Kyle Schochenmaier <kschoche@gmail.com>
2021-10-21 10:54:48 -0500

    clean this up

--- acceptance/framework/vault/vault_cluster.go
+++ acceptance/framework/vault/vault_cluster.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"strings"
 	"testing"
 	"time"
@@ -18,20 +18,31 @@ import (
 	"github.com/hashicorp/consul-k8s/acceptance/framework/k8s"
 	"github.com/hashicorp/consul-k8s/acceptance/framework/logger"
 	"github.com/hashicorp/consul/sdk/testutil/retry"
-	"k8s.io/apimachinery/pkg/api/errors"
-	// https://github.com/hashicorp/vault-examples/tree/main/go
 	vapi "github.com/hashicorp/vault/api"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-// Cluster represents a consul cluster object
+// Cluster represents a vault cluster object, it is modeled off of the consul_cluster implementation and will
+// provide thea ability to rapidly install and bootrstap a vault cluster so that end-to-end testing may be done.
 type Cluster interface {
+	// Create will install a vault cluster via helm using the default config defined at the end of this file.
 	Create(t *testing.T)
+
+	// Bootstrap will execute the Init(), Unseal() process and bootstrap the vault cluster by enabling the KV2 secret
+	// engine and also enabling the Kube Auth Method.
 	Bootstrap(t *testing.T, ctx environment.TestContext)
+
+	// Destroy will do a helm uninstall of the Vault installation and then delete the data PVC used by Vault.
 	Destroy(t *testing.T)
+
+	// SetupVaultClient will setup the port-forwarding to the Vault server so that we can create a vault client connection.
+	// This is run as part of Bootstrap.
 	SetupVaultClient(t *testing.T) *vapi.Client
+
+	// VaultClient returns the client that was built as part of SetupVaultClient.
 	VaultClient(t *testing.T) *vapi.Client
 }
 
@@ -97,6 +108,7 @@ func NewHelmCluster(
 	}
 }
 
+// VaultClient returns the vault client.
 func (v *VaultCluster) VaultClient(t *testing.T) *vapi.Client { return v.vaultClient }
 
 // checkForPriorInstallations checks if there is an existing Helm release
@@ -125,22 +137,19 @@ func (v *VaultCluster) checkForPriorVaultInstallations(t *testing.T) {
 	for _, r := range installedReleases {
 		require.NotContains(t, r["chart"], "vault", fmt.Sprintf("detected an existing installation of Vault %s, release name: %s", r["chart"], r["name"]))
 	}
-	/*
-		// TODO: Copied this from Consul Cluster implementation, is this necessary for vault?
-		// Wait for all pods in the "default" namespace to exit. A previous
-		// release may not be listed by Helm but its pods may still be terminating.
-		retry.RunWith(&retry.Counter{Wait: 1 * time.Second, Count: 60}, t, func(r *retry.R) {
-			consulPods, err := v.kubernetesClient.CoreV1().Pods(v.vaultHelmOptions.KubectlOptions.Namespace).List(context.Background(), metav1.ListOptions{})
-			require.NoError(r, err)
-			if len(consulPods.Items) > 0 {
-				var podNames []string
-				for _, p := range consulPods.Items {
-					podNames = append(podNames, p.Name)
-				}
-				r.Errorf("pods from previous installation still running: %s", strings.Join(podNames, ", "))
+	// Wait for all pods in the "default" namespace to exit. A previous
+	// release may not be listed by Helm but its pods may still be terminating.
+	retry.RunWith(&retry.Counter{Wait: 1 * time.Second, Count: 60}, t, func(r *retry.R) {
+		vaultPods, err := v.kubernetesClient.CoreV1().Pods(v.vaultHelmOptions.KubectlOptions.Namespace).List(context.Background(), metav1.ListOptions{})
+		require.NoError(r, err)
+		if len(vaultPods.Items) > 0 {
+			var podNames []string
+			for _, p := range vaultPods.Items {
+				podNames = append(podNames, p.Name)
 			}
-		})
-	*/
+			r.Errorf("pods from previous installation still running: %s", strings.Join(podNames, ", "))
+		}
+	})
 }
 
 // Setup Vault Client returns a Vault Client.
@@ -174,10 +183,9 @@ func (v *VaultCluster) SetupVaultClient(t *testing.T) *vapi.Client {
 	})
 
 	config.Address = fmt.Sprintf("http://127.0.0.1:%d", localPort)
-	consulClient, err := vapi.NewClient(config)
+	vaultClient, err := vapi.NewClient(config)
 	require.NoError(t, err)
-
-	return consulClient
+	return vaultClient
 }
 
 // Bootstrap runs Init, Unseals the Vault installation
@@ -217,7 +225,7 @@ func (v *VaultCluster) Bootstrap(t *testing.T, ctx environment.TestContext) {
 	if err != nil {
 		t.Fatal("unable to mount kv-v2 secrets engine", "err", err)
 	}
-	// TODO: Enable the PKI Secrets Engine
+	// TODO: add the PKI Secrets Engine when we have a need for it.
 
 	// Enable Kube Auth.
 	err = v.vaultClient.Sys().EnableAuthWithOptions("kubernetes", &vapi.EnableAuthOptions{
@@ -232,20 +240,7 @@ func (v *VaultCluster) Bootstrap(t *testing.T, ctx environment.TestContext) {
 	cmdString := fmt.Sprintf("VAULT_TOKEN=%s vault write auth/kubernetes/config token_reviewer_jwt=\"$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)\" kubernetes_host=\"https://${KUBERNETES_PORT_443_TCP_ADDR}:443\" kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt", v.rootToken)
 
 	v.logger.Logf(t, "updating vault kube auth config")
-	// TODO: figure out if this sleep can be done differently.
-	time.Sleep(time.Second * 30)
 	k8s.RunKubectl(t, ctx.KubectlOptions(t), "exec", "-i", fmt.Sprintf("%s-vault-0", v.vaultReleaseName), "--", "sh", "-c", cmdString)
-
-	// Create an access policy for Secrets Management, so we can create them later
-	/*
-		// TODO: Do we need this policy, because we will have to have root token (or some other priv'd) in our client
-		// TODO: already in order to create policies and roles.
-		devpolicyRule := `{ "policy": "path "consul/data/*" { capabilities = ["create", "update", "read"] } }`
-		err = v.vaultClient.Sys().PutPolicy("dev-root-policy", devpolicyRule)
-		if err != nil {
-			t.Fatal("unable to create secret mgmt policy", "err", err)
-		}
-	*/
 }
 
 // Create installs Vault via Helm.
@@ -284,10 +279,10 @@ func (v *VaultCluster) Destroy(t *testing.T) {
 	// always idempotently clean up resources in the cluster.
 	_ = helm.DeleteE(t, v.vaultHelmOptions, v.vaultReleaseName, false)
 	// Delete PVCs.
-	err := v.kubernetesClient.CoreV1().PersistentVolumeClaims(v.vaultHelmOptions.KubectlOptions.Namespace).DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: fmt.Sprintf("app.kubernetes.io/instance==data-%s-vault-0", v.vaultReleaseName)})
+	err := v.kubernetesClient.CoreV1().PersistentVolumeClaims(v.vaultHelmOptions.KubectlOptions.Namespace).DeleteCollection(context.Background(), metav1.DeleteOptions{}, metav1.ListOptions{LabelSelector: fmt.Sprintf("app.kubernetes.io/instance=%s", v.vaultReleaseName)})
 	require.NoError(t, err)
 
-	// Delete any secrets that have v.releaseName in their name.
+	// Delete any secrets that have v.releaseName in their name, this is only needed to delete the Helm release secret.
 	secrets, err := v.kubernetesClient.CoreV1().Secrets(v.vaultHelmOptions.KubectlOptions.Namespace).List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
 	for _, secret := range secrets.Items {
