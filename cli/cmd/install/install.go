5eaf12a874792ee623605558e35a3844dc822c01
David Yu <dyu@hashicorp.com>
2021-10-28 13:14:21 -0700

    cli: install - move not ready library log messages behind -verbose flag (#810)
    * cli: remove not ready library log messages
    
    * adding verbose flag and selectively output logs when verbose is not set
    
    * Update cli/cmd/install/install.go
    
    adding -v alias
    
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>
    
    * small comment change
    
    * adding changelog
    
    * Update cli/cmd/install/install.go
    
    Co-authored-by: Thomas Eckert <teckert@hashicorp.com>
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>
    Co-authored-by: Thomas Eckert <teckert@hashicorp.com>

--- cli/cmd/install/install.go
+++ cli/cmd/install/install.go
@@ -44,6 +44,9 @@ const (
 	flagNameTimeout = "timeout"
 	defaultTimeout  = "10m"
 
+	flagNameVerbose = "verbose"
+	defaultVerbose  = false
+
 	flagNameWait = "wait"
 	defaultWait  = true
 )
@@ -65,6 +68,7 @@ type Command struct {
 	flagFileValues      []string
 	flagTimeout         string
 	timeoutDuration     time.Duration
+	flagVerbose         bool
 	flagWait            bool
 
 	flagKubeConfig  string
@@ -136,6 +140,13 @@ func (c *Command) init() {
 		Usage:   "Timeout to wait for installation to be ready.",
 	})
 	f.BoolVar(&flag.BoolVar{
+		Name:    flagNameVerbose,
+		Aliases: []string{"v"},
+		Target:  &c.flagVerbose,
+		Default: defaultVerbose,
+		Usage:   "Output verbose logs from the install command with the status of resources being installed.",
+	})
+	f.BoolVar(&flag.BoolVar{
 		Name:    flagNameWait,
 		Target:  &c.flagWait,
 		Default: defaultWait,
@@ -192,7 +203,16 @@ func (c *Command) Run(args []string) int {
 	// Setup logger to stream Helm library logs
 	var uiLogger = func(s string, args ...interface{}) {
 		logMsg := fmt.Sprintf(s, args...)
-		c.UI.Output(logMsg, terminal.WithLibraryStyle())
+
+		if c.flagVerbose {
+			// Only output all logs when verbose is enabled
+			c.UI.Output(logMsg, terminal.WithLibraryStyle())
+		} else {
+			// When verbose is not enabled, output all logs except not ready messages for resources
+			if !strings.Contains(logMsg, "not ready") {
+				c.UI.Output(logMsg, terminal.WithLibraryStyle())
+			}
+		}
 	}
 
 	// Set up the kubernetes client to use for non Helm SDK calls to the Kubernetes API
