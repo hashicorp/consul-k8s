827c6a4a81c8690e4f2543a0cdcecbf6361049ec
NicoletaPopoviciu <nicoleta@hashicorp.com>
2021-11-05 11:57:17 -0400

    augment the uninstall tests w negative test

--- cli/cmd/uninstall/uninstall_test.go
+++ cli/cmd/uninstall/uninstall_test.go
@@ -53,6 +53,7 @@ func TestDeletePVCs(t *testing.T) {
 	pvcs, err := c.kubernetes.CoreV1().PersistentVolumeClaims("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
 	require.Len(t, pvcs.Items, 1)
+	require.Equal(t, pvcs.Items[0].Name, pvc3.Name)
 }
 
 func TestDeleteSecrets(t *testing.T) {
@@ -74,15 +75,26 @@ func TestDeleteSecrets(t *testing.T) {
 			},
 		},
 	}
+	secret3 := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "unrelated-test-secret3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.CoreV1().Secrets("default").Create(context.Background(), secret, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.CoreV1().Secrets("default").Create(context.Background(), secret2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.CoreV1().Secrets("default").Create(context.Background(), secret3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteSecrets("consul", "default")
 	require.NoError(t, err)
 	secrets, err := c.kubernetes.CoreV1().Secrets("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, secrets.Items, 0)
+	require.Len(t, secrets.Items, 1)
+	require.Equal(t, secrets.Items[0].Name, secret3.Name)
 }
 
 func TestDeleteServiceAccounts(t *testing.T) {
@@ -104,15 +116,26 @@ func TestDeleteServiceAccounts(t *testing.T) {
 			},
 		},
 	}
+	sa3 := &v1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-sa3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.CoreV1().ServiceAccounts("default").Create(context.Background(), sa, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.CoreV1().ServiceAccounts("default").Create(context.Background(), sa2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.CoreV1().ServiceAccounts("default").Create(context.Background(), sa3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteServiceAccounts("consul", "default")
 	require.NoError(t, err)
 	sas, err := c.kubernetes.CoreV1().ServiceAccounts("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, sas.Items, 0)
+	require.Len(t, sas.Items, 1)
+	require.Equal(t, sas.Items[0].Name, sa3.Name)
 }
 
 func TestDeleteRoles(t *testing.T) {
@@ -134,15 +157,26 @@ func TestDeleteRoles(t *testing.T) {
 			},
 		},
 	}
+	role3 := &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-role3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.RbacV1().Roles("default").Create(context.Background(), role, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.RbacV1().Roles("default").Create(context.Background(), role2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.RbacV1().Roles("default").Create(context.Background(), role3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteRoles("consul", "default")
 	require.NoError(t, err)
 	roles, err := c.kubernetes.RbacV1().Roles("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, roles.Items, 0)
+	require.Len(t, roles.Items, 1)
+	require.Equal(t, roles.Items[0].Name, role3.Name)
 }
 
 func TestDeleteRoleBindings(t *testing.T) {
@@ -164,15 +198,26 @@ func TestDeleteRoleBindings(t *testing.T) {
 			},
 		},
 	}
+	rolebinding3 := &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-role3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.RbacV1().RoleBindings("default").Create(context.Background(), rolebinding, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.RbacV1().RoleBindings("default").Create(context.Background(), rolebinding2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.RbacV1().RoleBindings("default").Create(context.Background(), rolebinding3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteRoleBindings("consul", "default")
 	require.NoError(t, err)
 	rolebindings, err := c.kubernetes.RbacV1().RoleBindings("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, rolebindings.Items, 0)
+	require.Len(t, rolebindings.Items, 1)
+	require.Equal(t, rolebindings.Items[0].Name, rolebinding3.Name)
 }
 
 func TestDeleteJobs(t *testing.T) {
@@ -194,15 +239,26 @@ func TestDeleteJobs(t *testing.T) {
 			},
 		},
 	}
+	job3 := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-job3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.BatchV1().Jobs("default").Create(context.Background(), job, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.BatchV1().Jobs("default").Create(context.Background(), job2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.BatchV1().Jobs("default").Create(context.Background(), job3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteJobs("consul", "default")
 	require.NoError(t, err)
 	jobs, err := c.kubernetes.BatchV1().Jobs("default").List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, jobs.Items, 0)
+	require.Len(t, jobs.Items, 1)
+	require.Equal(t, jobs.Items[0].Name, job3.Name)
 }
 
 func TestDeleteClusterRoles(t *testing.T) {
@@ -224,15 +280,26 @@ func TestDeleteClusterRoles(t *testing.T) {
 			},
 		},
 	}
+	clusterrole3 := &rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-clusterrole3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.RbacV1().ClusterRoles().Create(context.Background(), clusterrole, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.RbacV1().ClusterRoles().Create(context.Background(), clusterrole2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.RbacV1().ClusterRoles().Create(context.Background(), clusterrole3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteClusterRoles("consul")
 	require.NoError(t, err)
 	clusterroles, err := c.kubernetes.RbacV1().ClusterRoles().List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, clusterroles.Items, 0)
+	require.Len(t, clusterroles.Items, 1)
+	require.Equal(t, clusterroles.Items[0].Name, clusterrole3.Name)
 }
 
 func TestDeleteClusterRoleBindings(t *testing.T) {
@@ -254,15 +321,26 @@ func TestDeleteClusterRoleBindings(t *testing.T) {
 			},
 		},
 	}
+	clusterrolebinding3 := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "consul-test-clusterrolebinding3",
+			Labels: map[string]string{
+				"release": "unrelated",
+			},
+		},
+	}
 	_, err := c.kubernetes.RbacV1().ClusterRoleBindings().Create(context.Background(), clusterrolebinding, metav1.CreateOptions{})
 	require.NoError(t, err)
 	_, err = c.kubernetes.RbacV1().ClusterRoleBindings().Create(context.Background(), clusterrolebinding2, metav1.CreateOptions{})
+	require.NoError(t, err)
+	_, err = c.kubernetes.RbacV1().ClusterRoleBindings().Create(context.Background(), clusterrolebinding3, metav1.CreateOptions{})
 	require.NoError(t, err)
 	err = c.deleteClusterRoleBindings("consul")
 	require.NoError(t, err)
 	clusterrolebindings, err := c.kubernetes.RbacV1().ClusterRoleBindings().List(context.Background(), metav1.ListOptions{})
 	require.NoError(t, err)
-	require.Len(t, clusterrolebindings.Items, 0)
+	require.Len(t, clusterrolebindings.Items, 1)
+	require.Equal(t, clusterrolebindings.Items[0].Name, clusterrolebinding3.Name)
 }
 
 // getInitializedCommand sets up a command struct for tests.
