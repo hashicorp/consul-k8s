1918b48338861bf4d0b6f7712f039b82f4e447e4
Thomas Eckert <teckert@hashicorp.com>
2021-11-12 16:35:29 -0500

    connect-ignore -> service-ignore

--- control-plane/connect-inject/annotations.go
+++ control-plane/connect-inject/annotations.go
@@ -122,9 +122,9 @@ const (
 	// webhook/handler.
 	annotationOriginalPod = "consul.hashicorp.com/original-pod"
 
-	// labelConnectIgnore is a label that can be added to a service to prevent it from being
+	// labelServiceIgnore is a label that can be added to a service to prevent it from being
 	// registered with Consul.
-	labelConnectIgnore = "consul.hashicorp.com/connect-ignore"
+	labelServiceIgnore = "consul.hashicorp.com/service-ignore"
 
 	// injected is used as the annotation value for annotationInjected.
 	injected = "injected"
--- control-plane/connect-inject/endpoints_controller.go
+++ control-plane/connect-inject/endpoints_controller.go
@@ -149,7 +149,7 @@ func (r *EndpointsController) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	// If the endpoints object has the label "connect-ignore" set to true, deregister all instances in Consul for this service.
 	// It is possible that the endpoints object has never been registered, in which case deregistration is a no-op.
-	if value, ok := serviceEndpoints.Labels[labelConnectIgnore]; ok && value == "true" {
+	if value, ok := serviceEndpoints.Labels[labelServiceIgnore]; ok && value == "true" {
 		return r.deregisterServiceOnAllAgents(ctx, req.Name, req.Namespace, nil, endpointPods)
 	}
 
