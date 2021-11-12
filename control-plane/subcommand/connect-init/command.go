38bf002549952206c9df1d5515d8d0f7cd2e9a84
Thomas Eckert <teckert@hashicorp.com>
2021-11-12 12:43:40 -0500

    Add a warning in connect-init about running multiple services which point to the same pod.

--- control-plane/subcommand/connect-init/command.go
+++ control-plane/subcommand/connect-init/command.go
@@ -177,7 +177,12 @@ func (c *Command) Run(args []string) int {
 			// it is not "lost" to the user at the end of the retries when the pod enters a CrashLoop.
 			if registrationRetryCount%10 == 0 {
 				c.logger.Info("Check to ensure a Kubernetes service has been created for this application." +
-					" If your pod is not starting also check the connect-inject deployment logs.")
+					" If your pod is not starting also check the connect-inject deployment logs." +
+					" Note that only one service may be used to route requests to each pod." +
+					" If you need multiple services to point to the same pod, add the label" +
+					" `consul.hashicorp.com/connect-ignore: \"true\"` to services which should" +
+					" not be used by Consul for handling requests.")
+
 			}
 			return fmt.Errorf("did not find correct number of services: %d", len(serviceList))
 		}
