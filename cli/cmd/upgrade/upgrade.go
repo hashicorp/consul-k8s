71b53b574d4bbe62aaa35d1cd6aeb8e00277f212
Nitya Dhanushkodi <nitya@hashicorp.com>
2021-11-23 14:20:03 -0800

    notes from sync with Saad on what's left

--- cli/cmd/upgrade/upgrade.go
+++ cli/cmd/upgrade/upgrade.go
@@ -46,6 +46,9 @@ const (
 
 	flagNameWait = "wait"
 	defaultWait  = true
+	// action/upgrade
+	// --install, --reset-values vs --reuse-values,
+	// atomic
 )
 
 type Command struct {
@@ -248,6 +251,7 @@ func (c *Command) Run(args []string) int {
 	}
 
 	// Print out the upgrade summary.
+	// TODO: Fix this to show the diff between existing and proposed install rather than overrides.
 	if !c.flagAutoApprove {
 		c.UI.Output("Consul Upgrade Summary", terminal.WithHeaderStyle())
 		c.UI.Output("Installation name: %s", common.DefaultReleaseName, terminal.WithInfoStyle())
