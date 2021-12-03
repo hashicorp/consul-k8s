26a6667a3991c7eee75443b3e38f788bc380b45b
Thomas Eckert <teckert@hashicorp.com>
2021-12-03 17:24:54 -0500

    Remove duplicated MapMerge from git rebase

--- cli/cmd/upgrade/upgrade.go
+++ cli/cmd/upgrade/upgrade.go
@@ -305,11 +305,6 @@ func (c *Command) Run(args []string) int {
 		// newChart := common.MergeMaps(chart.Values, vals)
 	}
 
-	// Without informing the user, default global.name to consul if it hasn't been set already. We don't allow setting
-	// the release name, and since that is hardcoded to "consul", setting global.name to "consul" makes it so resources
-	// aren't double prefixed with "consul-consul-...".
-	vals = common.MergeMaps(config.Convert(config.GlobalNameConsul), vals)
-
 	if !c.flagAutoApprove && !c.flagDryRun {
 		confirmation, err := c.UI.Input(&terminal.Input{
 			Prompt: "Proceed with upgrade? (y/N)",
