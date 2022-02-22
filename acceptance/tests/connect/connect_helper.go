b2a62631120875cf1de0793daa98ea8398f6184a
Thomas Eckert <teckert@hashicorp.com>
2022-02-22 10:37:11 -0500

    Update acceptance/tests/connect/connect_helper.go
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>

--- acceptance/tests/connect/connect_helper.go
+++ acceptance/tests/connect/connect_helper.go
@@ -183,7 +183,7 @@ func (c *ConnectHelper) TestConnectionFailureWhenUnhealthy() {
 		k8s.CheckStaticServerConnectionMultipleFailureMessages(c.T, c.Ctx.KubectlOptions(c.T), staticClientName, false, []string{"curl: (56) Recv failure: Connection reset by peer", "curl: (52) Empty reply from server"}, "", "http://localhost:1234")
 	}
 
-	// Return the static-server to a "healthy state
+	// Return the static-server to a "healthy state".
 	k8s.RunKubectl(c.T, c.Ctx.KubectlOptions(c.T), "exec", "deploy/"+staticServerName, "--", "rm", "/tmp/unhealthy")
 }
 
