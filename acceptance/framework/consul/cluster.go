c3bd277f0de3041a479ff07b1aa589dc556b8d82
Thomas Eckert <teckert@hashicorp.com>
2022-03-02 15:06:20 -0500

    Smoke tests for the CLI `upgrade` command (#1019)
    * Add Upgrade command to cli_cluster
    * Change connect_helper to take configuration (setting up for the upgrade path)
    * Add the ConnectInject on upgrade test
    * Move client and cluster onto struct
    * AdditionalHelmValues instead of double accounting for helmValues
    * Fix autoencrypt getting set on upgrade test
    * Set `static-server` back to healthy
    * Move pod creation to Install method
    * Move MergeMaps to helpers
    * Move tests from CLI to connect
    * Merge mine and @ishustava's Upgrade impls
    * Move cluster interface to its own file
    * Refactor consul cluster code
    * Change tests to use cluster enum
    * Uncomment other install tests
    * Write out upgrade tests that don't restart the client
    * Abort upgrade if test has previously failed
    * Fix godot errors
    * s/True/NotNil/g on Upgrade
    * Pass t into methods and inline kustomize
    * Move secure check up to test level
    
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>

---
+++ acceptance/framework/consul/cluster.go
@@ -0,0 +1,32 @@
+package consul
+
+import (
+	"testing"
+
+	"github.com/hashicorp/consul/api"
+)
+
+// Cluster represents a consul cluster object.
+type Cluster interface {
+	// SetupConsulClient returns a new Consul client.
+	SetupConsulClient(t *testing.T, secure bool) *api.Client
+
+	// Create creates a new Consul Cluster.
+	Create(t *testing.T)
+
+	// Upgrade modifies the cluster in-place by merging the helm values
+	// from the initial install with helmValues. Any keys that were previously set
+	// will be overridden by the helmValues keys.
+	Upgrade(t *testing.T, helmValues map[string]string)
+
+	// Destroy destroys the cluster
+	Destroy(t *testing.T)
+}
+
+// ClusterKind represents the kind of Consul cluster being used (e.g. "Helm" or "CLI").
+type ClusterKind int
+
+const (
+	Helm ClusterKind = iota
+	CLI
+)
