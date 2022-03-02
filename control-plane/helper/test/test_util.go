c0dc0987e926f45bf790cd8904eb676e30f2222b
Kyle Schochenmaier <kschoche@gmail.com>
2022-03-02 13:10:56 -0600

    Use kube auth method to provision ACL token for the crd controller (#995)
    * Use a Consul Kubernetes Auth Method to issue consul-login to mint ACL tokens and consul-logout to clean them up for the CRD controller.
    
    Co-authored-by: Iryna Shustava <ishustava@users.noreply.github.com>

--- control-plane/helper/test/test_util.go
+++ control-plane/helper/test/test_util.go
@@ -15,6 +15,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	componentAuthMethod = "consul-k8s-component-auth-method"
+)
+
 // GenerateServerCerts generates Consul CA
 // and a server certificate and saves them to temp files.
 // It returns file names in this order:
@@ -55,6 +59,79 @@ func GenerateServerCerts(t *testing.T) (string, string, string) {
 		os.Remove(certKeyFile.Name())
 	})
 	return caFile.Name(), certFile.Name(), certKeyFile.Name()
+}
+
+// SetupK8sComponentAuthMethod creates a k8s auth method, sample "acl:write" ACL policy, Role and BindingRule
+// that allows a client using serviceAccount's JWT token to call "consul login".
+func SetupK8sComponentAuthMethod(t *testing.T, consulClient *api.Client, serviceAccountName, k8sComponentNS string) {
+	t.Helper()
+	// Start the mock k8s server.
+	k8sMockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		if r != nil && r.URL.Path == "/apis/authentication.k8s.io/v1/tokenreviews" && r.Method == "POST" {
+			w.Write([]byte(tokenReviewsResponse(serviceAccountName, k8sComponentNS)))
+		}
+		if r != nil && r.URL.Path == fmt.Sprintf("/api/v1/namespaces/%s/serviceaccounts/%s", k8sComponentNS, serviceAccountName) &&
+			r.Method == "GET" {
+			w.Write([]byte(serviceAccountGetResponse(serviceAccountName, k8sComponentNS)))
+		}
+	}))
+	t.Cleanup(k8sMockServer.Close)
+
+	// Set up Component's auth method.
+	authMethodTmpl := api.ACLAuthMethod{
+		Name:        componentAuthMethod,
+		Type:        "kubernetes",
+		Description: "Kubernetes Auth Method",
+		Config: map[string]interface{}{
+			"Host":              k8sMockServer.URL,
+			"CACert":            serviceAccountCACert,
+			"ServiceAccountJWT": ServiceAccountJWTToken,
+		},
+	}
+	// This API call will idempotently create the auth method (it won't fail if it already exists).
+	_, _, err := consulClient.ACL().AuthMethodCreate(&authMethodTmpl, nil)
+	require.NoError(t, err)
+
+	rules := `acl = "write"`
+	policyName := fmt.Sprintf("%s-token", serviceAccountName)
+	policy := api.ACLPolicy{
+		Name:        policyName,
+		Description: fmt.Sprintf("%s Token Policy", policyName),
+		Rules:       rules,
+		Datacenters: []string{"dc1"},
+	}
+	_, _, err = consulClient.ACL().PolicyCreate(&policy, &api.WriteOptions{})
+	require.NoError(t, err)
+
+	// Create the ACL Role, it requires an ACLRolePolicyLink which contains a list
+	// of ACL policies that are allowed to be fetched by an associated ACLBindingRule.
+	ap := &api.ACLRolePolicyLink{
+		Name: policyName,
+	}
+	apl := []*api.ACLRolePolicyLink{}
+	apl = append(apl, ap)
+	aclRoleName := fmt.Sprintf("%s-acl-role", serviceAccountName)
+	role := &api.ACLRole{
+		Name:        aclRoleName,
+		Description: fmt.Sprintf("ACL Role for %s", serviceAccountName),
+		Policies:    apl,
+	}
+	_, _, err = consulClient.ACL().RoleCreate(role, &api.WriteOptions{})
+	require.NoError(t, err)
+
+	// Create the ACLBindingRule, this specifies that a user using the AuthMethod
+	// is able to request an ACL Token with associated ACLRole from above via BindName
+	// as long as its serviceaccount matches the Selector.
+	abr := api.ACLBindingRule{
+		Description: fmt.Sprintf("Binding Rule for %s", serviceAccountName),
+		AuthMethod:  componentAuthMethod,
+		Selector:    fmt.Sprintf("serviceaccount.name==%q", serviceAccountName),
+		BindType:    api.BindingRuleBindTypeRole,
+		BindName:    aclRoleName,
+	}
+	_, _, err = consulClient.ACL().BindingRuleCreate(&abr, nil)
+	require.NoError(t, err)
 }
 
 // SetupK8sAuthMethod create a k8s auth method and a binding rule in Consul for the
