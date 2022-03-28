83339584ec8f0011da95276da213bc838f17bc59
Thomas Eckert <teckert@hashicorp.com>
2022-03-28 17:42:35 -0400

    Remove validations that aren't ready to be used yet

--- cli/validation/kubernetes.go
+++ cli/validation/kubernetes.go
@@ -2,8 +2,6 @@ package validation
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/hashicorp/consul-k8s/cli/common"
 	v1 "k8s.io/api/core/v1"
@@ -18,30 +16,4 @@ func ListConsulSecrets(ctx context.Context, client kubernetes.Interface) (*v1.Se
 	})
 
 	return secrets, err
-}
-
-// ListConsulPVCs attempts to find PVCs which have "consul-server" in the name.
-func ListConsulPVCs(ctx context.Context, client kubernetes.Interface) ([]v1.PersistentVolumeClaim, error) {
-	pvcs, err := client.CoreV1().PersistentVolumeClaims(v1.NamespaceDefault).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("error listing persistent volume claims: %s", err)
-	}
-
-	var consulPvcs []v1.PersistentVolumeClaim
-	for _, pvc := range pvcs.Items {
-		if strings.Contains(pvc.Name, "consul-server") {
-			consulPvcs = append(consulPvcs, pvc)
-		}
-	}
-
-	return consulPvcs, nil
-}
-
-// IsValidEnterprise checks if the Consul Enterprise secret is set.
-func IsValidEnterprise(ctx context.Context, client kubernetes.Interface, namespace, secretName string) (bool, error) {
-	_, err := client.CoreV1().Secrets(namespace).Get(ctx, secretName, metav1.GetOptions{})
-	if err != nil {
-		return false, err
-	}
-	return true, nil
 }
