143b28d94c7a5e5a73b204ee435235eb93d3d67f
Thomas Eckert <teckert@hashicorp.com>
2022-03-29 13:13:17 -0400

    Use string template in place of concatenation

--- cli/validation/kubernetes.go
+++ cli/validation/kubernetes.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/consul-k8s/cli/common"
 	v1 "k8s.io/api/core/v1"
@@ -12,7 +13,7 @@ import (
 // ListConsulSecrets attempts to find secrets with the Consul label.
 func ListConsulSecrets(ctx context.Context, client kubernetes.Interface) (*v1.SecretList, error) {
 	secrets, err := client.CoreV1().Secrets("").List(ctx, metav1.ListOptions{
-		LabelSelector: common.CLILabelKey + "=" + common.CLILabelValue,
+		LabelSelector: fmt.Sprintf("%s=%s", common.CLILabelKey, common.CLILabelValue),
 	})
 
 	return secrets, err
