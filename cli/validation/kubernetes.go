81be6cf676f8eec072b3e8367ab8c26b6c65e7ff
Thomas Eckert <teckert@hashicorp.com>
2022-04-08 14:46:15 -0400

    Only lookup secrets in the namespace where the install is targeting.

--- cli/validation/kubernetes.go
+++ cli/validation/kubernetes.go
@@ -11,8 +11,8 @@ import (
 )
 
 // ListConsulSecrets attempts to find secrets with the Consul label.
-func ListConsulSecrets(ctx context.Context, client kubernetes.Interface) (*v1.SecretList, error) {
-	secrets, err := client.CoreV1().Secrets("").List(ctx, metav1.ListOptions{
+func ListConsulSecrets(ctx context.Context, client kubernetes.Interface, namespace string) (*v1.SecretList, error) {
+	secrets, err := client.CoreV1().Secrets(namespace).List(ctx, metav1.ListOptions{
 		LabelSelector: fmt.Sprintf("%s=%s", common.CLILabelKey, common.CLILabelValue),
 	})
 
