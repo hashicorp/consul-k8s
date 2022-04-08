03804f20482e509ec938a5928fc6322a005bb340
Thomas Eckert <teckert@hashicorp.com>
2022-04-08 12:36:43 -0400

    Get federation secret from any of the places it is set.

--- cli/release/release.go
+++ cli/release/release.go
@@ -23,3 +23,27 @@ func (r *Release) ShouldExpectFederationSecret() bool {
 		r.Configuration.Global.Datacenter != r.Configuration.Global.Federation.PrimaryDatacenter &&
 		!r.Configuration.Global.Federation.CreateFederationSecret
 }
+
+// FedSecret fetches the federation secret for the release or an empty string
+// if the federation secret is not set. The federation secret could be set
+// in several places in the configuration. This method returns the first
+// secret name that is set.
+func (r *Release) FedSecret() string {
+	if s := r.Configuration.Global.TLS.CaCert.SecretName; s != "" {
+		return s
+	}
+
+	if s := r.Configuration.Global.TLS.CaKey.SecretName; s != "" {
+		return s
+	}
+
+	if s := r.Configuration.Global.Acls.ReplicationToken.SecretName; s != "" {
+		return s
+	}
+
+	if s := r.Configuration.Global.GossipEncryption.SecretName; s != "" {
+		return s
+	}
+
+	return ""
+}
--- cli/release/release_test.go
+++ cli/release/release_test.go
@@ -61,3 +61,75 @@ func TestShouldExpectFederationSecret(t *testing.T) {
 		})
 	}
 }
+
+func TestFedSecret(t *testing.T) {
+	t.Parallel()
+
+	cases := map[string]struct {
+		configuration helm.Values
+		expected      string
+	}{
+		"No Fed Secret": {
+			configuration: helm.Values{},
+			expected:      "",
+		},
+		"Secret in Global.TLS.CaCert.SecretName": {
+			configuration: helm.Values{
+				Global: helm.Global{
+					TLS: helm.TLS{
+						CaCert: helm.CaCert{
+							SecretName: "secret-name",
+						},
+					},
+				},
+			},
+			expected: "secret-name",
+		},
+		"Secret in Global.TLS.CaKey.SecretName": {
+			configuration: helm.Values{
+				Global: helm.Global{
+					TLS: helm.TLS{
+						CaKey: helm.CaKey{
+							SecretName: "secret-name",
+						},
+					},
+				},
+			},
+			expected: "secret-name",
+		},
+		"Secret in Global.Acls.ReplicationToken.SecretName": {
+			configuration: helm.Values{
+				Global: helm.Global{
+					Acls: helm.Acls{
+						ReplicationToken: helm.ReplicationToken{
+							SecretName: "secret-name",
+						},
+					},
+				},
+			},
+			expected: "secret-name",
+		},
+		"Secret in Global.GossipEncryption.SecretName": {
+			configuration: helm.Values{
+				Global: helm.Global{
+					GossipEncryption: helm.GossipEncryption{
+						SecretName: "secret-name",
+					},
+				},
+			},
+			expected: "secret-name",
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			release := Release{
+				Configuration: tc.configuration,
+			}
+
+			actual := release.FedSecret()
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+
+}
