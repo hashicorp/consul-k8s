a6ea7546e06ee755eec52e42fa6cccd21ff41322
John Murret <john.murret@hashicorp.com>
2022-04-26 22:45:32 -0600

    Adding logic to tests for controller tls cert.  also adding snapshot agent and vault namespaces tests

--- acceptance/tests/snapshot-agent/snapshot_agent_vault_test.go
+++ acceptance/tests/snapshot-agent/snapshot_agent_vault_test.go
@@ -177,6 +177,13 @@ func TestSnapshotAgent_Vault(t *testing.T) {
 	}
 	saAuthRoleConfig.ConfigureK8SAuthRole(t, vaultClient)
 
+	pathForConnectInjectWebookCerts :=
+		vault.ConfigurePKICertificatesForConnectInjectWebhook(t, vaultClient,
+			consulReleaseName, ns, "dc1", "1h")
+	pathForControllerWebookCerts :=
+		vault.ConfigurePKICertificatesForControllerWebhook(t, vaultClient,
+			consulReleaseName, ns, "dc1", "1h")
+
 	vaultCASecret := vault.CASecretName(vaultReleaseName)
 
 	consulHelmValues := map[string]string{
@@ -184,9 +191,11 @@ func TestSnapshotAgent_Vault(t *testing.T) {
 		"server.extraVolumes[0].name": vaultCASecret,
 		"server.extraVolumes[0].load": "false",
 
-		"connectInject.enabled":  "true",
-		"connectInject.replicas": "1",
-		"controller.enabled":     "true",
+		"connectInject.enabled":            "true",
+		"connectInject.replicas":           "1",
+		"connectInject.tlsCert.secretName": pathForConnectInjectWebookCerts,
+		"controller.enabled":               "true",
+		"controller.tlsCert.secretName":    pathForControllerWebookCerts,
 
 		"global.secretsBackend.vault.enabled":              "true",
 		"global.secretsBackend.vault.consulServerRole":     consulServerRole,
