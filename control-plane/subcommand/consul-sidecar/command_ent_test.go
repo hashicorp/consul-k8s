c701e71b05ae8b356cc42db31b83c4bf0798c855
John Murret <john.murret@hashicorp.com>
2022-04-28 21:50:11 -0600

    fixing enterprise tests

--- control-plane/subcommand/consul-sidecar/command_ent_test.go
+++ control-plane/subcommand/consul-sidecar/command_ent_test.go
@@ -34,6 +34,7 @@ func TestRun_ServicesRegistration_Namespaces(t *testing.T) {
 		"-http-addr", a.HTTPAddr,
 		"-service-config", configFile,
 		"-sync-period", "100ms",
+		"-cconsul-api-timeout", "5"
 	})
 	defer stopCommand(t, &cmd, exitChan)
 
