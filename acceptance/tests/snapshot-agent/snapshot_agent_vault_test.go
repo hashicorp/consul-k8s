69688cbb93b4d6bc56b1d60343ebc081473b49c4
John Murret <john.murret@hashicorp.com>
2022-05-10 10:37:27 -0600

    fixing vault namespaces and snapshot agent on vault acceptance tests

--- acceptance/tests/snapshot-agent/snapshot_agent_vault_test.go
+++ acceptance/tests/snapshot-agent/snapshot_agent_vault_test.go
@@ -177,12 +177,12 @@ func TestSnapshotAgent_Vault(t *testing.T) {
 	}
 	saAuthRoleConfig.ConfigureK8SAuthRole(t, vaultClient)
 
-	pathForConnectInjectWebookCerts :=
-		vault.ConfigurePKICertificatesForConnectInjectWebhook(t, vaultClient,
-			consulReleaseName, ns, "dc1", "1h")
-	pathForControllerWebookCerts :=
-		vault.ConfigurePKICertificatesForControllerWebhook(t, vaultClient,
-			consulReleaseName, ns, "dc1", "1h")
+	// pathForConnectInjectWebookCerts :=
+	// 	vault.ConfigurePKICertificatesForConnectInjectWebhook(t, vaultClient,
+	// 		consulReleaseName, ns, "dc1", "1h")
+	// pathForControllerWebookCerts :=
+	// 	vault.ConfigurePKICertificatesForControllerWebhook(t, vaultClient,
+	// 		consulReleaseName, ns, "dc1", "1h")
 
 	vaultCASecret := vault.CASecretName(vaultReleaseName)
 
@@ -191,11 +191,11 @@ func TestSnapshotAgent_Vault(t *testing.T) {
 		"server.extraVolumes[0].name": vaultCASecret,
 		"server.extraVolumes[0].load": "false",
 
-		"connectInject.enabled":                                 "true",
-		"connectInject.replicas":                                "1",
-		"secretsBackend.vault.connectInject.tlsCert.secretName": pathForConnectInjectWebookCerts,
-		"controller.enabled":                                    "true",
-		"secretsBackend.vault.controller.tlsCert.secretName":    pathForControllerWebookCerts,
+		"connectInject.enabled":  "true",
+		"connectInject.replicas": "1",
+		// "secretsBackend.vault.connectInject.tlsCert.secretName": pathForConnectInjectWebookCerts,
+		"controller.enabled": "true",
+		// "secretsBackend.vault.controller.tlsCert.secretName":    pathForControllerWebookCerts,
 
 		"global.secretsBackend.vault.enabled":              "true",
 		"global.secretsBackend.vault.consulServerRole":     consulServerRole,
