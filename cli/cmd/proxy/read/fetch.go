b0b4c5fce0ed6cee3074262b77c8bbd1a136f12b
Thomas Eckert <teckert@hashicorp.com>
2022-06-13 14:14:57 -0400

    Add FetchConfig

--- cli/cmd/proxy/read/fetch.go
+++ cli/cmd/proxy/read/fetch.go
@@ -2,10 +2,27 @@ package read
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net/http"
 
-	"k8s.io/client-go/kubernetes"
+	"github.com/hashicorp/consul-k8s/cli/common"
 )
 
-func FetchConfig(ctx context.Context, client kubernetes.Interface, namespace, podName string) (interface{}, error) {
-	return nil, nil
+// FetchConfig opens a port forward to the Envoy admin API and fetches the
+// configuration from the config dump endpoint.
+func FetchConfig(ctx context.Context, portForward common.PortForwarder) ([]byte, error) {
+	endpoint, err := portForward.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer portForward.Close()
+
+	response, err := http.Get(fmt.Sprintf("http://%s/config_dump?include_eds", endpoint))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	return io.ReadAll(response.Body)
 }
