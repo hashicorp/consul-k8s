957b85df0a509c46f3a326cadefeab2b4c0750a8
Thomas Eckert <teckert@hashicorp.com>
2022-06-15 09:00:52 -0400

    Fill out the config sections and stub out parsers

--- cli/cmd/proxy/read/config.go
+++ cli/cmd/proxy/read/config.go
@@ -6,31 +6,25 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
-	adminv3 "github.com/envoyproxy/go-control-plane/envoy/admin/v3"
-	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	"github.com/hashicorp/consul-k8s/cli/common"
-	any "google.golang.org/protobuf/types/known/anypb"
-)
-
-type Section string
-
-// Define the different types of sections in the Envoy config.
-const (
-	Clusters  Section = "type.googleapis.com/envoy.admin.v3.ClustersConfigDump"
-	Endpoints         = "type.googleapis.com/envoy.admin.v3.EndpointsConfigDump"
-	Listeners         = "type.googleapis.com/envoy.admin.v3.ListenersConfigDump"
-	Routes            = "type.googleapis.com/envoy.admin.v3.RoutesConfigDump"
-	Secrets           = "type.googleapis.com/envoy.admin.v3.SecretsConfigDump"
 )
 
 // EnvoyConfig represents the configuration retrieved from a config dump at the
 // admin endpoint. It wraps the Envoy ConfigDump struct to give us convenient
 // access to the different sections of the config.
 type EnvoyConfig struct {
-	configDump *adminv3.ConfigDump
+	rawCfg            []byte
+	Clusters          []Cluster
+	Endpoints         []Endpoint
+	InboundListeners  []InboundListener
+	OutboundListeners []OutboundListener
+	Routes            []Route
+	Secrets           []Secret
 }
 
+// Cluster represents a cluster in the Envoy config.
 type Cluster struct {
 	Name                     string
 	FullyQualifiedDomainName string
@@ -39,16 +33,42 @@ type Cluster struct {
 	LastUpdated              string
 }
 
-// NewEnvoyConfig creates a new EnvoyConfig from the raw bytes returned from the
-// config dump endpoint.
-func NewEnvoyConfig(cfg []byte) (*EnvoyConfig, error) {
-	envoyConfig := &EnvoyConfig{}
-	err := json.Unmarshal(cfg, envoyConfig)
-	if err != nil {
-		return nil, err
-	}
+type Endpoint struct {
+	Address string
+	Cluster string
+	Weight  float64
+	Status  string
+}
 
-	return envoyConfig, nil
+type InboundListener struct {
+	Name               string
+	Address            string
+	Filter             string
+	DestinationCluster string
+	LastUpdated        string
+}
+
+type OutboundListener struct {
+	Name               string
+	Address            string
+	FilterChainMatch   string
+	DestinationCluster string
+	LastUpdated        string
+}
+
+type Route struct {
+	Name               string
+	DestinationCluster string
+	LastUpdated        string
+}
+
+type Secret struct {
+	Name      string
+	Type      string
+	Status    string
+	Valid     bool
+	ValidFrom string
+	ValidTo   string
 }
 
 // FetchConfig opens a port forward to the Envoy admin API and fetches the
@@ -71,70 +91,128 @@ func FetchConfig(ctx context.Context, portForward common.PortForwarder) (*EnvoyC
 		return nil, err
 	}
 
-	return NewEnvoyConfig(raw)
-}
-
-// Clusters returns a list of clusters in the Envoy config.
-func (c *EnvoyConfig) Clusters() ([]Cluster, error) {
-	// Get the clusters section of the config dump.
-	clustersSect, err := c.section(Clusters)
+	envoyConfig := &EnvoyConfig{}
+	err = json.Unmarshal(raw, envoyConfig)
 	if err != nil {
 		return nil, err
 	}
-
-	// Unmarshal the clusterDump section into a ClustersConfigDump.
-	clusterDump := &adminv3.ClustersConfigDump{}
-	err = clustersSect.UnmarshalTo(clusterDump)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a list of clusters and populate it with Cluster data from the dump.
-	var clusters []Cluster
-	for _, dynamicActiveCluster := range clusterDump.GetDynamicActiveClusters() {
-		cluster := &cluster.Cluster{}
-		err = dynamicActiveCluster.Cluster.UnmarshalTo(cluster)
-		if err != nil {
-			return nil, err
-		}
-
-		clusters = append(clusters, Cluster{
-			Name:                     cluster.Name,
-			FullyQualifiedDomainName: cluster.Name,
-			Endpoints:                []string{},
-			Type:                     cluster.GetClusterType().Name,
-			LastUpdated:              dynamicActiveCluster.LastUpdated.String(),
-		})
-	}
-
-	return clusters, nil
+	return envoyConfig, nil
 }
 
 // UnmarshalJSON unmarshals the raw config dump bytes into EnvoyConfig.
 func (c *EnvoyConfig) UnmarshalJSON(b []byte) error {
-	configDump := &adminv3.ConfigDump{}
-	err := json.Unmarshal(b, configDump)
-	*c = EnvoyConfig{configDump: configDump}
+	// Save the original config dump bytes for marshalling. We should treat this
+	// struct as immutable so this should be safe.
+	c.rawCfg = b
+
+	var root map[string]interface{}
+	err := json.Unmarshal(b, &root)
+
+	// Dispatch each section to the appropriate parsing function by its type.
+	for _, config := range root["configs"].([]interface{}) {
+		switch config.(map[string]interface{})["@type"].(string) {
+		case "type.googleapis.com/envoy.admin.v3.ClustersConfigDump":
+			clusters, err := parseClusters(config.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			c.Clusters = clusters
+
+		case "type.googleapis.com/envoy.admin.v3.EndpointsConfigDump":
+			endpoints, err := parseEndpoints(config.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			c.Endpoints = endpoints
+		case "type.googleapis.com/envoy.admin.v3.ListenersConfigDump":
+			inbounds, outbounds, err := parseListeners(config.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			c.InboundListeners = inbounds
+			c.OutboundListeners = outbounds
+		case "type.googleapis.com/envoy.admin.v3.RoutesConfigDump":
+			routes, err := parseRoutes(config.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			c.Routes = routes
+		case "type.googleapis.com/envoy.admin.v3.SecretsConfigDump":
+			secrets, err := parseSecrets(config.(map[string]interface{}))
+			if err != nil {
+				return err
+			}
+			c.Secrets = secrets
+		}
+	}
+
 	return err
 }
 
 // MarshalJSON marshals the EnvoyConfig into the raw config dump bytes.
 func (c *EnvoyConfig) MarshalJSON() ([]byte, error) {
-	return json.Marshal(c.configDump)
+	return c.rawCfg, nil
 }
 
-func (c *EnvoyConfig) section(section Section) (*any.Any, error) {
-	var sect *any.Any
-	for _, s := range c.configDump.Configs {
-		if s.TypeUrl == string(section) {
-			sect = s
-			break
+func parseClusters(rawCfg map[string]interface{}) ([]Cluster, error) {
+	var clusters []Cluster
+
+	static := rawCfg["static_clusters"].([]interface{})
+	dynamic := rawCfg["dynamic_active_clusters"].([]interface{})
+
+	for _, cluster := range append(static, dynamic...) {
+		fqdn := cluster.(map[string]interface{})["cluster"].(map[string]interface{})["name"].(string)
+		name := strings.Split(fqdn, ".")[0]
+		ctype := cluster.(map[string]interface{})["cluster"].(map[string]interface{})["type"].(string)
+		lastupdated := cluster.(map[string]interface{})["last_updated"].(string)
+
+		var endpoints []string
+		if cluster.(map[string]interface{})["cluster"].(map[string]interface{})["load_assignment"] != nil {
+			for _, endpoint := range cluster.(map[string]interface{})["cluster"].(map[string]interface{})["load_assignment"].(map[string]interface{})["endpoints"].([]interface{}) {
+				lbEndpoints := endpoint.(map[string]interface{})["lb_endpoints"]
+				for _, lbEndpoint := range lbEndpoints.([]interface{}) {
+					sockaddr := lbEndpoint.(map[string]interface{})["endpoint"].(map[string]interface{})["address"].(map[string]interface{})["socket_address"].(map[string]interface{})
+					address := sockaddr["address"].(string)
+					port := sockaddr["port_value"].(float64)
+					endpoints = append(endpoints, fmt.Sprintf("%s:%d", address, int(port)))
+				}
+			}
 		}
+
+		cluster := Cluster{
+			Name:                     name,
+			FullyQualifiedDomainName: fqdn,
+			Endpoints:                endpoints,
+			Type:                     ctype,
+			LastUpdated:              lastupdated,
+		}
+
+		clusters = append(clusters, cluster)
 	}
 
-	if sect == nil {
-		return nil, fmt.Errorf("ConfigDump section %s not found", section)
-	}
+	return clusters, nil
+}
 
-	return sect, nil
+func parseEndpoints(rawCfg map[string]interface{}) ([]Endpoint, error) {
+	var endpoints []Endpoint
+
+	return endpoints, nil
+}
+
+func parseListeners(rawCfg map[string]interface{}) ([]InboundListener, []OutboundListener, error) {
+	inbounds, outbounds := []InboundListener{}, []OutboundListener{}
+
+	return inbounds, outbounds, nil
+}
+
+func parseRoutes(rawCfg map[string]interface{}) ([]Route, error) {
+	var routes []Route
+
+	return routes, nil
+}
+
+func parseSecrets(rawCfg map[string]interface{}) ([]Secret, error) {
+	var secrets []Secret
+
+	return secrets, nil
 }
