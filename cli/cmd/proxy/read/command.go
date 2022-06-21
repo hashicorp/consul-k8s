d80d5bc1570819a67c4a564ebc0bf54ca0b7f56b
Thomas Eckert <teckert@hashicorp.com>
2022-06-20 20:44:17 -0400

    Add filtering flags and fix issue with RestConfig

--- cli/cmd/proxy/read/command.go
+++ cli/cmd/proxy/read/command.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/consul-k8s/cli/common/terminal"
 	helmCLI "helm.sh/helm/v3/pkg/cli"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // adminPort is the port where the Envoy admin API is exposed.
@@ -28,11 +29,20 @@ type ReadCommand struct {
 	flagPodName   string
 	flagJSON      bool
 
+	// Output Filtering Opts
+	flagClusters  bool
+	flagListeners bool
+	flagRoutes    bool
+	flagEndpoints bool
+	flagSecrets   bool
+
 	// Global Flags
 	flagKubeConfig  string
 	flagKubeContext string
 
 	fetchConfig func(context.Context, common.PortForwarder) (*EnvoyConfig, error)
+
+	restConfig *rest.Config
 
 	once sync.Once
 	help string
@@ -48,7 +58,7 @@ func (c *ReadCommand) init() {
 	f.StringVar(&flag.StringVar{
 		Name:    "namespace",
 		Target:  &c.flagNamespace,
-		Usage:   "The namespace to list proxies in.",
+		Usage:   "The namespace where the target Pod can be found.",
 		Aliases: []string{"n"},
 	})
 	f.BoolVar(&flag.BoolVar{
@@ -56,6 +66,33 @@ func (c *ReadCommand) init() {
 		Target:  &c.flagJSON,
 		Default: false,
 		Usage:   "Output the whole Envoy Config as JSON.",
+	})
+
+	f = c.set.NewSet("Output Filtering Options")
+	f.BoolVar(&flag.BoolVar{
+		Name:   "clusters",
+		Target: &c.flagClusters,
+		Usage:  "Filter output to only show clusters.",
+	})
+	f.BoolVar(&flag.BoolVar{
+		Name:   "listeners",
+		Target: &c.flagListeners,
+		Usage:  "Filter output to only show listeners.",
+	})
+	f.BoolVar(&flag.BoolVar{
+		Name:   "routes",
+		Target: &c.flagRoutes,
+		Usage:  "Filter output to only show routes.",
+	})
+	f.BoolVar(&flag.BoolVar{
+		Name:   "endpoints",
+		Target: &c.flagEndpoints,
+		Usage:  "Filter output to only show endpoints.",
+	})
+	f.BoolVar(&flag.BoolVar{
+		Name:   "secrets",
+		Target: &c.flagSecrets,
+		Usage:  "Filter output to only show secrets.",
 	})
 
 	f = c.set.NewSet("GlobalOptions")
@@ -72,7 +109,6 @@ func (c *ReadCommand) init() {
 	})
 
 	c.help = c.set.Help()
-
 	c.Init()
 }
 
@@ -81,39 +117,29 @@ func (c *ReadCommand) Run(args []string) int {
 	c.Log.ResetNamed("read")
 	defer common.CloseWithError(c.BaseCommand)
 
-	if err := c.set.Parse(args); err != nil {
+	if err := c.parseFlags(args); err != nil {
 		c.UI.Output(err.Error(), terminal.WithErrorStyle())
-		return 1
-	}
-
-	if arguments := c.set.Args(); len(arguments) > 0 {
-		c.flagPodName = arguments[0]
-	}
-
-	if c.flagPodName == "" {
-		c.UI.Output("Missing pod name.")
+		c.UI.Output("\n" + c.Help())
 		return 1
 	}
 
 	if err := c.validateFlags(); err != nil {
 		c.UI.Output(err.Error(), terminal.WithErrorStyle())
+		c.UI.Output("\n" + c.Help())
 		return 1
 	}
 
-	if c.kubernetes == nil {
-		if err := c.initKubernetes(); err != nil {
-			c.UI.Output(err.Error(), terminal.WithErrorStyle())
-			return 1
-		}
+	if err := c.initKubernetes(); err != nil {
+		c.UI.Output(err.Error(), terminal.WithErrorStyle())
+		return 1
 	}
 
 	pf := common.PortForward{
-		Namespace:   c.flagNamespace,
-		PodName:     c.flagPodName,
-		RemotePort:  adminPort,
-		KubeClient:  c.kubernetes,
-		KubeConfig:  c.flagKubeConfig,
-		KubeContext: c.flagKubeContext,
+		Namespace:  c.flagNamespace,
+		PodName:    c.flagPodName,
+		RemotePort: adminPort,
+		KubeClient: c.kubernetes,
+		RestConfig: c.restConfig,
 	}
 
 	config, err := c.fetchConfig(c.Ctx, &pf)
@@ -127,12 +153,36 @@ func (c *ReadCommand) Run(args []string) int {
 	return 0
 }
 
-func (c *ReadCommand) Synopsis() string {
-	return ""
+func (c *ReadCommand) Help() string {
+	c.once.Do(c.init)
+	return fmt.Sprintf("%s\n\nUsage: consul-k8s proxy read <pod-name> [flags]\n\n%s", c.Synopsis(), c.help)
 }
 
-func (c *ReadCommand) Help() string {
-	return ""
+func (c *ReadCommand) Synopsis() string {
+	return "Print the Envoy configuration for a given Pod."
+}
+
+func (c *ReadCommand) parseFlags(args []string) error {
+	// Separate positional arguments from keyed arguments.
+	positional := []string{}
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "-") {
+			break
+		}
+		positional = append(positional, arg)
+	}
+	keyed := args[len(positional):]
+
+	if len(positional) != 1 {
+		return fmt.Errorf("Exactly one positional argument is required: <pod-name>")
+	}
+	c.flagPodName = positional[0]
+
+	if err := c.set.Parse(keyed); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *ReadCommand) validateFlags() error {
@@ -142,19 +192,25 @@ func (c *ReadCommand) validateFlags() error {
 func (c *ReadCommand) initKubernetes() error {
 	settings := helmCLI.New()
 
-	restConfig, err := settings.RESTClientGetter().ToRESTConfig()
-	if err != nil {
-		return fmt.Errorf("error retrieving Kubernetes authentication %v", err)
-	}
-	if c.kubernetes, err = kubernetes.NewForConfig(restConfig); err != nil {
-		return fmt.Errorf("error creating Kubernetes client %v", err)
-	}
 	if c.flagKubeConfig == "" {
-		c.flagKubeConfig = settings.KubeConfig
+		settings.KubeConfig = c.flagKubeConfig
 	}
+
 	if c.flagKubeContext == "" {
-		c.flagKubeContext = settings.KubeContext
+		settings.KubeContext = c.flagKubeContext
 	}
+
+	if c.kubernetes == nil {
+		var err error
+		c.restConfig, err = settings.RESTClientGetter().ToRESTConfig()
+		if err != nil {
+			return fmt.Errorf("error retrieving Kubernetes authentication %v", err)
+		}
+		if c.kubernetes, err = kubernetes.NewForConfig(c.restConfig); err != nil {
+			return fmt.Errorf("error creating Kubernetes client %v", err)
+		}
+	}
+
 	if c.flagNamespace == "" {
 		c.flagNamespace = settings.Namespace()
 	}
@@ -168,16 +224,37 @@ func (c *ReadCommand) outputConfig(config *EnvoyConfig) {
 		return
 	}
 
-	c.UI.Output("Clusters:")
-	clusters := terminal.NewTable("Name", "FQDN", "Endpoints", "Type", "Last Updated")
-	for _, cluster := range config.Clusters {
-		clusters.AddRow([]string{cluster.Name, cluster.FullyQualifiedDomainName, strings.Join(cluster.Endpoints, ", "),
-			cluster.Type, cluster.LastUpdated}, []string{})
+	// Track if any filters are passed in. If not, print everything; if so, only
+	// print the filters that are passed in.
+	filtersPassed := c.flagClusters || c.flagListeners || c.flagRoutes || c.flagEndpoints || c.flagSecrets
+
+	if !filtersPassed || c.flagClusters {
+		c.UI.Output("Clusters", terminal.WithHeaderStyle())
+		clusters := terminal.NewTable("Name", "FQDN", "Endpoints", "Type", "Last Updated")
+		for _, cluster := range config.Clusters {
+			clusters.AddRow([]string{cluster.Name, cluster.FullyQualifiedDomainName, strings.Join(cluster.Endpoints, ", "),
+				cluster.Type, cluster.LastUpdated}, []string{})
+		}
+		c.UI.Table(clusters)
+		c.UI.Output("")
 	}
 
-	c.UI.Output("Endpoints:")
-	endpoints := terminal.NewTable("Endpoint", "Cluster", "Weight", "Status")
-	for _, endpoint := range config.Endpoints {
-		endpoints.AddRow([]string{endpoint.Address, endpoint.Cluster, fmt.Sprintf("%f", endpoint.Weight), endpoint.Status}, []string{})
+	if !filtersPassed || c.flagEndpoints {
+		c.UI.Output("Endpoints", terminal.WithHeaderStyle())
+		endpoints := terminal.NewTable("Endpoint", "Cluster", "Weight", "Status")
+		for _, endpoint := range config.Endpoints {
+			var statusColor string
+			if endpoint.Status == "HEALTHY" {
+				statusColor = "green"
+			} else {
+				statusColor = "red"
+			}
+
+			endpoints.AddRow(
+				[]string{endpoint.Address, endpoint.Cluster, fmt.Sprintf("%f", endpoint.Weight), endpoint.Status},
+				[]string{"", "", "", statusColor})
+		}
+		c.UI.Table(endpoints)
+		c.UI.Output("")
 	}
 }
