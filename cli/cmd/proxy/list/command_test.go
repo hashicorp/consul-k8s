18bc8113e2521ee0392aa43fe7c23ffc8cdd15cf
Thomas Eckert <teckert@hashicorp.com>
2022-07-11 09:30:18 -0400

    Test end-to-end for list command

--- cli/cmd/proxy/list/command_test.go
+++ cli/cmd/proxy/list/command_test.go
@@ -1,10 +1,13 @@
 package list
 
 import (
+	"bytes"
+	"context"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/consul-k8s/cli/common"
+	"github.com/hashicorp/consul-k8s/cli/common/terminal"
 	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/core/v1"
@@ -13,7 +16,7 @@ import (
 )
 
 // getInitializedCommand sets up a command struct for tests.
-func getInitializedCommand(t *testing.T) *ListCommand {
+func getInitializedCommand(t *testing.T) (*ListCommand, *bytes.Buffer) {
 	t.Helper()
 	log := hclog.New(&hclog.LoggerOptions{
 		Name:   "test",
@@ -21,15 +24,17 @@ func getInitializedCommand(t *testing.T) *ListCommand {
 		Output: os.Stdout,
 	})
 
+	buffer := new(bytes.Buffer)
 	baseCommand := &common.BaseCommand{
 		Log: log,
+		UI:  terminal.NewUI(context.TODO(), buffer),
 	}
 
 	c := &ListCommand{
 		BaseCommand: baseCommand,
 	}
 	c.init()
-	return c
+	return c, buffer
 }
 
 func TestArgumentParsing(t *testing.T) {
@@ -53,7 +58,7 @@ func TestArgumentParsing(t *testing.T) {
 
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-			c := getInitializedCommand(t)
+			c, _ := getInitializedCommand(t)
 			c.kubernetes = fake.NewSimpleClientset()
 			out := c.Run(tc.args)
 			require.Equal(t, tc.out, out)
@@ -224,7 +229,7 @@ func TestFetchPods(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			client := fake.NewSimpleClientset(&v1.PodList{Items: tc.pods})
 
-			c := getInitializedCommand(t)
+			c, _ := getInitializedCommand(t)
 			c.kubernetes = client
 			c.flagNamespace = tc.namespace
 			pods, err := c.fetchPods()
@@ -232,5 +237,88 @@ func TestFetchPods(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, tc.expectedPods, len(pods))
 		})
+	}
+}
+
+func TestEndToEnd(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Namespace.*Name.*Type",
+		"consul.*mesh-gateway.*Mesh Gateway",
+		"consul.*terminating-gateway.*Terminating Gateway",
+		"default.*ingress-gateway.*Ingress Gateway",
+		"consul.*api-gateway.*Sidecar",
+		"default.*pod1.*Sidecar",
+	}
+
+	pods := []v1.Pod{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "ingress-gateway",
+				Namespace: "default",
+				Labels: map[string]string{
+					"component": "ingress-gateway",
+					"chart":     "consul-helm",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "mesh-gateway",
+				Namespace: "consul",
+				Labels: map[string]string{
+					"component": "mesh-gateway",
+					"chart":     "consul-helm",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "terminating-gateway",
+				Namespace: "consul",
+				Labels: map[string]string{
+					"component": "terminating-gateway",
+					"chart":     "consul-helm",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "api-gateway",
+				Namespace: "consul",
+				Labels: map[string]string{
+					"api-gateway.consul.hashicorp.com/managed": "true",
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "dont-fetch",
+				Namespace: "default",
+				Labels:    map[string]string{},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pod1",
+				Namespace: "default",
+				Labels: map[string]string{
+					"consul.hashicorp.com/connect-inject-status": "injected",
+				},
+			},
+		},
+	}
+	client := fake.NewSimpleClientset(&v1.PodList{Items: pods})
+
+	c, buf := getInitializedCommand(t)
+	c.kubernetes = client
+
+	out := c.Run([]string{"-A"})
+	require.Equal(t, 0, out)
+
+	actual := buf.String()
+
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
 	}
 }
