b326bc12810d92ea95a347caa4f2d96cd06deeea
Thomas Eckert <teckert@hashicorp.com>
2022-07-13 23:13:07 -0400

    Add port filtering

--- cli/cmd/proxy/read/filters.go
+++ cli/cmd/proxy/read/filters.go
@@ -1,15 +1,44 @@
 package read
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 // FilterFQDN takes a slice of clusters along with a substring
 // and filters the clusters to only those with fully qualified
 // domain names which contain the given substring.
 func FilterFQDN(clusters []Cluster, substring string) []Cluster {
+	if substring == "" {
+		return clusters
+	}
+
 	filtered := make([]Cluster, 0)
 	for _, cluster := range clusters {
 		if strings.Contains(cluster.FullyQualifiedDomainName, substring) {
 			filtered = append(filtered, cluster)
+		}
+	}
+
+	return filtered
+}
+
+// FilterPort takes a slice of clusters along with a port number
+// and filters the clusters to only those with endpoints whose
+// ports match the given port.
+func FilterPort(clusters []Cluster, port int) []Cluster {
+	if port == -1 {
+		return clusters
+	}
+
+	portStr := strconv.Itoa(port)
+
+	filtered := make([]Cluster, 0)
+	for _, cluster := range clusters {
+		for _, endpoint := range cluster.Endpoints {
+			if strings.HasSuffix(endpoint, portStr) {
+				filtered = append(filtered, cluster)
+			}
 		}
 	}
 
--- cli/cmd/proxy/read/filters_test.go
+++ cli/cmd/proxy/read/filters_test.go
@@ -137,5 +137,127 @@ func TestFilterFQDN(t *testing.T) {
 			require.Equal(t, tc.expected, actual)
 		})
 	}
+}
 
+func TestFilterPort(t *testing.T) {
+	given := []Cluster{
+		{
+			Name:                     "local_agent",
+			FullyQualifiedDomainName: "local_agent",
+			Endpoints:                []string{"192.168.79.187:8502"},
+			Type:                     "STATIC",
+			LastUpdated:              "2022-05-13T04:22:39.553Z",
+		},
+		{
+			Name:                     "client",
+			FullyQualifiedDomainName: "client.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+			Endpoints:                []string{},
+			Type:                     "EDS",
+			LastUpdated:              "2022-06-09T00:39:12.948Z",
+		},
+		{
+			Name:                     "frontend",
+			FullyQualifiedDomainName: "frontend.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+			Endpoints:                []string{},
+			Type:                     "EDS",
+			LastUpdated:              "2022-06-09T00:39:12.855Z",
+		},
+		{
+			Name:                     "local_app",
+			FullyQualifiedDomainName: "local_app",
+			Endpoints:                []string{"127.0.0.1:8080"},
+			Type:                     "STATIC",
+			LastUpdated:              "2022-05-13T04:22:39.655Z",
+		},
+		{
+			Name:                     "original-destination",
+			FullyQualifiedDomainName: "original-destination",
+			Endpoints:                []string{},
+			Type:                     "ORIGINAL_DST",
+			LastUpdated:              "2022-05-13T04:22:39.743Z",
+		},
+		{
+			Name:                     "server",
+			FullyQualifiedDomainName: "server.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+			Endpoints:                []string{},
+			Type:                     "EDS",
+			LastUpdated:              "2022-06-09T00:39:12.754Z",
+		},
+	}
+
+	cases := map[string]struct {
+		port     int
+		expected []Cluster
+	}{
+		"No filtering": {
+			port: -1,
+			expected: []Cluster{
+				{
+					Name:                     "local_agent",
+					FullyQualifiedDomainName: "local_agent",
+					Endpoints:                []string{"192.168.79.187:8502"},
+					Type:                     "STATIC",
+					LastUpdated:              "2022-05-13T04:22:39.553Z",
+				},
+				{
+					Name:                     "client",
+					FullyQualifiedDomainName: "client.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+					Endpoints:                []string{},
+					Type:                     "EDS",
+					LastUpdated:              "2022-06-09T00:39:12.948Z",
+				},
+				{
+					Name:                     "frontend",
+					FullyQualifiedDomainName: "frontend.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+					Endpoints:                []string{},
+					Type:                     "EDS",
+					LastUpdated:              "2022-06-09T00:39:12.855Z",
+				},
+				{
+					Name:                     "local_app",
+					FullyQualifiedDomainName: "local_app",
+					Endpoints:                []string{"127.0.0.1:8080"},
+					Type:                     "STATIC",
+					LastUpdated:              "2022-05-13T04:22:39.655Z",
+				},
+				{
+					Name:                     "original-destination",
+					FullyQualifiedDomainName: "original-destination",
+					Endpoints:                []string{},
+					Type:                     "ORIGINAL_DST",
+					LastUpdated:              "2022-05-13T04:22:39.743Z",
+				},
+				{
+					Name:                     "server",
+					FullyQualifiedDomainName: "server.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul",
+					Endpoints:                []string{},
+					Type:                     "EDS",
+					LastUpdated:              "2022-06-09T00:39:12.754Z",
+				},
+			},
+		},
+		"Port that exists": {
+			port: 8502,
+			expected: []Cluster{
+				{
+					Name:                     "local_agent",
+					FullyQualifiedDomainName: "local_agent",
+					Endpoints:                []string{"192.168.79.187:8502"},
+					Type:                     "STATIC",
+					LastUpdated:              "2022-05-13T04:22:39.553Z",
+				},
+			},
+		},
+		"Port that doesn't exist": {
+			port:     14000000,
+			expected: []Cluster{},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			actual := FilterPort(given, tc.port)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
 }
