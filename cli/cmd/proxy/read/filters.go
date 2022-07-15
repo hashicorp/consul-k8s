aeb178e3116d5042414fbb737a5393caf9cd840c
Thomas Eckert <teckert@hashicorp.com>
2022-07-15 15:31:45 -0400

    Add comments to filter functions

--- cli/cmd/proxy/read/filters.go
+++ cli/cmd/proxy/read/filters.go
@@ -26,6 +26,7 @@ func FilterClustersByFQDN(clusters []Cluster, substring string) []Cluster {
 // FilterClustersByPort takes a slice of clusters along with a port number
 // and filters the clusters to only those with endpoints whose
 // ports match the given port.
+// If -1 is passed as a port, no filtering will occur.
 func FilterClustersByPort(clusters []Cluster, port int) []Cluster {
 	if port == -1 {
 		return clusters
@@ -45,6 +46,9 @@ func FilterClustersByPort(clusters []Cluster, port int) []Cluster {
 	return filtered
 }
 
+// FilterEndpointsByPort takes a slice of endpoints along with a port number
+// and filters the endpoints to only those whose port matches the given port.
+// If -1 is passed as a port, no filtering will occur.
 func FilterEndpointsByPort(endpoints []Endpoint, port int) []Endpoint {
 	if port == -1 {
 		return endpoints
@@ -55,6 +59,10 @@ func FilterEndpointsByPort(endpoints []Endpoint, port int) []Endpoint {
 	return filtered
 }
 
+// FilterListenersByPort takes a slice of listeners along with a port number
+// and filters the listeners to only those with an address whose port matches
+// the given port.
+// If -1 is passed as a port, no filtering will occur.
 func FilterListenersByPort(listeners []Listener, port int) []Listener {
 	if port == -1 {
 		return listeners
@@ -65,6 +73,9 @@ func FilterListenersByPort(listeners []Listener, port int) []Listener {
 	return filtered
 }
 
+// FilterListenersByAddress takes a slice of listeners along with a substring
+// and filters the listeners to only those with an address that contains the
+// given substring.
 func FilterListenersByAddress(listeners []Listener, substring string) []Listener {
 	if substring == "" {
 		return listeners
