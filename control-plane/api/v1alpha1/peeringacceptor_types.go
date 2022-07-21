95c17e574e9d4d32b9c873f728afd712df494b5d
Ashwin Venkatesh <ashwin@hashicorp.com>
2022-07-21 13:01:23 -0400

    Support Conditions on Peering CRDs (#1335)
    * Support Conditions on Peering CRDs
    - Add printer columns so describes have more metadata.
    * CHANGELOG

--- control-plane/api/v1alpha1/peeringacceptor_types.go
+++ control-plane/api/v1alpha1/peeringacceptor_types.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -20,6 +21,10 @@ func init() {
 //+kubebuilder:subresource:status
 
 // PeeringAcceptor is the Schema for the peeringacceptors API.
+// +kubebuilder:printcolumn:name="Synced",type="string",JSONPath=".status.conditions[?(@.type==\"Synced\")].status",description="The sync status of the resource with Consul"
+// +kubebuilder:printcolumn:name="Last Synced",type="date",JSONPath=".status.lastSyncedTime",description="The last successful synced time of the resource with Consul"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="The age of the resource"
+// +kubebuilder:resource:shortName="peering-acceptor"
 type PeeringAcceptor struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -61,22 +66,17 @@ type Secret struct {
 type PeeringAcceptorStatus struct {
 	// LatestPeeringVersion is the latest version of the resource that was reconciled.
 	LatestPeeringVersion *uint64 `json:"latestPeeringVersion,omitempty"`
-	// LastReconcileTime is the last time the resource was reconciled.
-	// +optional
-	LastReconcileTime *metav1.Time `json:"lastReconcileTime,omitempty" description:"last time the resource was reconciled"`
-	// ReconcileError shows any errors during the last reconciliation of this resource.
-	// +optional
-	ReconcileError *ReconcileErrorStatus `json:"reconcileError,omitempty"`
 	// SecretRef shows the status of the secret.
 	// +optional
 	SecretRef *SecretRefStatus `json:"secret,omitempty"`
-}
-
-type ReconcileErrorStatus struct {
-	// Error is a boolean indicating if there was an error during the last reconcile of this resource.
-	Error *bool `json:"error,omitempty"`
-	// Message displays the error message from the last reconcile.
-	Message *string `json:"message,omitempty"`
+	// Conditions indicate the latest available observations of a resource's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+	// LastSyncedTime is the last time the resource successfully synced with Consul.
+	// +optional
+	LastSyncedTime *metav1.Time `json:"lastSyncedTime,omitempty" description:"last time the condition transitioned from one status to another"`
 }
 
 type SecretRefStatus struct {
@@ -123,4 +123,16 @@ func (pa *PeeringAcceptor) Validate() error {
 			pa.KubernetesName(), errs)
 	}
 	return nil
+}
+
+func (pa *PeeringAcceptor) SetSyncedCondition(status corev1.ConditionStatus, reason string, message string) {
+	pa.Status.Conditions = Conditions{
+		{
+			Type:               ConditionSynced,
+			Status:             status,
+			LastTransitionTime: metav1.Now(),
+			Reason:             reason,
+			Message:            message,
+		},
+	}
 }
--- control-plane/api/v1alpha1/peeringdialer_types.go
+++ control-plane/api/v1alpha1/peeringdialer_types.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -19,6 +20,10 @@ func init() {
 //+kubebuilder:subresource:status
 
 // PeeringDialer is the Schema for the peeringdialers API.
+// +kubebuilder:printcolumn:name="Synced",type="string",JSONPath=".status.conditions[?(@.type==\"Synced\")].status",description="The sync status of the resource with Consul"
+// +kubebuilder:printcolumn:name="Last Synced",type="date",JSONPath=".status.lastSyncedTime",description="The last successful synced time of the resource with Consul"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="The age of the resource"
+// +kubebuilder:resource:shortName="peering-dialer"
 type PeeringDialer struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -46,15 +51,17 @@ type PeeringDialerSpec struct {
 type PeeringDialerStatus struct {
 	// LatestPeeringVersion is the latest version of the resource that was reconciled.
 	LatestPeeringVersion *uint64 `json:"latestPeeringVersion,omitempty"`
-	// LastReconcileTime is the last time the resource was reconciled.
-	// +optional
-	LastReconcileTime *metav1.Time `json:"lastReconcileTime,omitempty" description:"last time the resource was reconciled"`
-	// ReconcileError shows any errors during the last reconciliation of this resource.
-	// +optional
-	ReconcileError *ReconcileErrorStatus `json:"reconcileError,omitempty"`
 	// SecretRef shows the status of the secret.
 	// +optional
 	SecretRef *SecretRefStatus `json:"secret,omitempty"`
+	// Conditions indicate the latest available observations of a resource's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+	// LastSyncedTime is the last time the resource successfully synced with Consul.
+	// +optional
+	LastSyncedTime *metav1.Time `json:"lastSyncedTime,omitempty" description:"last time the condition transitioned from one status to another"`
 }
 
 func (pd *PeeringDialer) Secret() *Secret {
@@ -98,4 +105,16 @@ func (pd *PeeringDialer) Validate() error {
 			pd.KubernetesName(), errs)
 	}
 	return nil
+}
+
+func (pd *PeeringDialer) SetSyncedCondition(status corev1.ConditionStatus, reason string, message string) {
+	pd.Status.Conditions = Conditions{
+		{
+			Type:               ConditionSynced,
+			Status:             status,
+			LastTransitionTime: metav1.Now(),
+			Reason:             reason,
+			Message:            message,
+		},
+	}
 }
