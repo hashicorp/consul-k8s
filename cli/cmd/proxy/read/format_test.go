bd58d37b805c84c58f4e3bc732db9284ad3aff2f
Thomas Eckert <teckert@hashicorp.com>
2022-07-25 13:53:50 -0400

    Read output for table formatting

--- cli/cmd/proxy/read/format_test.go
+++ cli/cmd/proxy/read/format_test.go
@@ -1,12 +1,26 @@
 package read
 
 import (
+	"bytes"
+	"context"
 	"testing"
 
+	"github.com/hashicorp/consul-k8s/cli/common/terminal"
 	"github.com/stretchr/testify/require"
 )
 
 func TestFormatClusters(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Name.*FQDN.*Endpoints.*Type.*Last Updated",
+		"local_agent.*local_agent.*192\\.168\\.79\\.187:8502.*STATIC.*2022-05-13T04:22:39\\.553Z",
+		"local_app.*local_app.*127\\.0\\.0\\.1:8080.*STATIC.*2022-05-13T04:22:39\\.655Z",
+		"client.*client\\.default\\.dc1\\.internal\\.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00\\.consul.*EDS.*2022-06-09T00:39:12\\.948Z",
+		"frontend.*frontend\\.default\\.dc1\\.internal\\.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00\\.consul.*EDS.*2022-06-09T00:39:12\\.855Z",
+		"original-destination.*original-destination.*ORIGINAL_DST.*2022-05-13T04:22:39.743Z",
+		"server.*server.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul.*EDS.*2022-06-09T00:39:12\\.754Z",
+	}
+
 	given := []Cluster{
 		{
 			Name:                     "local_agent",
@@ -54,13 +68,35 @@ func TestFormatClusters(t *testing.T) {
 
 	expectedHeaders := []string{"Name", "FQDN", "Endpoints", "Type", "Last Updated"}
 
-	actual := formatClusters(given)
+	table := formatClusters(given)
 
-	require.Equal(t, expectedHeaders, actual.Headers)
-	require.Equal(t, len(given), len(actual.Rows))
+	require.Equal(t, expectedHeaders, table.Headers)
+	require.Equal(t, len(given), len(table.Rows))
+
+	buf := new(bytes.Buffer)
+	terminal.NewUI(context.Background(), buf).Table(table)
+
+	actual := buf.String()
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
+	}
 }
 
 func TestFormatEndpoints(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Address:Port.*Cluster.*Weight.*Status",
+		"192.168.79.187:8502.*local_agent.*1.00.*HEALTHY",
+		"127.0.0.1:8080.*local_app.*1.00.*HEALTHY",
+		"192.168.31.201:20000.*1.00.*HEALTHY",
+		"192.168.47.235:20000.*1.00.*HEALTHY",
+		"192.168.71.254:20000.*1.00.*HEALTHY",
+		"192.168.63.120:20000.*1.00.*HEALTHY",
+		"192.168.18.110:20000.*1.00.*HEALTHY",
+		"192.168.52.101:20000.*1.00.*HEALTHY",
+		"192.168.65.131:20000.*1.00.*HEALTHY",
+	}
+
 	given := []Endpoint{
 		{
 			Address: "192.168.79.187:8502",
@@ -113,13 +149,31 @@ func TestFormatEndpoints(t *testing.T) {
 
 	expectedHeaders := []string{"Address:Port", "Cluster", "Weight", "Status"}
 
-	actual := formatEndpoints(given)
+	table := formatEndpoints(given)
 
-	require.Equal(t, expectedHeaders, actual.Headers)
-	require.Equal(t, len(given), len(actual.Rows))
+	require.Equal(t, expectedHeaders, table.Headers)
+	require.Equal(t, len(given), len(table.Rows))
+
+	buf := new(bytes.Buffer)
+	terminal.NewUI(context.Background(), buf).Table(table)
+
+	actual := buf.String()
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
+	}
 }
 
 func TestFormatListeners(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Name.*Address:Port.*Direction.*Filter Chain Match.*Filters.*Last Updated",
+		"public_listener.*192\\.168\\.69\\.179:20000.*INBOUND.*Any.*\\* -> local_app/.*2022-06-09T00:39:27\\.668Z",
+		"outbound_listener.*127.0.0.1:15001.*OUTBOUND.*10\\.100\\.134\\.173/32, 240\\.0\\.0\\.3/32.*-> client.default.dc1.internal.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00.consul.*2022-05-24T17:41:59\\.079Z",
+		"10\\.100\\.254\\.176/32, 240\\.0\\.0\\.4/32.*\\* -> server\\.default\\.dc1\\.internal\\.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00\\.consul/",
+		"10\\.100\\.31\\.2/32, 240\\.0\\.0\\.2/32.*-> frontend\\.default\\.dc1\\.internal\\.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00\\.consul",
+		"Any.*-> original-destination",
+	}
+
 	given := []Listener{
 		{
 			Name:    "public_listener",
@@ -169,13 +223,28 @@ func TestFormatListeners(t *testing.T) {
 		expectedRowCount += len(element.FilterChain)
 	}
 
-	actual := formatListeners(given)
+	table := formatListeners(given)
 
-	require.Equal(t, expectedHeaders, actual.Headers)
-	require.Equal(t, expectedRowCount, len(actual.Rows))
+	require.Equal(t, expectedHeaders, table.Headers)
+	require.Equal(t, expectedRowCount, len(table.Rows))
+
+	buf := new(bytes.Buffer)
+	terminal.NewUI(context.Background(), buf).Table(table)
+
+	actual := buf.String()
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
+	}
 }
 
 func TestFormatRoutes(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Name.*Destination Cluster.*Last Updated",
+		"public_listener.*local_app/.*2022-06-09T00:39:27.667Z",
+		"server.*server\\.default\\.dc1\\.internal\\.bc3815c2-1a0f-f3ff-a2e9-20d791f08d00\\.consul/.*2022-05-24T17:41:59\\.078Z",
+	}
+
 	given := []Route{
 		{
 			Name:               "public_listener",
@@ -191,13 +260,28 @@ func TestFormatRoutes(t *testing.T) {
 
 	expectedHeaders := []string{"Name", "Destination Cluster", "Last Updated"}
 
-	actual := formatRoutes(given)
+	table := formatRoutes(given)
 
-	require.Equal(t, expectedHeaders, actual.Headers)
-	require.Equal(t, len(given), len(actual.Rows))
+	require.Equal(t, expectedHeaders, table.Headers)
+	require.Equal(t, len(given), len(table.Rows))
+
+	buf := new(bytes.Buffer)
+	terminal.NewUI(context.Background(), buf).Table(table)
+
+	actual := buf.String()
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
+	}
 }
 
 func TestFormatSecrets(t *testing.T) {
+	// These regular expressions must be present in the output.
+	expected := []string{
+		"Name.*Type.*Last Updated",
+		"default.*Dynamic Active.*2022-05-24T17:41:59.078Z",
+		"ROOTCA.*Dynamic Warming.*2022-03-15T05:14:22.868Z",
+	}
+
 	given := []Secret{
 		{
 			Name:        "default",
@@ -213,8 +297,16 @@ func TestFormatSecrets(t *testing.T) {
 
 	expectedHeaders := []string{"Name", "Type", "Last Updated"}
 
-	actual := formatSecrets(given)
+	table := formatSecrets(given)
 
-	require.Equal(t, expectedHeaders, actual.Headers)
-	require.Equal(t, len(given), len(actual.Rows))
+	require.Equal(t, expectedHeaders, table.Headers)
+	require.Equal(t, len(given), len(table.Rows))
+
+	buf := new(bytes.Buffer)
+	terminal.NewUI(context.Background(), buf).Table(table)
+
+	actual := buf.String()
+	for _, expression := range expected {
+		require.Regexp(t, expression, actual)
+	}
 }
