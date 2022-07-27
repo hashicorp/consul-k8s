54f14952a82bfd372977c761385d186494617d3e
Nitya Dhanushkodi <nitya@hashicorp.com>
2022-07-26 18:18:42 -0700

    add longer timeouts, enable on aks

--- acceptance/framework/k8s/deploy.go
+++ acceptance/framework/k8s/deploy.go
@@ -96,7 +96,7 @@ func CheckStaticServerConnectionMultipleFailureMessages(t *testing.T, options *k
 		expectedOutput = expectedSuccessOutput
 	}
 
-	retrier := &retry.Timer{Timeout: 80 * time.Second, Wait: 2 * time.Second}
+	retrier := &retry.Timer{Timeout: 160 * time.Second, Wait: 2 * time.Second}
 
 	args := []string{"exec", "deploy/" + sourceApp, "-c", sourceApp, "--", "curl", "-vvvsSf"}
 	args = append(args, curlArgs...)
