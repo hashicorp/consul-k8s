9872a3516daffd5f5080473f02d235dc9d000bfc
Curt Bushko <cbushko@gmail.com>
2022-08-16 09:59:39 -0400

    remove the annotation that is not used

--- control-plane/connect-inject/annotations.go
+++ control-plane/connect-inject/annotations.go
@@ -5,10 +5,6 @@ const (
 	// a pod after an injection is done.
 	keyInjectStatus = "consul.hashicorp.com/connect-inject-status"
 
-	// keyTransparentProxyStatus is the key of the annotation that is added to
-	// a pod when transparent proxy is done.
-	keyTransparentProxyStatus = "consul.hashicorp.com/transparent-proxy-status"
-
 	// keyManagedBy is the key of the label that is added to pods managed
 	// by the Endpoints controller. This is to support upgrading from consul-k8s
 	// without Endpoints controller to consul-k8s with Endpoints controller
@@ -170,9 +166,6 @@ const (
 
 	// injected is used as the annotation value for keyInjectStatus and annotationInjected.
 	injected = "injected"
-
-	// enabled is used as the annotation value for keyTransparentProxyStatus.
-	enabled = "enabled"
 
 	// endpointsController is the value for keyManagedBy.
 	managedByValue = "consul-k8s-endpoints-controller"
