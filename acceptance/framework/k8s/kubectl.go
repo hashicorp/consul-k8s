6593a523d2b33a670c5b6ad34489522edd56169f
Kyle Schochenmaier <kschoche@gmail.com>
2022-08-17 16:21:39 -0500

    fix delete path for acceptance test

--- acceptance/framework/k8s/kubectl.go
+++ acceptance/framework/k8s/kubectl.go
@@ -102,9 +102,10 @@ func KubectlDelete(t *testing.T, options *k8s.KubectlOptions, configPath string)
 // deletes it from the cluster by running 'kubectl delete -k'.
 // If there's an error deleting the file, fail the test.
 func KubectlDeleteK(t *testing.T, options *k8s.KubectlOptions, kustomizeDir string) {
-	_, _ = RunKubectlAndGetOutputE(t, options, "delete", "--timeout=60s", "-k", kustomizeDir)
-	// TODO: this is to prevent test failures when Kubernetes automatically cleans up the kube secrets, fix me.
-	//	require.NoError(t, err)
+	// Ignore not found errors because Kubernetes automatically cleans up the kube secrets that we deployed
+	// referencing the ServiceAccount when it is deleted.
+	_, err := RunKubectlAndGetOutputE(t, options, "delete", "--timeout=60s", "--ignore-not-found", "-k", kustomizeDir)
+	require.NoError(t, err)
 }
 
 // RunKubectl runs an arbitrary kubectl command provided via args and ignores the output.
