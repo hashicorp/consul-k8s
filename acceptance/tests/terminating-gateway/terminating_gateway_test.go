315fd9f63178d436b4e2ffe297e899f8e562b6d3
Dan Stough <dan.stough@hashicorp.com>
2022-08-17 13:56:04 -0400

    test: egress destination tests (#1383)

--- acceptance/tests/terminating-gateway/terminating_gateway_test.go
+++ acceptance/tests/terminating-gateway/terminating_gateway_test.go
@@ -3,10 +3,8 @@ package terminatinggateway
 import (
 	"fmt"
 	"strconv"
-	"strings"
 	"testing"
 
-	terratestk8s "github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/hashicorp/consul-k8s/acceptance/framework/consul"
 	"github.com/hashicorp/consul-k8s/acceptance/framework/helpers"
 	"github.com/hashicorp/consul-k8s/acceptance/framework/k8s"
@@ -14,9 +12,6 @@ import (
 	"github.com/hashicorp/consul/api"
 	"github.com/stretchr/testify/require"
 )
-
-const StaticClientName = "static-client"
-const staticServerName = "static-server"
 
 // Test that terminating gateways work in a default and secure installations.
 func TestTerminatingGateway(t *testing.T) {
@@ -71,13 +66,13 @@ func TestTerminatingGateway(t *testing.T) {
 
 			// If ACLs are enabled we need to update the role of the terminating gateway
 			// with service:write permissions to the static-server service
-			// so that it can can request Connect certificates for it.
+			// so that it can request Connect certificates for it.
 			if c.secure {
 				updateTerminatingGatewayRole(t, consulClient, staticServerPolicyRules)
 			}
 
 			// Create the config entry for the terminating gateway.
-			createTerminatingGatewayConfigEntry(t, consulClient, "", "")
+			createTerminatingGatewayConfigEntry(t, consulClient, "", "", staticServerName)
 
 			// Deploy the static client
 			logger.Log(t, "deploying static client")
@@ -88,12 +83,16 @@ func TestTerminatingGateway(t *testing.T) {
 				// With the terminating gateway up, we test that we can make a call to it
 				// via the static-server. It should fail to connect with the
 				// static-server pod because of intentions.
-				assertNoConnectionAndAddIntention(t, consulClient, ctx.KubectlOptions(t), "", "")
+				logger.Log(t, "testing intentions prevent connections through the terminating gateway")
+				k8s.CheckStaticServerConnectionFailing(t, ctx.KubectlOptions(t), staticClientName, staticServerLocalAddress)
+
+				logger.Log(t, "adding intentions to allow traffic from client ==> server")
+				addIntention(t, consulClient, "", staticClientName, "", staticServerName)
 			}
 
 			// Test that we can make a call to the terminating gateway.
 			logger.Log(t, "trying calls to terminating gateway")
-			k8s.CheckStaticServerConnectionSuccessful(t, ctx.KubectlOptions(t), StaticClientName, "http://localhost:1234")
+			k8s.CheckStaticServerConnectionSuccessful(t, ctx.KubectlOptions(t), staticClientName, staticServerLocalAddress)
 		})
 	}
 }
@@ -129,82 +128,6 @@ func registerExternalService(t *testing.T, consulClient *api.Client, namespace s
 		Address:  address,
 		NodeMeta: map[string]string{"external-node": "true", "external-probe": "true"},
 		Service:  service,
-	}, nil)
-	require.NoError(t, err)
-}
-
-func updateTerminatingGatewayRole(t *testing.T, consulClient *api.Client, rules string) {
-	t.Helper()
-
-	logger.Log(t, "creating a write policy for the static-server")
-	_, _, err := consulClient.ACL().PolicyCreate(&api.ACLPolicy{
-		Name:  "static-server-write-policy",
-		Rules: rules,
-	}, nil)
-	require.NoError(t, err)
-
-	logger.Log(t, "getting the terminating gateway role")
-	roles, _, err := consulClient.ACL().RoleList(nil)
-	require.NoError(t, err)
-	terminatingGatewayRoleID := ""
-	for _, role := range roles {
-		if strings.Contains(role.Name, "terminating-gateway") {
-			terminatingGatewayRoleID = role.ID
-			break
-		}
-	}
-
-	logger.Log(t, "update role with policy")
-	termGwRole, _, err := consulClient.ACL().RoleRead(terminatingGatewayRoleID, nil)
-	require.NoError(t, err)
-	termGwRole.Policies = append(termGwRole.Policies, &api.ACLTokenPolicyLink{Name: "static-server-write-policy"})
-	_, _, err = consulClient.ACL().RoleUpdate(termGwRole, nil)
-	require.NoError(t, err)
-}
-
-func createTerminatingGatewayConfigEntry(t *testing.T, consulClient *api.Client, gwNamespace, serviceNamespace string) {
-	t.Helper()
-
-	logger.Log(t, "creating config entry")
-
-	if serviceNamespace != "" {
-		logger.Logf(t, "creating the %s namespace in Consul", serviceNamespace)
-		_, _, err := consulClient.Namespaces().Create(&api.Namespace{
-			Name: serviceNamespace,
-		}, nil)
-		require.NoError(t, err)
-	}
-
-	configEntry := &api.TerminatingGatewayConfigEntry{
-		Kind:      api.TerminatingGateway,
-		Name:      "terminating-gateway",
-		Namespace: gwNamespace,
-		Services:  []api.LinkedService{{Name: staticServerName, Namespace: serviceNamespace}},
-	}
-
-	created, _, err := consulClient.ConfigEntries().Set(configEntry, nil)
-	require.NoError(t, err)
-	require.True(t, created, "failed to create config entry")
-}
-
-func assertNoConnectionAndAddIntention(t *testing.T, consulClient *api.Client, k8sOptions *terratestk8s.KubectlOptions, sourceNS, destinationNS string) {
-	t.Helper()
-
-	logger.Log(t, "testing intentions prevent connections through the terminating gateway")
-	k8s.CheckStaticServerConnectionFailing(t, k8sOptions, StaticClientName, "http://localhost:1234")
-
-	logger.Log(t, "creating static-client => static-server intention")
-	_, _, err := consulClient.ConfigEntries().Set(&api.ServiceIntentionsConfigEntry{
-		Kind:      api.ServiceIntentions,
-		Name:      staticServerName,
-		Namespace: destinationNS,
-		Sources: []*api.SourceIntention{
-			{
-				Name:      StaticClientName,
-				Namespace: sourceNS,
-				Action:    api.IntentionActionAllow,
-			},
-		},
 	}, nil)
 	require.NoError(t, err)
 }
