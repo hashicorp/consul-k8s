c7c0ece362abac108b15a970c951996dcbb16851
Iryna Shustava <iryna@hashicorp.com>
2022-09-21 13:40:08 -0600

    wip: mesh gateways with connect-init and new consul-dataplane

--- acceptance/tests/wan-federation/wan_federation_test.go
+++ acceptance/tests/wan-federation/wan_federation_test.go
@@ -29,10 +29,10 @@ func TestWANFederation(t *testing.T) {
 			name:   "secure",
 			secure: true,
 		},
-		{
-			name:   "default",
-			secure: false,
-		},
+		//{
+		//	name:   "default",
+		//	secure: false,
+		//},
 	}
 
 	for _, c := range cases {
@@ -50,6 +50,8 @@ func TestWANFederation(t *testing.T) {
 
 			primaryHelmValues := map[string]string{
 				"global.datacenter": "dc1",
+
+				"global.logLevel": "debug",
 
 				"global.tls.enabled":   "true",
 				"global.tls.httpsOnly": strconv.FormatBool(c.secure),
@@ -104,6 +106,8 @@ func TestWANFederation(t *testing.T) {
 			// Create secondary cluster
 			secondaryHelmValues := map[string]string{
 				"global.datacenter": "dc2",
+
+				"global.logLevel": "debug",
 
 				"global.tls.enabled":           "true",
 				"global.tls.httpsOnly":         "false",
