fcc314e68977a16e096f5cd0f543bcbb8f1b85e8
Iryna Shustava <iryna@hashicorp.com>
2022-10-17 14:17:50 -0600

    enable tproxy acceptance tests

--- acceptance/tests/wan-federation/main_test.go
+++ acceptance/tests/wan-federation/main_test.go
@@ -13,7 +13,8 @@ var suite testsuite.Suite
 func TestMain(m *testing.M) {
 	suite = testsuite.NewSuite(m)
 
-	if suite.Config().EnableMultiCluster {
+	// todo(agentless): Re-enable tproxy tests once we support it for multi-cluster.
+	if suite.Config().EnableMultiCluster && !suite.Config().EnableTransparentProxy {
 		os.Exit(suite.Run())
 	} else {
 		fmt.Println("Skipping wan federation tests because -enable-multi-cluster is not set")
