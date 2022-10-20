2a3316681e813620cac3e3610eaee86a808fb8b6
Iryna Shustava <iryna@hashicorp.com>
2022-10-20 14:36:47 -0600

    disable connect inject for external servers install

--- acceptance/tests/connect/connect_external_servers_test.go
+++ acceptance/tests/connect/connect_external_servers_test.go
@@ -20,7 +20,10 @@ import (
 // It sets up an external Consul server in the same cluster but a different Helm installation
 // and then treats this server as external.
 func TestConnectInject_ExternalServers(t *testing.T) {
-	for _, secure := range []bool{false, true} {
+	for _, secure := range []bool{
+		false,
+		true,
+	} {
 		caseName := fmt.Sprintf("secure: %t", secure)
 		t.Run(caseName, func(t *testing.T) {
 			cfg := suite.Config()
@@ -31,7 +34,8 @@ func TestConnectInject_ExternalServers(t *testing.T) {
 				"global.tls.enabled":           strconv.FormatBool(secure),
 
 				// This prevents controllers from being installed twice, causing a failure.
-				"controller.enabled": "false",
+				"controller.enabled":    "false",
+				"connectInject.enabled": "false",
 			}
 			serverReleaseName := helpers.RandomName()
 			consulServerCluster := consul.NewHelmCluster(t, serverHelmValues, ctx, cfg, serverReleaseName)
