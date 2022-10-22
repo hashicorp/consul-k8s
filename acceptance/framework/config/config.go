1ae2e1cf7576d6c3d71fe653bcf86495ff86bb72
Iryna Shustava <ishustava@users.noreply.github.com>
2022-10-21 19:39:17 -0600

    Separate CLI tests into their own package (#1639)

--- acceptance/framework/config/config.go
+++ acceptance/framework/config/config.go
@@ -153,7 +153,7 @@ func (t *TestConfig) entImage() (string, error) {
 		preRelease = fmt.Sprintf("-%s", split[1])
 	}
 
-	return fmt.Sprintf("hashicorp/consul-enterprise:%s-ent%s", consulImageVersion, preRelease), nil
+	return fmt.Sprintf("hashicorp/consul-enterprise:%s%s-ent", consulImageVersion, preRelease), nil
 }
 
 // setIfNotEmpty sets key to val in map m if value is not empty.
