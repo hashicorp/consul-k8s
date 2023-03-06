7079fc8ad02f48bb7ca571233546e26864c76d1e
Paul Glass <pglass@hashicorp.com>
2023-03-06 13:11:11 -0600

    Automatic ACL bootstrap with Vault secrets backend (#1920)
    Support automatic ACL bootstrapping with the Vault secrets backend
    
    With the Vault secrets backend, server-acl-init now:
    * Runs the Vault agent as a sidecar
    * Bootstraps ACLs if the Vault bootstrap token is empty or not found,
      and writes the bootstrap token back to Vault via the Vault agent
    
    The Kubernetes backend will write the bootstrap token to the
    user-provided secret if that secret is empty. The Vault behavior is
    the same.
    
    The Vault backend writes to a default secret name if the secretName
    and secretKey are not set in the helm chart values.
    
    server-acl-init reads the secret directly from k8s or Vault.
    * Remove -bootstrap-token-file flag from server-acl-init and remove the
    * Remove the volume/mount for bootstrap token
    
    ---------
    
    Co-authored-by: Chris Thain <32781396+cthain@users.noreply.github.com>

---
+++ control-plane/subcommand/server-acl-init/k8s_secrets_backend.go
@@ -0,0 +1,62 @@
+package serveraclinit
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/consul-k8s/control-plane/subcommand/common"
+	apiv1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const SecretsBackendTypeKubernetes SecretsBackendType = "kubernetes"
+
+type KubernetesSecretsBackend struct {
+	ctx          context.Context
+	clientset    kubernetes.Interface
+	k8sNamespace string
+	secretName   string
+	secretKey    string
+}
+
+var _ SecretsBackend = (*KubernetesSecretsBackend)(nil)
+
+// BootstrapToken returns the existing bootstrap token if there is one by
+// reading the Kubernetes Secret. If there is no bootstrap token yet, then
+// it returns an empty string (not an error).
+func (b *KubernetesSecretsBackend) BootstrapToken() (string, error) {
+	secret, err := b.clientset.CoreV1().Secrets(b.k8sNamespace).Get(b.ctx, b.secretName, metav1.GetOptions{})
+	if err != nil {
+		if k8serrors.IsNotFound(err) {
+			return "", nil
+		}
+		return "", err
+	}
+	token, ok := secret.Data[b.secretKey]
+	if !ok {
+		return "", fmt.Errorf("secret %q does not have data key %q", b.secretName, b.secretKey)
+	}
+	return string(token), nil
+
+}
+
+// WriteBootstrapToken writes the given bootstrap token to the Kubernetes Secret.
+func (b *KubernetesSecretsBackend) WriteBootstrapToken(bootstrapToken string) error {
+	secret := &apiv1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   b.secretName,
+			Labels: map[string]string{common.CLILabelKey: common.CLILabelValue},
+		},
+		Data: map[string][]byte{
+			b.secretKey: []byte(bootstrapToken),
+		},
+	}
+	_, err := b.clientset.CoreV1().Secrets(b.k8sNamespace).Create(b.ctx, secret, metav1.CreateOptions{})
+	return err
+}
+
+func (b *KubernetesSecretsBackend) BootstrapTokenSecretName() string {
+	return b.secretName
+}
---
+++ control-plane/subcommand/server-acl-init/secrets_backend.go
@@ -0,0 +1,18 @@
+package serveraclinit
+
+type SecretsBackendType string
+
+type SecretsBackend interface {
+	// BootstrapToken fetches the bootstrap token from the backend. If the
+	// token is not found or empty, implementations should return an empty
+	// string (not an error).
+	BootstrapToken() (string, error)
+
+	// WriteBootstrapToken writes the given bootstrap token to the backend.
+	// Implementations of this method do not need to retry the write until
+	// successful.
+	WriteBootstrapToken(string) error
+
+	// BootstrapTokenSecretName returns the name of the bootstrap token secret.
+	BootstrapTokenSecretName() string
+}
