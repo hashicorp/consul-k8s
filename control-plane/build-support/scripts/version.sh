ebedd677d852b5a846498350cc4b206925aca344
Ronald Ekambi <ronekambi@gmail.com>
2023-03-15 08:41:26 -0400

    add copyright headers to file + config file

--- control-plane/build-support/scripts/version.sh
+++ control-plane/build-support/scripts/version.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 
 version_file=$1
 version=$(awk '$1 == "Version" && $2 == "=" { gsub(/"/, "", $3); print $3 }' < "${version_file}")
