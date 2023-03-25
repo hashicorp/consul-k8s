1c82681b416bb63d33cb3611a62a1b478e53229a
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2023-03-25 09:28:44 -0500

    update unit tests

--- control-plane/subcommand/server-acl-init/rules.go
+++ control-plane/subcommand/server-acl-init/rules.go
@@ -149,14 +149,16 @@ func (c *Command) apiGatewayControllerRules() (string, error) {
 	apiGatewayRulesTpl := `{{- if .EnablePartitions }}
 partition "{{ .PartitionName }}" {
   mesh = "write"
-  operator = "write"
   acl = "write"
+  operator = "write"
 {{- else }}
 operator = "write"
 acl = "write"
 {{- end }}
+
 {{- if .EnableNamespaces }}
 namespace_prefix "" {
+  policy = "write"
 {{- end }}
   service_prefix "" {
     policy = "write"
@@ -171,7 +173,7 @@ namespace_prefix "" {
 {{- if .EnablePartitions }}
 }
 {{- end }}
-  `
+`
 
 	return c.renderRules(apiGatewayRulesTpl)
 }
