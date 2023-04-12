fd4ab9306c6fa3172618a8c9ede1ea2f2c35aab6
Thomas Eckert <teckert@hashicorp.com>
2023-04-11 23:24:09 -0400

    Remove the helpers

--- control-plane/api/v1alpha1/api_gateway_types.go
+++ control-plane/api/v1alpha1/api_gateway_types.go
@@ -4,14 +4,8 @@
 package v1alpha1
 
 import (
-	"fmt"
-
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbac "k8s.io/api/rbac/v1"
-	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
 const (
@@ -58,14 +52,6 @@ type GatewayClassConfigSpec struct {
 	CopyAnnotations CopyAnnotationsSpec `json:"copyAnnotations,omitempty"`
 }
 
-// PortSpec defines the ports that the API Gateway will listen on.
-type PortSpec struct {
-	// The port for Consul's HTTP server.
-	HTTP int `json:"http,omitempty"`
-	// The grpc port for Consul's xDS server.
-	GRPC int `json:"grpc,omitempty"`
-}
-
 //+kubebuilder:object:generate=true
 
 // CopyAnnotationsSpec defines the annotations that should be copied to the gateway service.
@@ -83,178 +69,4 @@ type GatewayClassConfigList struct {
 
 	// Items is the list of Configs.
 	Items []GatewayClassConfig `json:"items"`
-}
-
-// RoleFor constructs a Kubernetes Role for the specified Gateway based
-// on the GatewayClassConfig.
-func (c *GatewayClassConfig) RoleFor(gw *gwv1beta1.Gateway) *rbac.Role {
-	return &rbac.Role{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      gw.Name,
-			Namespace: gw.Namespace,
-			Labels:    labelsForGateway(gw),
-		},
-		Rules: []rbac.PolicyRule{{
-			APIGroups: []string{"policy"},
-			Resources: []string{"podsecuritypolicies"},
-			Verbs:     []string{"use"},
-		}},
-	}
-}
-
-// RoleBindingFor constructs a Kubernetes RoleBinding for the specified Gateway
-// based on the GatewayClassConfig. If the GatewayClassConfig is configured in
-// such a way that does not require a RoleBinding, nil is returned.
-func (c *GatewayClassConfig) RoleBindingFor(gw *gwv1beta1.Gateway) *rbac.RoleBinding {
-	serviceAccount := c.ServiceAccountFor(gw)
-	if serviceAccount == nil {
-		return nil
-	}
-
-	role := c.RoleFor(gw)
-	if role == nil {
-		return nil
-	}
-
-	return &rbac.RoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      gw.Name,
-			Namespace: gw.Namespace,
-			Labels:    labelsForGateway(gw),
-		},
-		RoleRef: rbac.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "Role",
-			Name:     role.Name,
-		},
-		Subjects: []rbac.Subject{
-			{
-				Kind:      "ServiceAccount",
-				Name:      serviceAccount.Name,
-				Namespace: serviceAccount.Namespace,
-			},
-		},
-	}
-}
-
-// ServiceAccountFor constructs a Kubernetes ServiceAccount for the specified
-// Gateway based on the GatewayClassConfig.
-func (c *GatewayClassConfig) ServiceAccountFor(gw *gwv1beta1.Gateway) *corev1.ServiceAccount {
-	return &corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      gw.Name,
-			Namespace: gw.Namespace,
-			Labels:    labelsForGateway(gw),
-		},
-	}
-}
-
-func MergeSecret(a, b *corev1.Secret) *corev1.Secret {
-	if !compareSecrets(a, b) {
-		b.Annotations = a.Annotations
-		b.Data = a.Data
-	}
-
-	return b
-}
-
-func compareSecrets(a, b *corev1.Secret) bool {
-	if !equality.Semantic.DeepEqual(a.Annotations, b.Annotations) {
-		return false
-	}
-
-	if len(b.Data) != len(a.Data) {
-		return false
-	}
-
-	for k, v := range a.Data {
-		otherV, ok := b.Data[k]
-		if !ok || string(v) != string(otherV) {
-			return false
-		}
-	}
-	return true
-}
-
-// MergeService merges a gateway service a onto b and returns b, overriding all of
-// the fields that we'd normally set for a service deployment. It does not attempt
-// to change the service type.
-func MergeService(a, b *corev1.Service) *corev1.Service {
-	if !compareServices(a, b) {
-		b.Annotations = a.Annotations
-		b.Spec.Ports = a.Spec.Ports
-	}
-
-	return b
-}
-
-func compareServices(a, b *corev1.Service) bool {
-	// since K8s adds a bunch of defaults when we create a service, check that
-	// they don't differ by the things that we may actually change, namely container
-	// ports and propagated annotations
-	if !equality.Semantic.DeepEqual(a.Annotations, b.Annotations) {
-		return false
-	}
-	if len(b.Spec.Ports) != len(a.Spec.Ports) {
-		return false
-	}
-
-	for i, port := range a.Spec.Ports {
-		otherPort := b.Spec.Ports[i]
-		if port.Port != otherPort.Port {
-			return false
-		}
-		if port.Protocol != otherPort.Protocol {
-			return false
-		}
-	}
-	return true
-}
-
-// MergeDeployment merges a gateway deployment a onto b and returns b, overriding all of
-// the fields that we'd normally set for a service deployment. It does not attempt
-// to change the service type.
-func MergeDeployment(a, b *appsv1.Deployment) *appsv1.Deployment {
-	if !compareDeployments(a, b) {
-		b.Spec.Template = a.Spec.Template
-		b.Spec.Replicas = a.Spec.Replicas
-	}
-
-	return b
-}
-
-func compareDeployments(a, b *appsv1.Deployment) bool {
-	// since K8s adds a bunch of defaults when we create a deployment, check that
-	// they don't differ by the things that we may actually change, namely container
-	// ports
-	if len(b.Spec.Template.Spec.Containers) != len(a.Spec.Template.Spec.Containers) {
-		return false
-	}
-	for i, container := range a.Spec.Template.Spec.Containers {
-		otherPorts := b.Spec.Template.Spec.Containers[i].Ports
-		if len(container.Ports) != len(otherPorts) {
-			return false
-		}
-		for j, port := range container.Ports {
-			otherPort := otherPorts[j]
-			if port.ContainerPort != otherPort.ContainerPort {
-				return false
-			}
-			if port.Protocol != otherPort.Protocol {
-				return false
-			}
-		}
-	}
-
-	return *b.Spec.Replicas == *a.Spec.Replicas
-}
-
-// labelsForGateway formats the correct configuration labels for a Gateway resource.
-func labelsForGateway(gw *gwv1beta1.Gateway) map[string]string {
-	return map[string]string{
-		nameLabel:      gw.Name,
-		namespaceLabel: gw.Namespace,
-		createdAtLabel: fmt.Sprintf("%d", gw.CreationTimestamp.Unix()),
-		managedLabel:   "true",
-	}
 }
--- control-plane/api/v1alpha1/api_gateway_types_test.go
+++ control-plane/api/v1alpha1/api_gateway_types_test.go
@@ -6,11 +6,9 @@ package v1alpha1
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
 
 func TestGatewayClassConfigDeepCopy(t *testing.T) {
@@ -47,62 +45,4 @@ func TestGatewayClassConfigDeepCopy(t *testing.T) {
 	copyConfigList := configList.DeepCopy()
 	copyConfigListObject := configList.DeepCopyObject()
 	require.Equal(t, copyConfigList, copyConfigListObject)
-}
-
-func TestGatewayClassConfig_RoleFor(t *testing.T) {
-	t.Run("managed auth with podSecurityPolicy", func(t *testing.T) {
-		gcc := &GatewayClassConfig{}
-
-		gw := &gwv1beta1.Gateway{ObjectMeta: metav1.ObjectMeta{Name: t.Name(), Namespace: t.Name()}}
-
-		role := gcc.RoleFor(gw)
-		require.NotNil(t, role)
-		assert.Equal(t, t.Name(), role.Name)
-		assert.Equal(t, t.Name(), role.Namespace)
-		assert.Equal(t, labelsForGateway(gw), role.Labels)
-
-		require.Len(t, role.Rules, 1)
-		assert.ElementsMatch(t, []string{"policy"}, role.Rules[0].APIGroups)
-		assert.ElementsMatch(t, []string{"podsecuritypolicies"}, role.Rules[0].Resources)
-		assert.ElementsMatch(t, []string{"use"}, role.Rules[0].Verbs)
-	})
-}
-
-func TestGatewayClassConfig_RoleBindingFor(t *testing.T) {
-	t.Run("managed auth with podSecurityPolicy", func(t *testing.T) {
-		gcc := &GatewayClassConfig{
-			Spec: GatewayClassConfigSpec{},
-		}
-
-		gw := &gwv1beta1.Gateway{ObjectMeta: metav1.ObjectMeta{Name: t.Name(), Namespace: t.Name()}}
-
-		binding := gcc.RoleBindingFor(gw)
-		require.NotNil(t, binding)
-		assert.Equal(t, gw.Name, binding.Name)
-		assert.Equal(t, gw.Namespace, binding.Namespace)
-		assert.Equal(t, labelsForGateway(gw), binding.Labels)
-
-		assert.Equal(t, "rbac.authorization.k8s.io", binding.RoleRef.APIGroup)
-		assert.Equal(t, "Role", binding.RoleRef.Kind)
-		assert.Equal(t, gw.Name, binding.RoleRef.Name)
-
-		require.Len(t, binding.Subjects, 1)
-		assert.Equal(t, "ServiceAccount", binding.Subjects[0].Kind)
-		assert.Equal(t, gw.Name, binding.Subjects[0].Name)
-		assert.Equal(t, gw.Namespace, binding.Subjects[0].Namespace)
-	})
-}
-
-func TestGatewayClassConfig_ServiceAccountFor(t *testing.T) {
-	t.Run("managed auth", func(t *testing.T) {
-		gcc := &GatewayClassConfig{}
-
-		gw := &gwv1beta1.Gateway{ObjectMeta: metav1.ObjectMeta{Name: t.Name(), Namespace: t.Name()}}
-
-		sa := gcc.ServiceAccountFor(gw)
-		require.NotNil(t, sa)
-		assert.Equal(t, t.Name(), sa.Name)
-		assert.Equal(t, t.Name(), sa.Namespace)
-		assert.Equal(t, labelsForGateway(gw), sa.Labels)
-	})
 }
