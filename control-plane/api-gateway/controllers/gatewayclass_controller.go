5b3ce8f870612ed91039c374896817cda1cc611e
Thomas Eckert <teckert@hashicorp.com>
2023-04-14 17:15:23 -0400

    Use finalizer functions

--- control-plane/api-gateway/controllers/gatewayclass_controller.go
+++ control-plane/api-gateway/controllers/gatewayclass_controller.go
@@ -7,9 +7,10 @@ import (
 	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	gwv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
+
+const gatewayClassFinalizer = "gateway-exists-finalizer.networking.x-k8s.io"
 
 // GatewayClassReconciler reconciles a GatewayClass object.
 // The GatewayClass is responsible for defining the behavior of API gateways
@@ -34,11 +35,6 @@ func (r *GatewayClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	if gc == nil {
-		// We have been deleted. Clean up cached resources.
-		return ctrl.Result{}, deleteGatewayClass(ctx, r.Client, gc)
-	}
-
 	if string(gc.Spec.ControllerName) != r.ControllerName {
 		// This GatewayClass is not for this controller.
 		return ctrl.Result{}, nil
@@ -57,7 +53,7 @@ func (r *GatewayClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 			return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
 		}
 		// Remove our finalizer.
-		if _, err := r.removeFinalizer(ctx, gc); err != nil {
+		if _, err := RemoveFinalizer(ctx, r.Client, gc, gatewayClassFinalizer); err != nil {
 			log.Error(err, "unable to remove finalizer")
 			return ctrl.Result{}, err
 		}
@@ -65,18 +61,14 @@ func (r *GatewayClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	}
 
 	// We are creating or updating the GatewayClass.
-	updated, err := r.ensureFinalizer(ctx, gc)
+	didUpdate, err := EnsureFinalizer(ctx, r.Client, gc, gatewayClassFinalizer)
 	if err != nil {
 		log.Error(err, "unable to add finalizer")
 		return ctrl.Result{}, err
 	}
-	if updated {
+	if didUpdate {
 		// We updated the GatewayClass, requeue to avoid another update.
 		return ctrl.Result{Requeue: true}, nil
-	}
-	if err := r.upsertGatewayClass(ctx, gc); err != nil {
-		log.Error(err, "unable to update GatewayClass")
-		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
@@ -85,31 +77,20 @@ func (r *GatewayClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 func (r *GatewayClassReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&gwv1beta1.GatewayClass{}).
-		For(&gwv1alpha2.GatewayClass{}).
 		Complete(r)
 }
 
-func deleteGatewayClass(ctx context.Context, client client.Client, gatewayClass *gwv1beta1.GatewayClass) error {
-	// TODO: not sure what to do here yet
-	return nil
-}
-
 func (r *GatewayClassReconciler) isGatewayClassInUse(ctx context.Context, gc *gwv1beta1.GatewayClass) (bool, error) {
-	// TODO
-	return false, nil
-}
+	list := &gwv1beta1.GatewayList{}
+	if err := r.Client.List(ctx, list); err != nil {
+		return false, err
+	}
 
-func (r *GatewayClassReconciler) removeFinalizer(ctx context.Context, gc *gwv1beta1.GatewayClass) (bool, error) {
-	// TODO
-	return false, nil
-}
+	for _, gw := range list.Items {
+		if string(gw.Spec.GatewayClassName) == gc.Name {
+			return true, nil
+		}
+	}
 
-func (r *GatewayClassReconciler) ensureFinalizer(ctx context.Context, gc *gwv1beta1.GatewayClass) (bool, error) {
-	// TODO
 	return false, nil
-}
-
-func (r *GatewayClassReconciler) upsertGatewayClass(ctx context.Context, gc *gwv1beta1.GatewayClass) error {
-	// TODO
-	return nil
 }
