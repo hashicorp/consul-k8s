04c6d95d0bfdb2851890100cb1b3119d1c4a417f
Melisa Griffin <missylbytes@users.noreply.github.com>
2023-05-02 07:16:47 -0400

    Updates GatewayClassConfig Controller to use common finalizer methods

--- control-plane/api-gateway/controllers/gateway_class_config_controller.go
+++ control-plane/api-gateway/controllers/gateway_class_config_controller.go
@@ -32,12 +32,12 @@ type GatewayClassConfigController struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *GatewayClassConfigController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	r.Log.Info("Reconciling the Gateway Class Config GatewayClassConfigController", "name", req.Name)
+	log := r.Log.WithValues("gatewayClassConfig", req.NamespacedName.Name)
+	log.Info("Reconciling GatewayClassConfig ")
 
 	gcc := &v1alpha1.GatewayClassConfig{}
 	if err := r.Client.Get(ctx, req.NamespacedName, gcc); err != nil {
 		if k8serrors.IsNotFound(err) {
-			r.Log.Info("gateway class not found")
 			return ctrl.Result{}, nil
 		}
 		r.Log.Error(err, "failed to get gateway class config")
@@ -57,59 +57,19 @@ func (r *GatewayClassConfigController) Reconcile(ctx context.Context, req ctrl.R
 			return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
 		}
 		// gcc is no longer in use.
-		if _, err := removeFinalizer(ctx, r.Client, gcc, gatewayClassConfigFinalizer); err != nil {
+		if _, err := RemoveFinalizer(ctx, r.Client, gcc, gatewayClassConfigFinalizer); err != nil {
 			r.Log.Error(err, "error removing gateway class config finalizer")
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{}, nil
 	}
 
-	if _, err := ensureFinalizer(ctx, r.Client, gcc, gatewayClassConfigFinalizer); err != nil {
+	if _, err := EnsureFinalizer(ctx, r.Client, gcc, gatewayClassConfigFinalizer); err != nil {
 		r.Log.Error(err, "error adding gateway class config finalizer")
 		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
-}
-
-// EnsureFinalizer ensures that the given finalizer is added to the passed
-// object if it does not already exist on the object
-// it returns a boolean saying whether a finalizer was added, and any
-// potential errors.
-func ensureFinalizer(ctx context.Context, k8sClient client.Client, object client.Object, finalizer string) (bool, error) {
-	finalizers := object.GetFinalizers()
-	for _, f := range finalizers {
-		if f == finalizer {
-			return false, nil
-		}
-	}
-	object.SetFinalizers(append(finalizers, finalizer))
-	if err := k8sClient.Update(ctx, object); err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
-// RemoveFinalizer ensures that the given finalizer is removed from the passed object
-// it returns a boolean saying whether a finalizer was removed, and any
-// potential errors.
-func removeFinalizer(ctx context.Context, k8sClient client.Client, object client.Object, finalizer string) (bool, error) {
-	finalizers := []string{}
-	found := false
-	for _, f := range object.GetFinalizers() {
-		if f == finalizer {
-			found = true
-			continue
-		}
-		finalizers = append(finalizers, f)
-	}
-	if found {
-		object.SetFinalizers(finalizers)
-		if err := k8sClient.Update(ctx, object); err != nil {
-			return false, err
-		}
-	}
-	return found, nil
 }
 
 // gatewayClassUsesConfig determines whether a given GatewayClass references a
