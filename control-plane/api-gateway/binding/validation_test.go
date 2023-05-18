a680b894600e499b916906231ed81e25179e6b02
Andrew Stucki <andrew.stucki@hashicorp.com>
2023-05-18 12:42:10 -0400

    Add utils unit tests

--- control-plane/api-gateway/binding/validation_test.go
+++ control-plane/api-gateway/binding/validation_test.go
@@ -1,1 +1,14 @@
 package binding
+
+// func validateRefs(namespace string, refs []gwv1beta1.BackendRef, services map[types.NamespacedName]api.CatalogService) routeValidationResults {
+// func validateGateway(gateway gwv1beta1.Gateway) gatewayValidationResult {
+// func (m mergedListeners) validateProtocol() error {
+// func (m mergedListeners) validateHostname(index int, listener gwv1beta1.Listener) error {
+// func validateTLS(namespace string, tls *gwv1beta1.GatewayTLSConfig, certificates []corev1.Secret) (error, error) {
+// func validateListeners(namespace string, listeners []gwv1beta1.Listener, secrets []corev1.Secret) listenerValidationResults {
+// func routeAllowedForListenerNamespaces(gatewayNamespace string, allowedRoutes *gwv1beta1.AllowedRoutes, namespace corev1.Namespace) bool {
+// func routeAllowedForListenerHostname(hostname *gwv1beta1.Hostname, hostnames []gwv1beta1.Hostname) bool {
+// func hostnamesMatch(a gwv1alpha2.Hostname, b gwv1beta1.Hostname) bool {
+// func routeKindIsAllowedForListener(kinds []gwv1beta1.RouteGroupKind, gk schema.GroupKind) bool {
+// func routeKindIsAllowedForListenerExplicit(allowedRoutes *gwv1alpha2.AllowedRoutes, gk schema.GroupKind) bool {
+// func toNamespaceSet(name string, labels map[string]string) klabels.Labels {
