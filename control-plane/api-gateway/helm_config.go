76e0ce667a63ccf374442aec76426d26d1ee22d1
Thomas Eckert <teckert@hashicorp.com>
2023-05-23 17:37:32 -0400

    Add DeploymentSpec to GatewayClassConfig

--- control-plane/api-gateway/helm_config.go
+++ control-plane/api-gateway/helm_config.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package apigateway
 
 // HelmConfig is the configuration of gateways that comes in from the user's Helm values.
