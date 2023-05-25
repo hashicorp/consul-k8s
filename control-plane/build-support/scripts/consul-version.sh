35593ca1be5cc2dff5209efd8d794c074dce5843
Curt Bushko <cbushko@gmail.com>
2023-05-25 14:43:20 -0400

    better variable
    Update consul image on prepare-dev and prepare-release
    
    remove consul-version script
    
    remove consul-version script
    
    Revert "remove consul-version script"
    
    This reverts commit c7f00f5f6e3d6f18742b21d650ad0f624f4b0f77.
    
    set script/make target to give image version

--- control-plane/build-support/scripts/consul-version.sh
+++ control-plane/build-support/scripts/consul-version.sh
@@ -4,5 +4,4 @@
 FILE=$1
 VERSION=$(yq .global.image $FILE)
 
-# echo full string "hashicorp/consul:1.15.1" | remove first and last characters | cut everything before ':'
-echo "${VERSION}" | sed 's/^.//;s/.$//' | cut -d ':' -f2-
+echo "${VERSION}"
