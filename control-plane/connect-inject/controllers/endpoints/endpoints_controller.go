66424c29a4eab3259b6d62e8cedf9643a40f3555
Chris Chapman <cchapman@hashicorp.com>
2023-05-31 09:51:27 -0700

    Fix telemetry collector issue and add a bat test

--- control-plane/connect-inject/controllers/endpoints/endpoints_controller.go
+++ control-plane/connect-inject/controllers/endpoints/endpoints_controller.go
@@ -487,7 +487,7 @@ func (r *Controller) createServiceRegistrations(pod corev1.Pod, serviceEndpoints
 		proxyConfig.Config[envoyPrometheusBindAddr] = prometheusScrapeListener
 	}
 
-	if r.EnableTelemetryCollector {
+	if r.EnableTelemetryCollector && proxyConfig.Config != nil {
 		proxyConfig.Config[envoyTelemetryCollectorBindSocketDir] = "/consul/connect-inject"
 	}
 
@@ -770,7 +770,7 @@ func (r *Controller) createGatewayRegistrations(pod corev1.Pod, serviceEndpoints
 		}
 	}
 
-	if r.EnableTelemetryCollector {
+	if r.EnableTelemetryCollector && service.Proxy != nil && service.Proxy.Config != nil {
 		service.Proxy.Config[envoyTelemetryCollectorBindSocketDir] = "/consul/service"
 	}
 
