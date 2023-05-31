cb67e6ac296510377b6a870ad8737497cc7b56e8
Nick Ethier <nethier@hashicorp.com>
2023-05-30 20:47:23 -0400

    update cloud preset to enable telemetry collector (#2205)

--- cli/preset/cloud_preset.go
+++ cli/preset/cloud_preset.go
@@ -201,6 +201,8 @@ global:
     bootstrapToken:
       secretName: %s
       secretKey: %s
+  metrics:
+    enableTelemetryCollector: true
   cloud:
     enabled: true
     resourceId:
@@ -215,6 +217,15 @@ global:
     %s
     %s
     %s
+telemetryCollector:
+  enabled: true
+  cloud:
+    clientId:
+      secretName: %s
+      secretKey: %s
+    clientSecret:
+      secretName: %s
+      secretKey: %s
 server:
   replicas: %d
   affinity: null
@@ -231,6 +242,8 @@ controller:
 		secretNameHCPClientID, secretKeyHCPClientID,
 		secretNameHCPClientSecret, secretKeyHCPClientSecret,
 		apiHostCfg, authURLCfg, scadaAddressCfg,
+		secretNameHCPClientID, secretKeyHCPClientID,
+		secretNameHCPClientSecret, secretKeyHCPClientSecret,
 		cfg.BootstrapResponse.Cluster.BootstrapExpect, secretNameServerCert)
 	valuesMap := config.ConvertToMap(values)
 	return valuesMap
