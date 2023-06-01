17df5419e66b0882221306491f8dbe151e78248c
Joshua Timmons <joshua.timmons1@gmail.com>
2023-06-01 19:22:48 -0400

    Add fake demo/crds to get around that expectation in chart install (#2245)

--- charts/embed_chart.go
+++ charts/embed_chart.go
@@ -15,9 +15,9 @@ import "embed"
 //
 // The embed directive does not include files with underscores unless explicitly listed, which is why _helpers.tpl is
 // explicitly embedded.
-//
+
 //go:embed consul/Chart.yaml consul/values.yaml consul/crds consul/templates consul/templates/_helpers.tpl
 var ConsulHelmChart embed.FS
 
-//go:embed demo/Chart.yaml demo/values.yaml demo/templates
+//go:embed demo/Chart.yaml demo/values.yaml demo/crds/blank demo/templates
 var DemoHelmChart embed.FS
