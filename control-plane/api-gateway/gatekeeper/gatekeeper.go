a4ad40525da66788fb1dc2dcb8416f431eb83e4c
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-06-02 15:55:57 -0400

    api-gateway: create RoleBinding attaching Role to ServiceAccount (#2252)
    * Create RoleBinding attaching Role to ServiceAccount
    
    * Update ClusterRole for controller to allow management of RoleBindings
    
    * Separate logic for RoleBinding management from logic for Role
    
    * Use pointer receiver for all functions on Gatekeeper struct
    
    * Use more descriptive name for NamespacedName arg on delete
    
    * Clean up missed code in cherrypick
    
    * Remove out-of-scope TODO
    
    * Make Upsert docstring more robust, explaining dependency ordering
    
    * Add RoleBindings to unit tests for Gatekeeper

--- control-plane/api-gateway/gatekeeper/gatekeeper.go
+++ control-plane/api-gateway/gatekeeper/gatekeeper.go
@@ -30,6 +30,7 @@ func New(log logr.Logger, client client.Client) *Gatekeeper {
 }
 
 // Upsert creates or updates the resources for handling routing of network traffic.
+// This is done in order based on dependencies between resources.
 func (g *Gatekeeper) Upsert(ctx context.Context, gateway gwv1beta1.Gateway, gcc v1alpha1.GatewayClassConfig, config common.HelmConfig) error {
 	g.Log.Info(fmt.Sprintf("Upsert Gateway Deployment %s/%s", gateway.Namespace, gateway.Name))
 
@@ -38,6 +39,10 @@ func (g *Gatekeeper) Upsert(ctx context.Context, gateway gwv1beta1.Gateway, gcc 
 	}
 
 	if err := g.upsertServiceAccount(ctx, gateway, config); err != nil {
+		return err
+	}
+
+	if err := g.upsertRoleBinding(ctx, gateway, gcc, config); err != nil {
 		return err
 	}
 
@@ -53,20 +58,27 @@ func (g *Gatekeeper) Upsert(ctx context.Context, gateway gwv1beta1.Gateway, gcc 
 }
 
 // Delete removes the resources for handling routing of network traffic.
-func (g *Gatekeeper) Delete(ctx context.Context, nsname types.NamespacedName) error {
-	if err := g.deleteRole(ctx, nsname); err != nil {
+// This is done in the reverse order of Upsert due to dependencies between resources.
+func (g *Gatekeeper) Delete(ctx context.Context, gatewayName types.NamespacedName) error {
+	g.Log.Info(fmt.Sprintf("Delete Gateway Deployment %s/%s", gatewayName.Namespace, gatewayName.Name))
+
+	if err := g.deleteDeployment(ctx, gatewayName); err != nil {
 		return err
 	}
 
-	if err := g.deleteServiceAccount(ctx, nsname); err != nil {
+	if err := g.deleteService(ctx, gatewayName); err != nil {
 		return err
 	}
 
-	if err := g.deleteService(ctx, nsname); err != nil {
+	if err := g.deleteRoleBinding(ctx, gatewayName); err != nil {
 		return err
 	}
 
-	if err := g.deleteDeployment(ctx, nsname); err != nil {
+	if err := g.deleteServiceAccount(ctx, gatewayName); err != nil {
+		return err
+	}
+
+	if err := g.deleteRole(ctx, gatewayName); err != nil {
 		return err
 	}
 
@@ -76,14 +88,14 @@ func (g *Gatekeeper) Delete(ctx context.Context, nsname types.NamespacedName) er
 // resourceMutator is passed to create or update functions to mutate Kubernetes resources.
 type resourceMutator = func() error
 
-func (g Gatekeeper) namespacedName(gateway gwv1beta1.Gateway) types.NamespacedName {
+func (g *Gatekeeper) namespacedName(gateway gwv1beta1.Gateway) types.NamespacedName {
 	return types.NamespacedName{
 		Namespace: gateway.Namespace,
 		Name:      gateway.Name,
 	}
 }
 
-func (g Gatekeeper) serviceAccountName(gateway gwv1beta1.Gateway, config common.HelmConfig) string {
+func (g *Gatekeeper) serviceAccountName(gateway gwv1beta1.Gateway, config common.HelmConfig) string {
 	if config.AuthMethod == "" {
 		return ""
 	}
