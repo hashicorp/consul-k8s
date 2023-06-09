2b675e80c51d905d2286f21f326fad748d64276b
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2023-06-09 17:01:58 -0400

    Backport of [API Gateway] Conformance Test Fixes into release/1.2.x (#2329)
    * backport of commit 613c0cc0df52ef8d9c1c496999f0702b0c1dc98a
    
    * backport of commit ac7d0dbdf3780a81cd89de2a9110d934037294cd
    
    * backport of commit c04bd0ff0e304b96bf971fa5fbb232467651756c
    
    * backport of commit 0fe80e78e0090a1b4d40efccf854ee5254a9887a
    
    ---------
    
    Co-authored-by: Andrew Stucki <andrew.stucki@hashicorp.com>
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

--- control-plane/api-gateway/binding/result.go
+++ control-plane/api-gateway/binding/result.go
@@ -213,8 +213,8 @@ func (p parentBindResults) boundSections() mapset.Set {
 }
 
 var (
-	// Each of the below are specified in the Gateway spec under ListenerConditionReason
-	// the general usage is that each error is specified as errListener* where * corresponds
+	// Each of the below are specified in the Gateway spec under ListenerConditionReason.
+	// The general usage is that each error is specified as errListener* where * corresponds
 	// to the ListenerConditionReason given in the spec. If a reason is overloaded and can
 	// be used with two different types of things (i.e. something is not found or it's not supported)
 	// then we distinguish those two usages with errListener*_Usage.
@@ -225,6 +225,8 @@ var (
 	errListenerInvalidCertificateRef_NotFound     = errors.New("certificate not found")
 	errListenerInvalidCertificateRef_NotSupported = errors.New("certificate type is not supported")
 	errListenerInvalidCertificateRef_InvalidData  = errors.New("certificate is invalid or does not contain a supported server name")
+	errListenerInvalidRouteKinds                  = errors.New("allowed route kind is invalid")
+	errListenerProgrammed_Invalid                 = errors.New("listener cannot be programmed because it is invalid")
 
 	// Below is where any custom generic listener validation errors should go.
 	// We map anything under here to a custom ListenerConditionReason of Invalid on
@@ -243,7 +245,36 @@ type listenerValidationResult struct {
 	conflictedErr error
 	// status type: ResolvedRefs
 	refErr error
-	// TODO: programmed
+	// status type: ResolvedRefs (but with internal validation)
+	routeKindErr error
+}
+
+// programmedCondition constructs the condition for the Programmed status type.
+// If there are no validation errors for the listener, we mark it as programmed.
+// If there are validation errors for the listener, we mark it as invalid.
+func (l listenerValidationResult) programmedCondition(generation int64) metav1.Condition {
+	now := timeFunc()
+
+	switch {
+	case l.acceptedErr != nil, l.conflictedErr != nil, l.refErr != nil, l.routeKindErr != nil:
+		return metav1.Condition{
+			Type:               "Programmed",
+			Status:             metav1.ConditionFalse,
+			Reason:             "Invalid",
+			ObservedGeneration: generation,
+			Message:            errListenerProgrammed_Invalid.Error(),
+			LastTransitionTime: now,
+		}
+	default:
+		return metav1.Condition{
+			Type:               "Programmed",
+			Status:             metav1.ConditionTrue,
+			Reason:             "Programmed",
+			ObservedGeneration: generation,
+			Message:            "listener programmed",
+			LastTransitionTime: now,
+		}
+	}
 }
 
 // acceptedCondition constructs the condition for the Accepted status type.
@@ -329,6 +360,17 @@ func (l listenerValidationResult) conflictedCondition(generation int64) metav1.C
 func (l listenerValidationResult) resolvedRefsCondition(generation int64) metav1.Condition {
 	now := timeFunc()
 
+	if l.routeKindErr != nil {
+		return metav1.Condition{
+			Type:               "ResolvedRefs",
+			Status:             metav1.ConditionFalse,
+			Reason:             "InvalidRouteKinds",
+			ObservedGeneration: generation,
+			Message:            l.routeKindErr.Error(),
+			LastTransitionTime: now,
+		}
+	}
+
 	switch l.refErr {
 	case errListenerInvalidCertificateRef_NotFound, errListenerInvalidCertificateRef_NotSupported, errListenerInvalidCertificateRef_InvalidData:
 		return metav1.Condition{
@@ -364,6 +406,7 @@ func (l listenerValidationResult) resolvedRefsCondition(generation int64) metav1
 func (l listenerValidationResult) Conditions(generation int64) []metav1.Condition {
 	return []metav1.Condition{
 		l.acceptedCondition(generation),
+		l.programmedCondition(generation),
 		l.conflictedCondition(generation),
 		l.resolvedRefsCondition(generation),
 	}
