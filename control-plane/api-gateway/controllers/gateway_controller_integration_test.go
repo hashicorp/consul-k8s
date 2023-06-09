6d15fccf82fd397feadc09963cb7fb2641d61d23
jm96441n <john.maguire@hashicorp.com>
2023-06-09 14:59:28 +0000

    backport of commit 04f1e691378ad12adc6b626926ffe6253d45a57f

--- control-plane/api-gateway/controllers/gateway_controller_integration_test.go
+++ control-plane/api-gateway/controllers/gateway_controller_integration_test.go
@@ -64,6 +64,13 @@ func TestControllerDoesNotInfinitelyReconcile(t *testing.T) {
 			httpRouteFn: minimalFieldsSetHTTPRoute,
 			tcpRouteFn:  minimalFieldsSetTCPRoute,
 		},
+		"funky casing to test normalization doesnt cause infinite reconciliation": {
+			namespace:   "",
+			certFn:      createCert,
+			gwFn:        createFunkyCasingFieldsAPIGW,
+			httpRouteFn: createAllFieldsSetHTTPRoute,
+			tcpRouteFn:  createFunkyCasingFieldsTCPRoute,
+		},
 	}
 
 	for name, tc := range testCases {
@@ -281,7 +288,7 @@ func createAllFieldsSetAPIGW(t *testing.T, ctx context.Context, k8sClient client
 	listenerTwoName := "listener-two"
 	listenerTwoHostname := "*.consul.io"
 	listenerTwoPort := 5432
-	listenerTwoProtocol := "HTTP"
+	listenerTwoProtocol := "http"
 
 	// listener three configuration
 	listenerThreeName := "listener-three"
@@ -292,7 +299,7 @@ func createAllFieldsSetAPIGW(t *testing.T, ctx context.Context, k8sClient client
 	listenerFourName := "listener-four"
 	listenerFourHostname := "*.consul.io"
 	listenerFourPort := 5433
-	listenerFourProtocol := "HtTp"
+	listenerFourProtocol := "http"
 
 	// Write gw to k8s
 	gwClassCfg := &v1alpha1.GatewayClassConfig{
@@ -933,6 +940,369 @@ func minimalFieldsSetTCPRoute(t *testing.T, ctx context.Context, k8sClient clien
 								Name: "Service",
 								Port: common.PointerTo(gwv1beta1.PortNumber(25000)),
 							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := k8sClient.Create(ctx, route)
+	require.NoError(t, err)
+
+	return route
+}
+
+func createFunkyCasingFieldsAPIGW(t *testing.T, ctx context.Context, k8sClient client.WithWatch, namespace string) *gwv1beta1.Gateway {
+	// listener one configuration
+	listenerOneName := "listener-one"
+	listenerOneHostname := "*.consul.io"
+	listenerOnePort := 3366
+	listenerOneProtocol := "hTtPs"
+
+	// listener two configuration
+	listenerTwoName := "listener-two"
+	listenerTwoHostname := "*.consul.io"
+	listenerTwoPort := 5432
+	listenerTwoProtocol := "HTTP"
+
+	// listener three configuration
+	listenerThreeName := "listener-three"
+	listenerThreePort := 8081
+	listenerThreeProtocol := "tCp"
+
+	// listener four configuration
+	listenerFourName := "listener-four"
+	listenerFourHostname := "*.consul.io"
+	listenerFourPort := 5433
+	listenerFourProtocol := "hTTp"
+
+	// Write gw to k8s
+	gwClassCfg := &v1alpha1.GatewayClassConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "GatewayClassConfig",
+			APIVersion: "gateway.networking.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "gateway-class-config",
+		},
+		Spec: v1alpha1.GatewayClassConfigSpec{},
+	}
+	gwClass := &gwv1beta1.GatewayClass{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "GatewayClass",
+			APIVersion: "gateway.networking.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "gatewayclass",
+		},
+		Spec: gwv1beta1.GatewayClassSpec{
+			ControllerName: "hashicorp.com/consul-api-gateway-controller",
+			ParametersRef: &gwv1beta1.ParametersReference{
+				Group: "consul.hashicorp.com",
+				Kind:  "GatewayClassConfig",
+				Name:  "gateway-class-config",
+			},
+			Description: new(string),
+		},
+	}
+	gw := &gwv1beta1.Gateway{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Gateway",
+			APIVersion: "gateway.networking.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "gw",
+			Namespace:   namespace,
+			Annotations: make(map[string]string),
+		},
+		Spec: gwv1beta1.GatewaySpec{
+			GatewayClassName: gwv1beta1.ObjectName(gwClass.Name),
+			Listeners: []gwv1beta1.Listener{
+				{
+					Name:     gwv1beta1.SectionName(listenerOneName),
+					Hostname: common.PointerTo(gwv1beta1.Hostname(listenerOneHostname)),
+					Port:     gwv1beta1.PortNumber(listenerOnePort),
+					Protocol: gwv1beta1.ProtocolType(listenerOneProtocol),
+					TLS: &gwv1beta1.GatewayTLSConfig{
+						CertificateRefs: []gwv1beta1.SecretObjectReference{
+							{
+								Kind:      common.PointerTo(gwv1beta1.Kind("Secret")),
+								Name:      gwv1beta1.ObjectName("one-cert"),
+								Namespace: common.PointerTo(gwv1beta1.Namespace(namespace)),
+							},
+						},
+					},
+					AllowedRoutes: &gwv1beta1.AllowedRoutes{
+						Namespaces: &gwv1beta1.RouteNamespaces{
+							From: common.PointerTo(gwv1beta1.FromNamespaces("All")),
+						},
+					},
+				},
+				{
+					Name:     gwv1beta1.SectionName(listenerTwoName),
+					Hostname: common.PointerTo(gwv1beta1.Hostname(listenerTwoHostname)),
+					Port:     gwv1beta1.PortNumber(listenerTwoPort),
+					Protocol: gwv1beta1.ProtocolType(listenerTwoProtocol),
+					AllowedRoutes: &gwv1beta1.AllowedRoutes{
+						Namespaces: &gwv1beta1.RouteNamespaces{
+							From: common.PointerTo(gwv1beta1.FromNamespaces("Same")),
+						},
+					},
+				},
+				{
+					Name:     gwv1beta1.SectionName(listenerThreeName),
+					Port:     gwv1beta1.PortNumber(listenerThreePort),
+					Protocol: gwv1beta1.ProtocolType(listenerThreeProtocol),
+					AllowedRoutes: &gwv1beta1.AllowedRoutes{
+						Namespaces: &gwv1beta1.RouteNamespaces{
+							From: common.PointerTo(gwv1beta1.FromNamespaces("All")),
+						},
+					},
+				},
+				{
+					Name:     gwv1beta1.SectionName(listenerFourName),
+					Hostname: common.PointerTo(gwv1beta1.Hostname(listenerFourHostname)),
+					Port:     gwv1beta1.PortNumber(listenerFourPort),
+					Protocol: gwv1beta1.ProtocolType(listenerFourProtocol),
+					AllowedRoutes: &gwv1beta1.AllowedRoutes{
+						Namespaces: &gwv1beta1.RouteNamespaces{
+							From: common.PointerTo(gwv1beta1.FromNamespaces("Selector")),
+							Selector: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									common.NamespaceNameLabel: "consul",
+								},
+								MatchExpressions: []metav1.LabelSelectorRequirement{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := k8sClient.Create(ctx, gwClassCfg)
+	require.NoError(t, err)
+
+	err = k8sClient.Create(ctx, gwClass)
+	require.NoError(t, err)
+
+	err = k8sClient.Create(ctx, gw)
+	require.NoError(t, err)
+
+	return gw
+}
+
+func createFunkyCasingFieldsHTTPRoute(t *testing.T, ctx context.Context, k8sClient client.WithWatch, gw *gwv1beta1.Gateway) *gwv1beta1.HTTPRoute {
+	svcDefault := &v1alpha1.ServiceDefaults{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "ServiceDefaults",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "Service",
+		},
+		Spec: v1alpha1.ServiceDefaultsSpec{
+			Protocol: "hTtp",
+		},
+	}
+
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Service",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "Service",
+			Labels: map[string]string{"app": "Service"},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "high",
+					Protocol: "TCP",
+					Port:     8080,
+				},
+			},
+			Selector: map[string]string{"app": "Service"},
+		},
+	}
+
+	serviceAccount := &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "Service",
+		},
+	}
+
+	deployment := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "Service",
+			Labels: map[string]string{"app": "Service"},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: common.PointerTo(int32(1)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"app": "Service"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{},
+				Spec:       corev1.PodSpec{},
+			},
+		},
+	}
+
+	err := k8sClient.Create(ctx, svcDefault)
+	require.NoError(t, err)
+
+	err = k8sClient.Create(ctx, svc)
+	require.NoError(t, err)
+
+	err = k8sClient.Create(ctx, serviceAccount)
+	require.NoError(t, err)
+
+	err = k8sClient.Create(ctx, deployment)
+	require.NoError(t, err)
+
+	route := &gwv1beta1.HTTPRoute{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "HTTPRoute",
+			APIVersion: "gateway.networking.k8s.io/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "http-route",
+		},
+		Spec: gwv1beta1.HTTPRouteSpec{
+			CommonRouteSpec: gwv1beta1.CommonRouteSpec{
+				ParentRefs: []gwv1beta1.ParentReference{
+					{
+						Namespace:   (*gwv1beta1.Namespace)(&gw.Namespace),
+						Name:        gwv1beta1.ObjectName(gw.Name),
+						SectionName: &gw.Spec.Listeners[0].Name,
+						Port:        &gw.Spec.Listeners[0].Port,
+					},
+				},
+			},
+			Hostnames: []gwv1beta1.Hostname{"route.consul.io"},
+			Rules: []gwv1beta1.HTTPRouteRule{
+				{
+					Matches: []gwv1beta1.HTTPRouteMatch{
+						{
+							Path: &gwv1beta1.HTTPPathMatch{
+								Type: common.PointerTo(gwv1beta1.PathMatchPathPrefix),
+							},
+							Headers: []gwv1beta1.HTTPHeaderMatch{
+								{
+									Type:  common.PointerTo(gwv1beta1.HeaderMatchExact),
+									Name:  "version",
+									Value: "version",
+								},
+							},
+							QueryParams: []gwv1beta1.HTTPQueryParamMatch{
+								{
+									Type:  common.PointerTo(gwv1beta1.QueryParamMatchExact),
+									Name:  "search",
+									Value: "q",
+								},
+							},
+							Method: common.PointerTo(gwv1beta1.HTTPMethod("GET")),
+						},
+					},
+					Filters: []gwv1beta1.HTTPRouteFilter{
+						{
+							Type: gwv1beta1.HTTPRouteFilterRequestHeaderModifier,
+							RequestHeaderModifier: &gwv1beta1.HTTPHeaderFilter{
+								Set: []gwv1beta1.HTTPHeader{
+									{
+										Name:  "foo",
+										Value: "bax",
+									},
+								},
+								Add: []gwv1beta1.HTTPHeader{
+									{
+										Name:  "arc",
+										Value: "reactor",
+									},
+								},
+								Remove: []string{"remove"},
+							},
+						},
+						{
+							Type: gwv1beta1.HTTPRouteFilterURLRewrite,
+							URLRewrite: &gwv1beta1.HTTPURLRewriteFilter{
+								Hostname: common.PointerTo(gwv1beta1.PreciseHostname("host.com")),
+								Path: &gwv1beta1.HTTPPathModifier{
+									Type:            gwv1beta1.FullPathHTTPPathModifier,
+									ReplaceFullPath: common.PointerTo("/foobar"),
+								},
+							},
+						},
+
+						{
+							Type: gwv1beta1.HTTPRouteFilterURLRewrite,
+							URLRewrite: &gwv1beta1.HTTPURLRewriteFilter{
+								Hostname: common.PointerTo(gwv1beta1.PreciseHostname("host.com")),
+								Path: &gwv1beta1.HTTPPathModifier{
+									Type:               gwv1beta1.PrefixMatchHTTPPathModifier,
+									ReplacePrefixMatch: common.PointerTo("/foo"),
+								},
+							},
+						},
+					},
+					BackendRefs: []gwv1beta1.HTTPBackendRef{
+						{
+							BackendRef: gwv1beta1.BackendRef{
+								BackendObjectReference: gwv1beta1.BackendObjectReference{
+									Name: "Service",
+									Port: common.PointerTo(gwv1beta1.PortNumber(8080)),
+								},
+								Weight: common.PointerTo(int32(-50)),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err = k8sClient.Create(ctx, route)
+	require.NoError(t, err)
+
+	return route
+}
+
+func createFunkyCasingFieldsTCPRoute(t *testing.T, ctx context.Context, k8sClient client.WithWatch, gw *gwv1beta1.Gateway) *v1alpha2.TCPRoute {
+	route := &v1alpha2.TCPRoute{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "TCPRoute",
+			APIVersion: "gateway.networking.k8s.io/v1alpha2",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "tcp-route",
+		},
+		Spec: gwv1alpha2.TCPRouteSpec{
+			CommonRouteSpec: gwv1beta1.CommonRouteSpec{
+				ParentRefs: []gwv1beta1.ParentReference{
+					{
+						Namespace:   (*gwv1beta1.Namespace)(&gw.Namespace),
+						Name:        gwv1beta1.ObjectName(gw.Name),
+						SectionName: &gw.Spec.Listeners[2].Name,
+						Port:        &gw.Spec.Listeners[2].Port,
+					},
+				},
+			},
+			Rules: []gwv1alpha2.TCPRouteRule{
+				{
+					BackendRefs: []gwv1beta1.BackendRef{
+						{
+							BackendObjectReference: gwv1beta1.BackendObjectReference{
+								Name: "Service",
+								Port: common.PointerTo(gwv1beta1.PortNumber(25000)),
+							},
+							Weight: common.PointerTo(int32(-50)),
 						},
 					},
 				},
