ccc0daa9531b069157df15ce6080c16416261b8c
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2023-06-12 14:45:58 +0530

    fix docker file

--- control-plane/connect-inject/webhook/container_init.go
+++ control-plane/connect-inject/webhook/container_init.go
@@ -168,7 +168,7 @@ func (w *MeshWebhook) containerInit(namespace corev1.Namespace, pod corev1.Pod, 
 		},
 		Resources:    w.InitContainerResources,
 		VolumeMounts: volMounts,
-		Command:      []string{initContainerCommandInterpreter, "", buf.String()},
+		Command:      []string{initContainerCommandInterpreter, "-ec", buf.String()},
 	}
 
 	if w.TLSEnabled {
