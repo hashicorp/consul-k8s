15d61b6db4afce1b9c7e982881a7f51dcfb2c1fc
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2023-06-13 16:44:09 +0530

    Added test -is-windows

--- control-plane/subcommand/connect-init/command_test.go
+++ control-plane/subcommand/connect-init/command_test.go
@@ -57,6 +57,7 @@ func TestRun_FlagValidation(t *testing.T) {
 				"-service-account-name", "foo",
 				"-log-level", "invalid",
 				"-consul-node-name", "bar",
+				"-is-windows", "true",
 			},
 			expErr: "unknown log level: invalid",
 		},
