253209f12b9de5da1cffe0414b82921d9e759439
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-06-13 13:28:48 +0000

    backport of commit 9fed79d304013d6fa9a78373692242e443d29ae8

--- control-plane/api-gateway/cache/gateway.go
+++ control-plane/api-gateway/cache/gateway.go
@@ -18,7 +18,7 @@ import (
 )
 
 type GatewayCache struct {
-	config    *consul.Config
+	config    Config
 	serverMgr consul.ServerConnectionManager
 	logger    logr.Logger
 
@@ -35,7 +35,7 @@ type GatewayCache struct {
 
 func NewGatewayCache(ctx context.Context, config Config) *GatewayCache {
 	return &GatewayCache{
-		config:             config.ConsulClientConfig,
+		config:             config,
 		serverMgr:          config.ConsulServerConnMgr,
 		logger:             config.Logger,
 		events:             make(chan event.GenericEvent),
@@ -53,13 +53,13 @@ func (r *GatewayCache) ServicesFor(ref api.ResourceReference) []api.CatalogServi
 }
 
 func (r *GatewayCache) FetchServicesFor(ctx context.Context, ref api.ResourceReference) ([]api.CatalogService, error) {
-	client, err := consul.NewClientFromConnMgr(r.config, r.serverMgr)
+	client, err := consul.NewClientFromConnMgr(r.config.ConsulClientConfig, r.serverMgr)
 	if err != nil {
 		return nil, err
 	}
 
 	opts := &api.QueryOptions{}
-	if ref.Namespace != "" {
+	if r.config.NamespacesEnabled && ref.Namespace != "" {
 		opts.Namespace = ref.Namespace
 	}
 
@@ -95,7 +95,7 @@ func (r *GatewayCache) RemoveSubscription(ref api.ResourceReference) {
 
 func (r *GatewayCache) subscribeToGateway(ctx context.Context, ref api.ResourceReference, resource types.NamespacedName) {
 	opts := &api.QueryOptions{}
-	if ref.Namespace != "" {
+	if r.config.NamespacesEnabled && ref.Namespace != "" {
 		opts.Namespace = ref.Namespace
 	}
 
@@ -117,7 +117,7 @@ func (r *GatewayCache) subscribeToGateway(ctx context.Context, ref api.ResourceR
 		retryBackoff := backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 10)
 
 		if err := backoff.Retry(func() error {
-			client, err := consul.NewClientFromConnMgr(r.config, r.serverMgr)
+			client, err := consul.NewClientFromConnMgr(r.config.ConsulClientConfig, r.serverMgr)
 			if err != nil {
 				return err
 			}
