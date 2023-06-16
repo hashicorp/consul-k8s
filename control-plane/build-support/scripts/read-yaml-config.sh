2c23f40eb7446ae7feda9bc6c1dd41c68bfad358
Michael Wilkerson <mwilkerson@hashicorp.com>
2023-06-16 10:01:57 -0700

    updated input file with upper case

--- control-plane/build-support/scripts/read-yaml-config.sh
+++ control-plane/build-support/scripts/read-yaml-config.sh
@@ -2,9 +2,9 @@
 
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-input_file=$1
+INPUT_FILE=$1
 
 # Convert YAML content to JSON as it is easier to deal with
-JSON_CONTENTS=$(yq eval '. | tojson' "${input_file}")
+JSON_CONTENTS=$(yq eval '. | tojson' "${INPUT_FILE}")
 
 echo "${JSON_CONTENTS}"
