f6f41b2d14b0eec2c1b82e128510508aa509dffd
Curt Bushko <cbushko@gmail.com>
2023-06-28 13:01:51 -0400

    Backport of Proxy Lifecycle helm, connect-inject and acceptance tests into release/1.0.x (#2233) (#2467)
    * Proxy Lifecycle helm, connect-inject and acceptance tests (#2233)
    
    Proxy Lifecycle helm, connect-inject and acceptance tests (#2233)
    
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>
    
    * disable lifecycle test
    
    ---------
    
    Co-authored-by: Mike Morris <mikemorris@users.noreply.github.com>
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>
    Co-authored-by: Michael Wilkerson <mwilkerson@hashicorp.com>

--- acceptance/framework/flags/flags.go
+++ acceptance/framework/flags/flags.go
@@ -31,11 +31,13 @@ type TestFlags struct {
 
 	flagEnableTransparentProxy bool
 
-	flagHelmChartVersion string
-	flagConsulImage      string
-	flagConsulK8sImage   string
-	flagConsulVersion    string
-	flagEnvoyImage       string
+	flagHelmChartVersion       string
+	flagConsulImage            string
+	flagConsulK8sImage         string
+	flagConsulDataplaneImage   string
+	flagConsulVersion          string
+	flagConsulDataplaneVersion string
+	flagEnvoyImage             string
 
 	flagNoCleanupOnFailure bool
 
@@ -67,7 +69,9 @@ func (t *TestFlags) init() {
 
 	flag.StringVar(&t.flagConsulImage, "consul-image", "", "The Consul image to use for all tests.")
 	flag.StringVar(&t.flagConsulK8sImage, "consul-k8s-image", "", "The consul-k8s image to use for all tests.")
+	flag.StringVar(&t.flagConsulDataplaneImage, "consul-dataplane-image", "", "The consul-dataplane image to use for all tests.")
 	flag.StringVar(&t.flagConsulVersion, "consul-version", "", "The consul version used for all tests.")
+	flag.StringVar(&t.flagConsulDataplaneVersion, "consul-dataplane-version", "", "The consul-dataplane version used for all tests.")
 	flag.StringVar(&t.flagHelmChartVersion, "helm-chart-version", config.HelmChartPath, "The helm chart used for all tests.")
 	flag.StringVar(&t.flagEnvoyImage, "envoy-image", "", "The Envoy image to use for all tests.")
 
@@ -142,6 +146,8 @@ func (t *TestFlags) TestConfigFromFlags() *config.TestConfig {
 
 	// if the Version is empty consulVersion will be nil
 	consulVersion, _ := version.NewVersion(t.flagConsulVersion)
+	consulDataplaneVersion, _ := version.NewVersion(t.flagConsulDataplaneVersion)
+	//vaultserverVersion, _ := version.NewVersion(t.flagVaultServerVersion)
 
 	return &config.TestConfig{
 		Kubeconfig:    t.flagKubeconfig,
@@ -166,11 +172,13 @@ func (t *TestFlags) TestConfigFromFlags() *config.TestConfig {
 
 		DisablePeering: t.flagDisablePeering,
 
-		HelmChartVersion: t.flagHelmChartVersion,
-		ConsulImage:      t.flagConsulImage,
-		ConsulK8SImage:   t.flagConsulK8sImage,
-		ConsulVersion:    consulVersion,
-		EnvoyImage:       t.flagEnvoyImage,
+		HelmChartVersion:       t.flagHelmChartVersion,
+		ConsulImage:            t.flagConsulImage,
+		ConsulK8SImage:         t.flagConsulK8sImage,
+		ConsulDataplaneImage:   t.flagConsulDataplaneImage,
+		ConsulVersion:          consulVersion,
+		ConsulDataplaneVersion: consulDataplaneVersion,
+		EnvoyImage:             t.flagEnvoyImage,
 
 		NoCleanupOnFailure: t.flagNoCleanupOnFailure,
 		DebugDirectory:     tempDir,
