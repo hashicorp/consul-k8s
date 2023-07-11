fc01090e3356cce779d64add7a6bd8addbb6422b
Michael Wilkerson <mwilkerson@hashicorp.com>
2023-07-11 20:26:37 +0000

    backport of commit 5d4d5e464424ce8ef92e1b2c0348ea690a7c4c73

--- acceptance/framework/consul/cli_cluster.go
+++ acceptance/framework/consul/cli_cluster.go
@@ -97,16 +97,17 @@ func NewCLICluster(
 	cli, err := cli.NewCLI()
 	require.NoError(t, err)
 
+	require.Greater(t, len(cfg.KubeEnvs), 0)
 	return &CLICluster{
 		ctx:                ctx,
 		helmOptions:        hopts,
 		kubectlOptions:     kopts,
-		namespace:          cfg.KubeNamespace,
+		namespace:          cfg.GetPrimaryKubeEnv().KubeNamespace,
 		values:             values,
 		releaseName:        releaseName,
 		kubernetesClient:   ctx.KubernetesClient(t),
-		kubeConfig:         cfg.Kubeconfig,
-		kubeContext:        cfg.KubeContext,
+		kubeConfig:         cfg.GetPrimaryKubeEnv().KubeConfig,
+		kubeContext:        cfg.GetPrimaryKubeEnv().KubeContext,
 		noCleanupOnFailure: cfg.NoCleanupOnFailure,
 		debugDirectory:     cfg.DebugDirectory,
 		logger:             logger,
