128d8a70c693a30a5cc9818e16c5738ad16cb4f0
Michael Wilkerson <mwilkerson@hashicorp.com>
2023-07-20 11:05:20 -0700

    added check to prepare-release make target

--- control-plane/build-support/scripts/check-hashicorppreview.sh
+++ control-plane/build-support/scripts/check-hashicorppreview.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-if grep -rnw -e 'hashicorppreviewsadfsd'  './charts'; then
-    echo charts contain hashicorppreview images
+echo "Checking charts for hashicorpreview images. . ."
+if grep -rnw -e 'hashicorppreview'  './charts'; then
+    echo Charts contain hashicorppreview images. If this is intended for release, please remove the preview images.
 else
-    echo charts do not contain hashicorpreview images
+    echo Charts do not contain hashicorpreview images, ready for release!
 fi
