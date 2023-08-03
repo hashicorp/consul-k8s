939e7c31993b0f3c83feceea79ae6c2e3064f700
Curt Bushko <cbushko@gmail.com>
2023-08-03 16:43:08 -0400

    Acceptance tests: increase api-gateway retries (#2716)
    * Increase the retries and add config entry retries

--- acceptance/tests/api-gateway/api_gateway_test.go
+++ acceptance/tests/api-gateway/api_gateway_test.go
@@ -101,6 +101,11 @@ func TestAPIGateway_Basic(t *testing.T) {
 			logger.Log(t, "creating target http server")
 			k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.NoCleanup, cfg.DebugDirectory, "../fixtures/cases/static-server-inject")
 
+			// We use the static-client pod so that we can make calls to the api gateway
+			// via kubectl exec without needing a route into the cluster from the test machine.
+			logger.Log(t, "creating static-client pod")
+			k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.NoCleanup, cfg.DebugDirectory, "../fixtures/bases/static-client")
+
 			logger.Log(t, "patching route to target http server")
 			k8s.RunKubectl(t, ctx.KubectlOptions(t), "patch", "httproute", "http-route", "-p", `{"spec":{"rules":[{"backendRefs":[{"name":"static-server","port":80}]}]}}`, "--type=merge")
 
@@ -115,11 +120,6 @@ func TestAPIGateway_Basic(t *testing.T) {
 				k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "delete", "-f", "../fixtures/cases/api-gateways/tcproute/route.yaml")
 			})
 
-			// We use the static-client pod so that we can make calls to the api gateway
-			// via kubectl exec without needing a route into the cluster from the test machine.
-			logger.Log(t, "creating static-client pod")
-			k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.NoCleanup, cfg.DebugDirectory, "../fixtures/bases/static-client")
-
 			// Grab a kubernetes client so that we can verify binding
 			// behavior prior to issuing requests through the gateway.
 			k8sClient := ctx.ControllerRuntimeClient(t)
@@ -128,7 +128,7 @@ func TestAPIGateway_Basic(t *testing.T) {
 			// leader election so we may need to wait a long time for
 			// the reconcile loop to run (hence the 1m timeout here).
 			var gatewayAddress string
-			counter := &retry.Counter{Count: 60, Wait: 2 * time.Second}
+			counter := &retry.Counter{Count: 120, Wait: 2 * time.Second}
 			retry.RunWith(counter, t, func(r *retry.R) {
 				var gateway gwv1beta1.Gateway
 				err := k8sClient.Get(context.Background(), types.NamespacedName{Name: "gateway", Namespace: "default"}, &gateway)
@@ -209,17 +209,22 @@ func TestAPIGateway_Basic(t *testing.T) {
 			checkStatusCondition(t, tcpRoute.Status.Parents[0].Conditions, trueCondition("ConsulAccepted", "Accepted"))
 
 			// check that the Consul entries were created
-			entry, _, err := consulClient.ConfigEntries().Get(api.APIGateway, "gateway", nil)
-			require.NoError(t, err)
-			gateway := entry.(*api.APIGatewayConfigEntry)
+			var gateway *api.APIGatewayConfigEntry
+			var httpRoute *api.HTTPRouteConfigEntry
+			var route *api.TCPRouteConfigEntry
+			retry.RunWith(counter, t, func(r *retry.R) {
+				entry, _, err := consulClient.ConfigEntries().Get(api.APIGateway, "gateway", nil)
+				require.NoError(r, err)
+				gateway = entry.(*api.APIGatewayConfigEntry)
 
-			entry, _, err = consulClient.ConfigEntries().Get(api.HTTPRoute, "http-route", nil)
-			require.NoError(t, err)
-			httpRoute := entry.(*api.HTTPRouteConfigEntry)
+				entry, _, err = consulClient.ConfigEntries().Get(api.HTTPRoute, "http-route", nil)
+				require.NoError(r, err)
+				httpRoute = entry.(*api.HTTPRouteConfigEntry)
 
-			entry, _, err = consulClient.ConfigEntries().Get(api.TCPRoute, "tcp-route", nil)
-			require.NoError(t, err)
-			route := entry.(*api.TCPRouteConfigEntry)
+				entry, _, err = consulClient.ConfigEntries().Get(api.TCPRoute, "tcp-route", nil)
+				require.NoError(r, err)
+				route = entry.(*api.TCPRouteConfigEntry)
+			})
 
 			// now check the gateway status conditions
 			checkConsulStatusCondition(t, gateway.Status.Conditions, trueConsulCondition("Accepted", "Accepted"))
