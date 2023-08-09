a287fce551affef7d358394032e8997f79220c8b
John Maguire <john.maguire@hashicorp.com>
2023-08-09 16:03:14 -0400

    NET-4993 JWT auth basic acceptance test (#2706)
    * JWT auth basic acceptance test
    
    * Update to run only in enterprise mode, update comment to be correct
    
    * Remove usage of `testing.t` in retry block
    
    * Fixed last `t` in retry block in tests
    
    * Update acceptance/tests/api-gateway/api_gateway_test.go
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>
    
    * Update acceptance/tests/api-gateway/api_gateway_test.go
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>
    
    * Updating filenames for gw jwt cases and adding message about why this
    test is skipped
    
    ---------
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

--- acceptance/tests/api-gateway/api_gateway_test.go
+++ acceptance/tests/api-gateway/api_gateway_test.go
@@ -126,7 +126,7 @@ func TestAPIGateway_Basic(t *testing.T) {
 
 			// On startup, the controller can take upwards of 1m to perform
 			// leader election so we may need to wait a long time for
-			// the reconcile loop to run (hence the 1m timeout here).
+			// the reconcile loop to run (hence the timeout here).
 			var gatewayAddress string
 			counter := &retry.Counter{Count: 120, Wait: 2 * time.Second}
 			retry.RunWith(counter, t, func(r *retry.R) {
@@ -286,6 +286,257 @@ func TestAPIGateway_Basic(t *testing.T) {
 			k8s.CheckStaticServerConnectionSuccessful(t, k8sOptions, StaticClientName, targetHTTPSAddress, "-k")
 		})
 	}
+}
+
+func TestAPIGateway_JWTAuth_Basic(t *testing.T) {
+	t.Skip("skipping this test until GW JWT auth is complete")
+	ctx := suite.Environment().DefaultContext(t)
+	cfg := suite.Config()
+
+	if !cfg.EnableEnterprise {
+		t.Skipf("skipping this test because -enable-enterprise is not set")
+	}
+
+	helmValues := map[string]string{
+		"connectInject.enabled":                       "true",
+		"connectInject.consulNamespaces.mirroringK8S": "true",
+		"global.acls.manageSystemACLs":                "true", // acls must be enabled for JWT auth to take place
+		"global.tls.enabled":                          "true",
+		"global.logLevel":                             "trace",
+	}
+
+	releaseName := helpers.RandomName()
+	consulCluster := consul.NewHelmCluster(t, helmValues, ctx, cfg, releaseName)
+
+	consulCluster.Create(t)
+
+	// Override the default proxy config settings for this test
+	consulClient, _ := consulCluster.SetupConsulClient(t, true)
+	_, _, err := consulClient.ConfigEntries().Set(&api.ProxyConfigEntry{
+		Kind: api.ProxyDefaults,
+		Name: api.ProxyConfigGlobal,
+		Config: map[string]interface{}{
+			"protocol": "http",
+		},
+	}, nil)
+	require.NoError(t, err)
+
+	logger.Log(t, "creating api-gateway resources")
+	out, err := k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "apply", "-k", "../fixtures/cases/api-gateways/jwt-auth")
+	require.NoError(t, err, out)
+	helpers.Cleanup(t, cfg.NoCleanupOnFailure, cfg.NoCleanup, func() {
+		// Ignore errors here because if the test ran as expected
+		// the custom resources will have been deleted.
+		k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "delete", "-k", "../fixtures/cases/api-gateways/jwt-auth")
+	})
+
+	// Create certificate secret, we do this separately since
+	// applying the secret will make an invalid certificate that breaks other tests
+	logger.Log(t, "creating certificate secret")
+	out, err = k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "apply", "-f", "../fixtures/bases/api-gateway/certificate.yaml")
+	require.NoError(t, err, out)
+	helpers.Cleanup(t, cfg.NoCleanupOnFailure, cfg.NoCleanup, func() {
+		// Ignore errors here because if the test ran as expected
+		// the custom resources will have been deleted.
+		k8s.RunKubectlAndGetOutputE(t, ctx.KubectlOptions(t), "delete", "-f", "../fixtures/bases/api-gateway/certificate.yaml")
+	})
+
+	// patch certificate with data
+	logger.Log(t, "patching certificate secret with generated data")
+	certificate := generateCertificate(t, nil, "gateway.test.local")
+	k8s.RunKubectl(t, ctx.KubectlOptions(t), "patch", "secret", "certificate", "-p", fmt.Sprintf(`{"data":{"tls.crt":"%s","tls.key":"%s"}}`, base64.StdEncoding.EncodeToString(certificate.CertPEM), base64.StdEncoding.EncodeToString(certificate.PrivateKeyPEM)), "--type=merge")
+
+	// We use the static-client pod so that we can make calls to the api gateway
+	// via kubectl exec without needing a route into the cluster from the test machine.
+	logger.Log(t, "creating static-client pod")
+	k8s.DeployKustomize(t, ctx.KubectlOptions(t), cfg.NoCleanupOnFailure, cfg.NoCleanup, cfg.DebugDirectory, "../fixtures/bases/static-client")
+
+	k8s.RunKubectl(t, ctx.KubectlOptions(t), "wait", "--for=condition=available", "--timeout=5m", fmt.Sprintf("deploy/%s", "static-server"))
+	// Grab a kubernetes client so that we can verify binding
+	// behavior prior to issuing requests through the gateway.
+	k8sClient := ctx.ControllerRuntimeClient(t)
+
+	// On startup, the controller can take upwards of 1m to perform
+	// leader election so we may need to wait a long time for
+	// the reconcile loop to run (hence the 2m timeout here).
+	var (
+		gatewayAddress string
+		gatewayClass   gwv1beta1.GatewayClass
+		httpRoute      gwv1beta1.HTTPRoute
+		httpRouteAuth  gwv1beta1.HTTPRoute
+	)
+
+	counter := &retry.Counter{Count: 60, Wait: 2 * time.Second}
+	retry.RunWith(counter, t, func(r *retry.R) {
+		var gateway gwv1beta1.Gateway
+		err := k8sClient.Get(context.Background(), types.NamespacedName{Name: "gateway", Namespace: "default"}, &gateway)
+		require.NoError(r, err)
+
+		// check our finalizers
+		require.Len(r, gateway.Finalizers, 1)
+		require.EqualValues(r, gatewayFinalizer, gateway.Finalizers[0])
+
+		// check our statuses
+		checkStatusCondition(r, gateway.Status.Conditions, trueCondition("Accepted", "Accepted"))
+		checkStatusCondition(r, gateway.Status.Conditions, trueCondition("ConsulAccepted", "Accepted"))
+		require.Len(r, gateway.Status.Listeners, 4)
+
+		require.EqualValues(r, 1, gateway.Status.Listeners[0].AttachedRoutes)
+		checkStatusCondition(r, gateway.Status.Listeners[0].Conditions, trueCondition("Accepted", "Accepted"))
+		checkStatusCondition(r, gateway.Status.Listeners[0].Conditions, falseCondition("Conflicted", "NoConflicts"))
+		checkStatusCondition(r, gateway.Status.Listeners[0].Conditions, trueCondition("ResolvedRefs", "ResolvedRefs"))
+		require.EqualValues(r, 1, gateway.Status.Listeners[1].AttachedRoutes)
+		checkStatusCondition(r, gateway.Status.Listeners[1].Conditions, trueCondition("Accepted", "Accepted"))
+		checkStatusCondition(r, gateway.Status.Listeners[1].Conditions, falseCondition("Conflicted", "NoConflicts"))
+		checkStatusCondition(r, gateway.Status.Listeners[1].Conditions, trueCondition("ResolvedRefs", "ResolvedRefs"))
+
+		// check that we have an address to use
+		require.Len(r, gateway.Status.Addresses, 1)
+		// now we know we have an address, set it so we can use it
+		gatewayAddress = gateway.Status.Addresses[0].Value
+
+		// gateway class checks
+		err = k8sClient.Get(context.Background(), types.NamespacedName{Name: "gateway-class"}, &gatewayClass)
+		require.NoError(r, err)
+
+		// check our finalizers
+		require.Len(r, gatewayClass.Finalizers, 1)
+		require.EqualValues(r, gatewayClassFinalizer, gatewayClass.Finalizers[0])
+
+		// http route checks
+		err = k8sClient.Get(context.Background(), types.NamespacedName{Name: "http-route", Namespace: "default"}, &httpRoute)
+		require.NoError(r, err)
+
+		// http route checks
+		err = k8sClient.Get(context.Background(), types.NamespacedName{Name: "http-route-auth", Namespace: "default"}, &httpRouteAuth)
+		require.NoError(r, err)
+
+		// check our finalizers
+		require.Len(r, httpRoute.Finalizers, 1)
+		require.EqualValues(r, gatewayFinalizer, httpRoute.Finalizers[0])
+
+		// check parent status
+		require.Len(r, httpRoute.Status.Parents, 1)
+		require.EqualValues(r, gatewayClassControllerName, httpRoute.Status.Parents[0].ControllerName)
+		require.EqualValues(r, "gateway", httpRoute.Status.Parents[0].ParentRef.Name)
+		checkStatusCondition(r, httpRoute.Status.Parents[0].Conditions, trueCondition("Accepted", "Accepted"))
+		checkStatusCondition(r, httpRoute.Status.Parents[0].Conditions, trueCondition("ResolvedRefs", "ResolvedRefs"))
+		checkStatusCondition(r, httpRoute.Status.Parents[0].Conditions, trueCondition("ConsulAccepted", "Accepted"))
+
+		// check our finalizers
+		require.Len(r, httpRouteAuth.Finalizers, 1)
+		require.EqualValues(r, gatewayFinalizer, httpRouteAuth.Finalizers[0])
+
+		// check parent status
+		require.Len(r, httpRouteAuth.Status.Parents, 1)
+		require.EqualValues(r, gatewayClassControllerName, httpRouteAuth.Status.Parents[0].ControllerName)
+		require.EqualValues(r, "gateway", httpRouteAuth.Status.Parents[0].ParentRef.Name)
+		checkStatusCondition(r, httpRouteAuth.Status.Parents[0].Conditions, trueCondition("Accepted", "Accepted"))
+		checkStatusCondition(r, httpRouteAuth.Status.Parents[0].Conditions, trueCondition("ResolvedRefs", "ResolvedRefs"))
+		checkStatusCondition(r, httpRouteAuth.Status.Parents[0].Conditions, trueCondition("ConsulAccepted", "Accepted"))
+	})
+
+	// check that the Consul entries were created
+	entry, _, err := consulClient.ConfigEntries().Get(api.APIGateway, "gateway", nil)
+	require.NoError(t, err)
+	gateway := entry.(*api.APIGatewayConfigEntry)
+
+	entry, _, err = consulClient.ConfigEntries().Get(api.HTTPRoute, "http-route", nil)
+	require.NoError(t, err)
+	consulHTTPRoute := entry.(*api.HTTPRouteConfigEntry)
+
+	entry, _, err = consulClient.ConfigEntries().Get(api.HTTPRoute, "http-route-auth", nil)
+	require.NoError(t, err)
+	consulHTTPRouteAuth := entry.(*api.HTTPRouteConfigEntry)
+
+	// now check the gateway status conditions
+	checkConsulStatusCondition(t, gateway.Status.Conditions, trueConsulCondition("Accepted", "Accepted"))
+
+	// and the route status conditions
+	checkConsulStatusCondition(t, consulHTTPRoute.Status.Conditions, trueConsulCondition("Bound", "Bound"))
+	checkConsulStatusCondition(t, consulHTTPRouteAuth.Status.Conditions, trueConsulCondition("Bound", "Bound"))
+
+	// finally we check that we can actually route to the service(s) via the gateway
+	k8sOptions := ctx.KubectlOptions(t)
+	targetHTTPAddress := fmt.Sprintf("http://%s/v1", gatewayAddress)
+	targetHTTPAddressAdmin := fmt.Sprintf("http://%s:8080/admin", gatewayAddress)
+	targetHTTPAddressPet := fmt.Sprintf("http://%s:8080/pet", gatewayAddress)
+	// valid JWT token with role of "doctor"
+	doctorToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJQUzI1NiIsImtpZCI6IkMtRTFuQ2p3Z0JDLVB1R00yTzQ2N0ZSRGhLeDhBa1ZjdElTQWJvM3JpZXcifQ.eyJpc3MiOiJsb2NhbCIsInJvbGUiOiJkb2N0b3IifQ.FfgpzjMf8Evh6K-fJ1cLXklfIXOm-vojVbWlPPbGVFtzxZ9hxMxoyAY_G8i36SfGrpUlp-RJ6ohMvprMrEgyRgbenu7u5kkm5iGHW-zpMus4izXRxPELBcpWOGF105HIssT2NYRstXieNR8EVzvGfLdvR0GW8ttEERgseqGvuAfdb4-aNYsysGwUUHbsZjazA6H1rZmWqHdCLOJ2ZwFsIdckO9CadnkyTILpcPUmLYyUVJdtlLGOySb0GG8c_dPML_IR5jSXCSUZt6S2JBNBNBdqukrlqpA-fIaaWft0dbWVMhv8DqPC8znult8dKvLZ1qXeU0itsqqJUyE16ihJjw"
+	// valid JWT token with role of "pet"
+	petToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJQUzI1NiIsImtpZCI6IkMtRTFuQ2p3Z0JDLVB1R00yTzQ2N0ZSRGhLeDhBa1ZjdElTQWJvM3JpZXcifQ.eyJpc3MiOiJsb2NhbCIsInJvbGUiOiJwZXQifQ.l94rJayGGTMB426HwEw5ipSjaIHjm-UWDHiBAlB_Slmi814AxAfl_0AdRwSz67UDnkoygKbvPpR5xUB03JCXNshLZuKLegWsBeQg_OJYvZGmFagl5NglBFvH7Jbta4e1eQoAxZI6Xyy1jHbu7jFBjQPVnK8EaRvWoW8Pe8a8rp_5xhub0pomhvRF6Pm5kAS4cMnxvqpVc5Oo5nO7ws_SmoNnbt2Ok14k23Zx5E2EWmGStOfbgFsdbhVbepB2DMzqv1j8jvBbwa_OxCwc_7pEOthOOxRV6L3ZjgbRSB4GumlXAOCBYXD1cRLgrMSrWB1GkefAKu8PV0Ho1px6sI9Evg"
+
+	// check that intentions keep our connection from happening
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, targetHTTPAddress)
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, targetHTTPAddressAdmin)
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", doctorToken, targetHTTPAddressAdmin)
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", petToken, targetHTTPAddressAdmin)
+
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, targetHTTPAddressPet)
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", doctorToken, targetHTTPAddressPet)
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", petToken, targetHTTPAddressPet)
+
+	// Now we create the allow intention.
+	_, _, err = consulClient.ConfigEntries().Set(&api.ServiceIntentionsConfigEntry{
+		Kind: api.ServiceIntentions,
+		Name: "static-server",
+		Sources: []*api.SourceIntention{
+			{
+				Name:   "gateway",
+				Action: api.IntentionActionAllow,
+			},
+		},
+	}, nil)
+	require.NoError(t, err)
+
+	_, _, err = consulClient.ConfigEntries().Set(&api.ServiceIntentionsConfigEntry{
+		Kind: api.ServiceIntentions,
+		Name: "static-server-protected",
+		Sources: []*api.SourceIntention{
+			{
+				Name:   "gateway",
+				Action: api.IntentionActionAllow,
+			},
+		},
+	}, nil)
+	require.NoError(t, err)
+
+	// Test that we can make a call to the api gateway
+	logger.Log(t, "trying calls to api gateway http")
+	k8s.CheckStaticServerConnectionSuccessful(t, k8sOptions, StaticClientName, targetHTTPAddress)
+
+	// ensure that overrides -> route extension -> default by making a request to the admin route with a JWT that has an issuer of "local" and a "role" of "doctor"
+	// we can see that:
+	// * the "iss" verification in the gateway override takes precedence over the "iss" verification in the route filter
+	// * the "role" verification in the route extension takes precedence over the "role" verification in the gateway default
+	// should fail because we're missing JWT
+	logger.Log(t, "trying calls to api gateway /admin should fail without JWT token")
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, targetHTTPAddressAdmin)
+
+	// should fail because we use the token with the wrong role and correct issuer
+	logger.Log(t, "trying calls to api gateway /admin should fail with wrong role")
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", petToken, targetHTTPAddressAdmin)
+
+	// will succeed because we use the token with the correct role and the correct issuer
+	logger.Log(t, "trying calls to api gateway /admin should succeed with JWT token with correct role")
+	k8s.CheckStaticServerConnectionSuccessful(t, k8sOptions, StaticClientName, "-H", doctorToken, targetHTTPAddressAdmin)
+
+	// ensure that overrides -> route extension -> default by making a request to the admin route with a JWT that has an issuer of "local" and a "role" of "pet"
+	// the route does not define
+	// we can see that:
+	// * the "iss" verification in the gateway override takes precedence over the "iss" verification in the route filter
+	// * the "role" verification in the route extension takes precedence over the "role" verification in the gateway default
+	// should fail because we're missing JWT
+	logger.Log(t, "trying calls to api gateway /pet should fail without JWT token")
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, targetHTTPAddressPet)
+
+	// should fail because we use the token with the wrong role and correct issuer
+	logger.Log(t, "trying calls to api gateway /pet should fail with wrong role")
+	k8s.CheckStaticServerHTTPConnectionFailing(t, k8sOptions, StaticClientName, "-H", doctorToken, targetHTTPAddressPet)
+
+	// will succeed because we use the token with the correct role and the correct issuer
+	logger.Log(t, "trying calls to api gateway /pet should succeed with JWT token with correct role")
+	k8s.CheckStaticServerConnectionSuccessful(t, k8sOptions, StaticClientName, "-H", petToken, targetHTTPAddressPet)
 }
 
 func checkStatusCondition(t require.TestingT, conditions []metav1.Condition, toCheck metav1.Condition) {
