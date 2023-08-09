2a232acd63bd13fedc979c6b6c1bd6277712a0b6
aahel <aahel.guha@hashicorp.com>
2023-08-09 12:30:08 +0530

    setting nodeSelectors in values.yaml instead of test config

--- acceptance/framework/config/config.go
+++ acceptance/framework/config/config.go
@@ -131,24 +131,6 @@ func (t *TestConfig) HelmValuesFromConfig() (map[string]string, error) {
 	if t.EnableOpenshift {
 		setIfNotEmpty(helmValues, "global.openshift.enabled", "true")
 	}
-	nodeSelectors := map[string]string{
-		"connectInject.nodeSelector":                                "kubernetes.io/os: linux",
-		"connectInject.apiGateway.managedGatewayClass.nodeSelector": "kubernetes.io/os: linux",
-		"client.nodeSelector":                                       "kubernetes.io/os: linux",
-		"acls.nodeSelector":                                         "kubernetes.io/os: linux",
-		"global.acls.nodeSelector":                                  "kubernetes.io/os: linux",
-		"global.tls.nodeSelector":                                   "kubernetes.io/os: linux",
-		"meshGateway.nodeSelector":                                  "kubernetes.io/os: linux",
-		"server.nodeSelector":                                       "kubernetes.io/os: linux",
-		"syncCatalog.nodeSelector":                                  "kubernetes.io/os: linux",
-		"telemetryCollector.nodeSelector":                           "kubernetes.io/os: linux",
-		"webhookCertManager.nodeSelector":                           "kubernetes.io/os: linux",
-		"ingressGateways.defaults.nodeSelector":                     "kubernetes.io/os: linux",
-		"terminatingGateways.defaults.nodeSelector":                 "kubernetes.io/os: linux",
-	}
-	for nodeSelectorKey, nodeSelectorVal := range nodeSelectors {
-		setIfNotEmpty(helmValues, nodeSelectorKey, nodeSelectorVal)
-	}
 
 	if t.EnableWindows {
 		setIfNotEmpty(helmValues, "global.imageK8SWindows", fmt.Sprintf("%s-windows", t.ConsulK8SImage))
