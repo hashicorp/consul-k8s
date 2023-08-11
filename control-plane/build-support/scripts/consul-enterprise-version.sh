b57b9369c61ee60b4f2a48a1bf0abf7a71cd8e8a
Curt Bushko <cbushko@gmail.com>
2023-08-11 16:21:09 -0400

    Update consul-enterprise-version script to add -ent (#2756)

--- control-plane/build-support/scripts/consul-enterprise-version.sh
+++ control-plane/build-support/scripts/consul-enterprise-version.sh
@@ -4,8 +4,10 @@
 FILE=$1
 VERSION=$(yq .global.image $FILE)
 
-if [[ !"${VERSION}" == *"consul:"* ]]; then
+if [[ !"${VERSION}" == *"hashicorppreview/consul:"* ]]; then
 	VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g")
+elif [[ !"${VERSION}" == *"hashicorp/consul:"* ]]; then
+	VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
 fi
 
 echo "${VERSION}"
