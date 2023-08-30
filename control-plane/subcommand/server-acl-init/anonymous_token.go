30563234c4196a80c6c0bef9dd02ac6f17d599c1
aahel <aahel.guha@hashicorp.com>
2023-08-30 10:54:20 +0530

    added check if anonymous token policy exists (#2790)
    * added check if anonymous token policy exists
    
    * changed checkIfAnonymousTokenPolicyExists impl
    
    * made consts private
    
    * added test for configureAnonymousPolicy
    
    * fixed unit test
    
    * fixed test and minor refactoring
    
    * fix typo
    
    * changed some var names
    
    * added changelog

--- control-plane/subcommand/server-acl-init/anonymous_token.go
+++ control-plane/subcommand/server-acl-init/anonymous_token.go
@@ -7,9 +7,24 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
+const (
+	anonymousTokenPolicyName = "anonymous-token-policy"
+	anonymousTokenAccessorID = "00000000-0000-0000-0000-000000000002"
+)
+
 // configureAnonymousPolicy sets up policies and tokens so that Consul DNS and
 // cross-datacenter Consul connect calls will work.
 func (c *Command) configureAnonymousPolicy(consulClient *api.Client) error {
+	exists, err := checkIfAnonymousTokenPolicyExists(consulClient)
+	if err != nil {
+		c.log.Error("Error checking if anonymous token policy exists", "err", err)
+		return err
+	}
+	if exists {
+		c.log.Info("skipping creating anonymous token since it already exists")
+		return nil
+	}
+
 	anonRules, err := c.anonymousTokenRules()
 	if err != nil {
 		c.log.Error("Error templating anonymous token rules", "err", err)
@@ -18,7 +33,7 @@ func (c *Command) configureAnonymousPolicy(consulClient *api.Client) error {
 
 	// Create policy for the anonymous token
 	anonPolicy := api.ACLPolicy{
-		Name:        "anonymous-token-policy",
+		Name:        anonymousTokenPolicyName,
 		Description: "Anonymous token Policy",
 		Rules:       anonRules,
 	}
@@ -33,7 +48,7 @@ func (c *Command) configureAnonymousPolicy(consulClient *api.Client) error {
 
 	// Create token to get sent to TokenUpdate
 	aToken := api.ACLToken{
-		AccessorID: "00000000-0000-0000-0000-000000000002",
+		AccessorID: anonymousTokenAccessorID,
 		Policies:   []*api.ACLTokenPolicyLink{{Name: anonPolicy.Name}},
 	}
 
@@ -43,4 +58,19 @@ func (c *Command) configureAnonymousPolicy(consulClient *api.Client) error {
 			_, _, err := consulClient.ACL().TokenUpdate(&aToken, &api.WriteOptions{})
 			return err
 		})
+}
+
+func checkIfAnonymousTokenPolicyExists(consulClient *api.Client) (bool, error) {
+	token, _, err := consulClient.ACL().TokenRead(anonymousTokenAccessorID, nil)
+	if err != nil {
+		return false, err
+	}
+
+	for _, policy := range token.Policies {
+		if policy.Name == anonymousTokenPolicyName {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
