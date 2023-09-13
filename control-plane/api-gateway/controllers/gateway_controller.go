93ef8d075800c8844cab887b980fbd3b952314d2
Thomas Eckert <teckert@hashicorp.com>
2023-09-13 15:05:15 -0400

    Reconcile Route Auth Filter changes (#2954)
    * Group indices by resource
    
    * Add index for HTTPRoutes referencing RouteAuthFilters
    
    * Add watch for HTTPRoutes referencing RouteAuthFilters
    
    * Add permissions to connect-inject clusterrole
    
    * Compare JWT filters for equality
    
    * Add RouteAuthFilter to resource translator

--- control-plane/api-gateway/controllers/gateway_controller.go
+++ control-plane/api-gateway/controllers/gateway_controller.go
@@ -477,7 +477,13 @@ func SetupGatewayControllerWithManager(ctx context.Context, mgr ctrl.Manager, co
 		Watches(
 			source.NewKindWithCache((&v1alpha1.RouteTimeoutFilter{}), mgr.GetCache()),
 			handler.EnqueueRequestsFromMapFunc(r.transformRouteTimeoutFilter(ctx)),
-		).Complete(r)
+		).
+		Watches(
+			// Subscribe to changes in RouteAuthFilter custom resources referenced by HTTPRoutes.
+			source.NewKindWithCache((&v1alpha1.RouteAuthFilter{}), mgr.GetCache()),
+			handler.EnqueueRequestsFromMapFunc(r.transformRouteAuthFilter(ctx)),
+		).
+		Complete(r)
 }
 
 // transformGatewayClass will check the list of GatewayClass objects for a matching
@@ -625,6 +631,12 @@ func (r *GatewayController) transformRouteRetryFilter(ctx context.Context) func(
 func (r *GatewayController) transformRouteTimeoutFilter(ctx context.Context) func(object client.Object) []reconcile.Request {
 	return func(o client.Object) []reconcile.Request {
 		return r.gatewaysForRoutesReferencing(ctx, "", HTTPRoute_RouteTimeoutFilterIndex, client.ObjectKeyFromObject(o).String())
+	}
+}
+
+func (r *GatewayController) transformRouteAuthFilter(ctx context.Context) func(object client.Object) []reconcile.Request {
+	return func(o client.Object) []reconcile.Request {
+		return r.gatewaysForRoutesReferencing(ctx, "", HTTPRoute_RouteAuthFilterIndex, client.ObjectKeyFromObject(o).String())
 	}
 }
 
@@ -883,6 +895,8 @@ func (c *GatewayController) filterFiltersForExternalRefs(ctx context.Context, ro
 			externalFilter = &v1alpha1.RouteRetryFilter{}
 		case v1alpha1.RouteTimeoutFilterKind:
 			externalFilter = &v1alpha1.RouteTimeoutFilter{}
+		case v1alpha1.RouteAuthFilterKind:
+			externalFilter = &v1alpha1.RouteAuthFilter{}
 		default:
 			continue
 		}
