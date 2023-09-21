c3ce59e4a49f1663a562acd4baa6612d47091b4a
Michael Wilkerson <mwilkerson@hashicorp.com>
2023-09-21 13:50:28 -0700

    added container env logic for injecting env variables - due to refactoring can support same labeled/unlabeld parsing logic as when pod controller processes the annotations

--- control-plane/connect-inject/webhookv2/container_env.go
+++ control-plane/connect-inject/webhookv2/container_env.go
@@ -9,38 +9,34 @@ import (
 	"strings"
 
 	"github.com/hashicorp/consul-k8s/control-plane/connect-inject/common"
-	"github.com/hashicorp/consul-k8s/control-plane/connect-inject/constants"
 	corev1 "k8s.io/api/core/v1"
 )
 
-func (w *MeshWebhook) containerEnvVars(pod corev1.Pod) []corev1.EnvVar {
-	raw, ok := pod.Annotations[constants.AnnotationMeshDestinations]
-	if !ok || raw == "" {
-		return []corev1.EnvVar{}
+func (w *MeshWebhook) containerEnvVars(pod corev1.Pod) ([]corev1.EnvVar, error) {
+	p := common.NewPodAnnotationProcessor(true, true)
+	upstreams, err := p.ProcessUpstreams(pod)
+	if err != nil {
+		return nil, err
+	}
+	if upstreams == nil {
+		return nil, nil
 	}
 
 	var result []corev1.EnvVar
-	for _, raw := range strings.Split(raw, ",") {
-		parts := strings.SplitN(raw, ":", 3)
-		port, _ := common.PortValue(pod, strings.TrimSpace(parts[1]))
-		if port > 0 {
-			service := parts[0]
-			pieces := strings.Split(service, ".")
-			serviceName := strings.TrimSpace(pieces[1])
-			serviceName = strings.ToUpper(strings.Replace(serviceName, "-", "_", -1))
-			portName := strings.TrimSpace(pieces[0])
-			portName = strings.ToUpper(strings.Replace(portName, "-", "_", -1))
-			portStr := strconv.Itoa(int(port))
+	for _, upstream := range upstreams.Upstreams {
+		serviceName := strings.TrimSpace(upstream.DestinationRef.Name)
+		serviceName = strings.ToUpper(strings.Replace(serviceName, "-", "_", -1))
+		portName := strings.TrimSpace(upstream.DestinationPort)
+		portName = strings.ToUpper(strings.Replace(portName, "-", "_", -1))
 
-			result = append(result, corev1.EnvVar{
-				Name:  fmt.Sprintf("%s_%s_CONNECT_SERVICE_HOST", serviceName, portName),
-				Value: "127.0.0.1",
-			}, corev1.EnvVar{
-				Name:  fmt.Sprintf("%s_%s_CONNECT_SERVICE_PORT", serviceName, portName),
-				Value: portStr,
-			})
-		}
+		result = append(result, corev1.EnvVar{
+			Name:  fmt.Sprintf("%s_%s_CONNECT_SERVICE_HOST", serviceName, portName),
+			Value: upstream.GetIpPort().Ip,
+		}, corev1.EnvVar{
+			Name:  fmt.Sprintf("%s_%s_CONNECT_SERVICE_PORT", serviceName, portName),
+			Value: strconv.Itoa(int(upstream.GetIpPort().Port)),
+		})
 	}
 
-	return result
+	return result, nil
 }
