61052d97f451f9a27a7eca8f6e41b25ffc159f19
Michael Wilkerson <mwilkerson@hashicorp.com>
2023-09-29 13:07:31 -0700

    added rc branch enterprise handling

--- control-plane/build-support/scripts/consul-enterprise-version.sh
+++ control-plane/build-support/scripts/consul-enterprise-version.sh
@@ -6,6 +6,8 @@ VERSION=$(yq .global.image $FILE)
 
 if [[ !"${VERSION}" == *"hashicorp/consul:"* ]]; then
 	VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
+elif [[ !"${VERSION}" == *"-rc"* ]]; then
+  VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
 else
   VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g")
 fi
