214c79377905bcf2684ada7e9e33f2b881207d2b
Chris Thain <chris.m.thain@gmail.com>
2023-10-06 12:14:02 -0700

    test is passing consistently locally with kind; retry in CI

--- acceptance/tests/connect/local_rate_limit_test.go
+++ acceptance/tests/connect/local_rate_limit_test.go
@@ -20,7 +20,7 @@ import (
 
 // TestConnectInject_LocalRateLimiting tests that local rate limiting works as expected between services.
 func TestConnectInject_LocalRateLimiting(t *testing.T) {
-	t.Skipf("TODO(flaky-1.17): NET-XXXX")
+	//t.Skipf("TODO(flaky-1.17): NET-5727")
 	cfg := suite.Config()
 	ctx := suite.Environment().DefaultContext(t)
 
