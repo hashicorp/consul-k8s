2817d1d55b3b6383be9f4aeccfcc7865ffed715d
Thomas Eckert <teckert@hashicorp.com>
2023-10-26 22:24:10 +0000

    backport of commit d4c33f67d0b7d1af1b2e781f5c6d4a03762f0db4

--- acceptance/test.sh
+++
@@ -1,7 +0,0 @@
-gotest ./tests/peering -run TestPeering_Connect/secure_installation \
-	-timeout 6h -p 1 -enable-enterprise -enable-multi-cluster \
-	-enterprise-license $CONSUL_ENT_LICENSE \
-	-kube-contexts "kind-dc1,kind-dc2" \
-	-enable-transparent-proxy \
-	-use-kind \
-	-v 
