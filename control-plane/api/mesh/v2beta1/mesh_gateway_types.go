bb2fe92c0957d9bbfaf90a02a2f52496aa39e161
Thomas Eckert <teckert@hashicorp.com>
2023-11-10 12:58:42 -0500

    NET-6331 Create MeshGateway CRD (#3195)
    Add MeshGateway to `mesh.consul.hashicorp.com/v2beta1`

---
+++ control-plane/api/mesh/v2beta1/mesh_gateway_types.go
@@ -0,0 +1,151 @@
+// // Copyright (c) HashiCorp, Inc.
+// // SPDX-License-Identifier: MPL-2.0
+package v2beta1
+
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/hashicorp/consul-k8s/control-plane/api/common"
+	inject "github.com/hashicorp/consul-k8s/control-plane/connect-inject/common"
+	"github.com/hashicorp/consul-k8s/control-plane/connect-inject/constants"
+	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v2beta1"
+	"github.com/hashicorp/consul/proto-public/pbresource"
+	"google.golang.org/protobuf/testing/protocmp"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	meshGatewayKubeKind = "meshgateway"
+)
+
+func init() {
+	MeshSchemeBuilder.Register(&MeshGateway{}, &MeshGatewayList{})
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// MeshGateway is the Schema for the Mesh Gateway API
+// +kubebuilder:printcolumn:name="Synced",type="string",JSONPath=".status.conditions[?(@.type==\"Synced\")].status",description="The sync status of the resource with Consul"
+// +kubebuilder:printcolumn:name="Last Synced",type="date",JSONPath=".status.lastSyncedTime",description="The last successful synced time of the resource with Consul"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="The age of the resource"
+// +kubebuilder:resource:shortName="mesh-gateway"
+type MeshGateway struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   pbmesh.MeshGateway `json:"spec,omitempty"`
+	Status `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// MeshGatewayList contains a list of MeshGateway.
+type MeshGatewayList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []*MeshGateway `json:"items"`
+}
+
+func (in *MeshGateway) ResourceID(namespace, partition string) *pbresource.ID {
+	return &pbresource.ID{
+		Name: in.Name,
+		Type: pbmesh.MeshGatewayType,
+		Tenancy: &pbresource.Tenancy{
+			Partition: partition,
+			Namespace: namespace,
+
+			// Because we are explicitly defining NS/partition, this will not default and must be explicit.
+			// At a future point, this will move out of the Tenancy block.
+			PeerName: constants.DefaultConsulPeer,
+		},
+	}
+}
+
+func (in *MeshGateway) Resource(namespace, partition string) *pbresource.Resource {
+	return &pbresource.Resource{
+		Id:       in.ResourceID(namespace, partition),
+		Data:     inject.ToProtoAny(&in.Spec),
+		Metadata: meshConfigMeta(),
+	}
+}
+
+func (in *MeshGateway) AddFinalizer(f string) {
+	in.ObjectMeta.Finalizers = append(in.Finalizers(), f)
+}
+
+func (in *MeshGateway) RemoveFinalizer(f string) {
+	var newFinalizers []string
+	for _, oldF := range in.Finalizers() {
+		if oldF != f {
+			newFinalizers = append(newFinalizers, oldF)
+		}
+	}
+	in.ObjectMeta.Finalizers = newFinalizers
+}
+
+func (in *MeshGateway) Finalizers() []string {
+	return in.ObjectMeta.Finalizers
+}
+
+func (in *MeshGateway) MatchesConsul(candidate *pbresource.Resource, namespace, partition string) bool {
+	return cmp.Equal(
+		in.Resource(namespace, partition),
+		candidate,
+		protocmp.IgnoreFields(&pbresource.Resource{}, "status", "generation", "version"),
+		protocmp.IgnoreFields(&pbresource.ID{}, "uid"),
+		protocmp.Transform(),
+		cmpopts.SortSlices(func(a, b any) bool { return fmt.Sprintf("%v", a) < fmt.Sprintf("%v", b) }),
+	)
+}
+
+func (in *MeshGateway) KubeKind() string {
+	return meshGatewayKubeKind
+}
+
+func (in *MeshGateway) KubernetesName() string {
+	return in.ObjectMeta.Name
+}
+
+func (in *MeshGateway) SetSyncedCondition(status corev1.ConditionStatus, reason, message string) {
+	in.Status.Conditions = Conditions{
+		{
+			Type:               ConditionSynced,
+			Status:             status,
+			LastTransitionTime: metav1.Now(),
+			Reason:             reason,
+			Message:            message,
+		},
+	}
+}
+
+func (in *MeshGateway) SetLastSyncedTime(time *metav1.Time) {
+	in.Status.LastSyncedTime = time
+}
+
+func (in *MeshGateway) SyncedCondition() (status corev1.ConditionStatus, reason, message string) {
+	cond := in.Status.GetCondition(ConditionSynced)
+	if cond == nil {
+		return corev1.ConditionUnknown, "", ""
+	}
+	return cond.Status, cond.Reason, cond.Message
+}
+
+func (in *MeshGateway) SyncedConditionStatus() corev1.ConditionStatus {
+	condition := in.Status.GetCondition(ConditionSynced)
+	if condition == nil {
+		return corev1.ConditionUnknown
+	}
+	return condition.Status
+}
+
+func (in *MeshGateway) Validate(tenancy common.ConsulTenancyConfig) error {
+	// TODO add validation logic that ensures we only ever write this to the default namespace.
+	return nil
+}
+
+// DefaultNamespaceFields is required as part of the common.MeshConfig interface.
+func (in *MeshGateway) DefaultNamespaceFields(tenancy common.ConsulTenancyConfig) {}
