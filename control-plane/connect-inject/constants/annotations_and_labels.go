83ca25c8d23875ea466c025d6a63abf7c2ae14dc
Joshua Timmons <joshua.timmons1@gmail.com>
2023-11-14 12:28:51 -0500

    Fix deployment label comma

--- control-plane/connect-inject/constants/annotations_and_labels.go
+++ control-plane/connect-inject/constants/annotations_and_labels.go
@@ -196,8 +196,8 @@ const (
 	// by the peering controllers.
 	LabelPeeringToken = "consul.hashicorp.com/peering-token"
 
-	// LabelTelemetryCollector is a label signaling pod is associated with the deployment of a Consul Telemetry
-	// Collector. If this is set, the endpoints-controller ensures the deployed Namespace exists in Consul and create it if it does not.
+	// LabelTelemetryCollector is a label signaling the pod is associated with the deployment of a Consul Telemetry
+	// Collector. If this is set, during connect-inject, the endpoints-controller ensures the deployed Namespace exists in Consul and create it if it does not.
 	// This is only meant to be used by Deployment/consul-telemetry-collector.
 	LabelTelemetryCollector = "consul.hashicorp.com/telemetry-collector"
 
