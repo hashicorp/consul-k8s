5f36c91cf9c3014a3abfb2d5727ccf25755f1469
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-11-21 13:57:46 -0500

    NET-6664 Specify cluster scope for MeshGateway CRD (#3236)
    Specify cluster scope for MeshGateway CRD

--- control-plane/api/mesh/v2beta1/mesh_gateway_types.go
+++ control-plane/api/mesh/v2beta1/mesh_gateway_types.go
@@ -33,7 +33,7 @@ func init() {
 // +kubebuilder:printcolumn:name="Synced",type="string",JSONPath=".status.conditions[?(@.type==\"Synced\")].status",description="The sync status of the resource with Consul"
 // +kubebuilder:printcolumn:name="Last Synced",type="date",JSONPath=".status.lastSyncedTime",description="The last successful synced time of the resource with Consul"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="The age of the resource"
-// +kubebuilder:resource:shortName="mesh-gateway"
+// +kubebuilder:resource:scope="Cluster"
 type MeshGateway struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
