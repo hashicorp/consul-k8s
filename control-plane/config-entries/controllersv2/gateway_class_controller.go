dc6a70a30093d0165cc4959f029720018f604638
Melisa Griffin <missylbytes@users.noreply.github.com>
2023-11-27 19:01:47 -0500

    NET-6536 Adds stub of GatewayClass controller into v2 controllers (#3245)
    * Adds stub of GatewayClass controller into v2 controllers
    
    * regen without weird spaces
    
    * Update control-plane/config-entries/controllersv2/gateway_class_controller.go
    
    ---------
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

---
+++ control-plane/config-entries/controllersv2/gateway_class_controller.go
@@ -0,0 +1,43 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package controllersv2
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	meshv2beta1 "github.com/hashicorp/consul-k8s/control-plane/api/mesh/v2beta1"
+)
+
+// GatewayClassController reconciles a MeshGateway object.
+type GatewayClassController struct {
+	client.Client
+	Log                  logr.Logger
+	Scheme               *runtime.Scheme
+	MeshConfigController *MeshConfigController
+}
+
+// +kubebuilder:rbac:groups=mesh.consul.hashicorp.com,resources=gatewayclass,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=mesh.consul.hashicorp.com,resources=gatewayclass/status,verbs=get;update;patch
+
+func (r *GatewayClassController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	return r.MeshConfigController.ReconcileEntry(ctx, r, req, &meshv2beta1.GatewayClass{})
+}
+
+func (r *GatewayClassController) Logger(name types.NamespacedName) logr.Logger {
+	return r.Log.WithValues("request", name)
+}
+
+func (r *GatewayClassController) UpdateStatus(ctx context.Context, obj client.Object, opts ...client.SubResourceUpdateOption) error {
+	return r.Status().Update(ctx, obj, opts...)
+}
+
+func (r *GatewayClassController) SetupWithManager(mgr ctrl.Manager) error {
+	return setupWithManager(mgr, &meshv2beta1.GatewayClass{}, r)
+}
