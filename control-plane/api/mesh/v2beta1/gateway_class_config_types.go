964b0133a9bc621e3e8a67c29075393890924c47
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-12-01 17:06:08 -0500

    Use existing GatewayClassConfig structure
    This allows us to keep the discussion about the new structure of the GatewayClassConfig in a separate PR for discussion

--- control-plane/api/mesh/v2beta1/gateway_class_config_types.go
+++ control-plane/api/mesh/v2beta1/gateway_class_config_types.go
@@ -8,10 +8,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const (
-	gatewayClassConfigKubeKind = "gatewayclassconfig"
-)
-
 func init() {
 	MeshSchemeBuilder.Register(&GatewayClassConfig{}, &GatewayClassConfigList{})
 }
@@ -33,95 +29,68 @@ type GatewayClassConfig struct {
 	Status `json:"status,omitempty"`
 }
 
+// +k8s:deepcopy-gen=true
+
+// GatewayClassConfigSpec specifies the desired state of the Config CRD.
 type GatewayClassConfigSpec struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
 
-	// Deployment contains config specific to the Deployment created from this GatewayClass
-	Deployment GatewayClassDeploymentConfig `json:"deployment,omitempty"`
-	// Role contains config specific to the Role created from this GatewayClass
-	Role GatewayClassRoleConfig `json:"role,omitempty"`
-	// RoleBinding contains config specific to the RoleBinding created from this GatewayClass
-	RoleBinding GatewayClassRoleBindingConfig `json:"roleBinding,omitempty"`
-	// Service contains config specific to the Service created from this GatewayClass
-	Service GatewayClassServiceConfig `json:"service,omitempty"`
-	// ServiceAccount contains config specific to the corev1.ServiceAccount created from this GatewayClass
-	ServiceAccount GatewayClassServiceAccountConfig `json:"serviceAccount,omitempty"`
-}
+	// +kubebuilder:validation:Enum=ClusterIP;NodePort;LoadBalancer
+	ServiceType *corev1.ServiceType `json:"serviceType,omitempty"`
 
-type GatewayClassDeploymentConfig struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
+	// NodeSelector is a selector which must be true for the pod to fit on a node.
+	// Selector which must match a node's labels for the pod to be scheduled on that node.
+	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
-	// Container contains config specific to the created Deployment's container
-	Container GatewayClassContainerConfig `json:"container,omitempty"`
-	// InitContainer contains config specific to the created Deployment's init container
-	InitContainer GatewayClassInitContainerConfig `json:"initContainer,omitempty"`
-	// NodeSelector specifies the node selector to use on the created Deployment
-	NodeSelector *corev1.NodeSelector `json:"nodeSelector,omitempty"`
-	// PriorityClassName specifies the priority class name to use on the created Deployment
-	PriorityClassName string `json:"priorityClassName"`
-	// Tolerations specifies the tolerations to use on the created Deployment
+	// Tolerations allow the scheduler to schedule nodes with matching taints.
+	// More Info: https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/
 	Tolerations []corev1.Toleration `json:"tolerations,omitempty"`
+
+	// Deployment defines the deployment configuration for the gateway.
+	DeploymentSpec DeploymentSpec `json:"deployment,omitempty"`
+
+	// Annotation Information to copy to services or deployments
+	CopyAnnotations CopyAnnotationsSpec `json:"copyAnnotations,omitempty"`
+
+	// The name of an existing Kubernetes PodSecurityPolicy to bind to the managed ServiceAccount if ACLs are managed.
+	PodSecurityPolicy string `json:"podSecurityPolicy,omitempty"`
+
+	// The name of the OpenShift SecurityContextConstraints resource for this gateway class to use.
+	OpenshiftSCCName string `json:"openshiftSCCName,omitempty"`
+
+	// The value to add to privileged ports ( ports < 1024) for gateway containers
+	MapPrivilegedContainerPorts int32 `json:"mapPrivilegedContainerPorts,omitempty"`
 }
 
-type GatewayClassInitContainerConfig struct {
-	// Consul specifies configuration for the consul-k8s-control-plane init container
-	Consul GatewayClassConsulConfig `json:"consul,omitempty"`
-	// Resources specifies the resource requirements for the created Deployment
+// +k8s:deepcopy-gen=true
+
+type DeploymentSpec struct {
+	// +kubebuilder:default:=1
+	// +kubebuilder:validation:Maximum=8
+	// +kubebuilder:validation:Minimum=1
+	// Number of gateway instances that should be deployed by default
+	DefaultInstances *int32 `json:"defaultInstances,omitempty"`
+	// +kubebuilder:default:=8
+	// +kubebuilder:validation:Maximum=8
+	// +kubebuilder:validation:Minimum=1
+	// Max allowed number of gateway instances
+	MaxInstances *int32 `json:"maxInstances,omitempty"`
+	// +kubebuilder:default:=1
+	// +kubebuilder:validation:Maximum=8
+	// +kubebuilder:validation:Minimum=1
+	// Minimum allowed number of gateway instances
+	MinInstances *int32 `json:"minInstances,omitempty"`
+
+	// Resources defines the resource requirements for the gateway.
 	Resources *corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
-type GatewayClassContainerConfig struct {
-	// Consul specifies configuration for the consul-dataplane container
-	Consul GatewayClassConsulConfig `json:"consul,omitempty"`
-	// Resources specifies the resource requirements for the created Deployment
-	Resources *corev1.ResourceRequirements `json:"resources,omitempty"`
-}
+//+kubebuilder:object:generate=true
 
-type GatewayClassRoleConfig struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
-}
-
-type GatewayClassRoleBindingConfig struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
-}
-
-type GatewayClassServiceConfig struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
-
-	// Type specifies the type of Service to use (LoadBalancer, ClusterIP, etc.)
-	Type *corev1.ServiceType `json:"type,omitempty"`
-}
-
-type GatewayClassServiceAccountConfig struct {
-	GatewayClassAnnotationsAndLabels `json:",inline"`
-}
-
-type GatewayClassConsulConfig struct {
-	// Logging specifies the logging configuration for Consul Dataplane
-	Logging GatewayClassConsulLoggingConfig `json:"logging,omitempty"`
-}
-
-type GatewayClassConsulLoggingConfig struct {
-	// Level sets the logging level for Consul Dataplane (debug, info, etc.)
-	Level string `json:"level,omitempty"`
-}
-
-// GatewayClassAnnotationsAndLabels exists to provide a commonly-embedded wrapper
-// for Annotations and Labels on a given resource configuration
-type GatewayClassAnnotationsAndLabels struct {
-	// Annotations are applied to the created resource
-	Annotations GatewayClassAnnotationsLabelsConfig `json:"annotations,omitempty"`
-	// Labels are applied to the created resource
-	Labels GatewayClassAnnotationsLabelsConfig `json:"labels,omitempty"`
-}
-
-type GatewayClassAnnotationsLabelsConfig struct {
-	// InheritFromGateway lists the names/keys of annotations or labels to copy from the Gateway resource.
-	// Any name/key included here will override those in Set if specified on the Gateway.
-	InheritFromGateway []string `json:"inheritFromGateway,omitempty"`
-	// Set lists the names/keys and values of annotations or labels to set on the resource.
-	// Any name/key included here will be overridden if present in InheritFromGateway and set on the Gateway.
-	Set map[string]string `json:"set,omitempty"`
+// CopyAnnotationsSpec defines the annotations that should be copied to the gateway service.
+type CopyAnnotationsSpec struct {
+	// List of annotations to copy to the gateway service.
+	Service []string `json:"service,omitempty"`
 }
 
 // +kubebuilder:object:root=true
