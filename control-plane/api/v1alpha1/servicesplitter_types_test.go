6e9f63d64c1633abd0274c11c682f69019a0c73d
Ashwin Venkatesh <ashwin@hashicorp.com>
2023-12-01 11:12:43 -0500

    Update MatchesConsul to normalize partitions during comparison. (#3284)
    * Update MatchesConsul to normalize partitions during comparison.
    * Update test cases with valid datasets

--- control-plane/api/v1alpha1/servicesplitter_types_test.go
+++ control-plane/api/v1alpha1/servicesplitter_types_test.go
@@ -7,11 +7,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hashicorp/consul-k8s/control-plane/api/common"
 	capi "github.com/hashicorp/consul/api"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/hashicorp/consul-k8s/control-plane/api/common"
 )
 
 // Test MatchesConsul.
@@ -96,6 +97,7 @@ func TestServiceSplitter_MatchesConsul(t *testing.T) {
 						Service:       "foo",
 						ServiceSubset: "bar",
 						Namespace:     "baz",
+						Partition:     "default",
 						RequestHeaders: &capi.HTTPHeaderModifiers{
 							Add: map[string]string{
 								"foo":    "bar",
