c21e5676f441e4d5fe9049e8ae66e078e410a14c
John Murret <john.murret@hashicorp.com>
2023-12-12 12:24:41 -0700

    add comment to test. remove extra lines from config map.

--- acceptance/tests/segments/segments_test.go
+++ acceptance/tests/segments/segments_test.go
@@ -40,7 +40,9 @@ func TestSegments_MeshWithAgentfulClients(t *testing.T) {
 				"server.replicas":    "3",
 				"server.extraConfig": `"{\"segments\": [{\"name\":\"alpha1\"\,\"bind\":\"0.0.0.0\"\,\"port\":8303}]}"`,
 
-				"client.enabled":     "true",
+				"client.enabled": "true",
+				// need to configure clients to connect to port 8303 that the alpha segment was configured on rather than
+				// the standard serf LAN port.
 				"client.join[0]":     "${CONSUL_FULLNAME}-server-0.${CONSUL_FULLNAME}-server.${NAMESPACE}.svc:8303",
 				"client.join[1]":     "${CONSUL_FULLNAME}-server-1.${CONSUL_FULLNAME}-server.${NAMESPACE}.svc:8303",
 				"client.join[2]":     "${CONSUL_FULLNAME}-server-2.${CONSUL_FULLNAME}-server.${NAMESPACE}.svc:8303",
