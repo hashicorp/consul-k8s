33ba18365b73d62dd690a1e4ec698146a92243f1
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2023-12-19 23:30:57 -0600

    Backport of [Net-5510][Net-5455]: CRD controller should only patch the finalizer in the metadata of a CRD, rather than the whole object into release/1.3.x (#3412)
    backport of commit 962f047e4b856510f8f835b88209688d84bb053c
    
    Co-authored-by: Nitya Dhanushkodi <nitya@hashicorp.com>

--- control-plane/config-entries/controllers/proxydefaults_controller.go
+++ control-plane/config-entries/controllers/proxydefaults_controller.go
@@ -15,9 +15,12 @@ import (
 	consulv1alpha1 "github.com/hashicorp/consul-k8s/control-plane/api/v1alpha1"
 )
 
+var _ Controller = (*ProxyDefaultsController)(nil)
+
 // ProxyDefaultsController reconciles a ProxyDefaults object.
 type ProxyDefaultsController struct {
 	client.Client
+	FinalizerPatcher
 	Log                   logr.Logger
 	Scheme                *runtime.Scheme
 	ConfigEntryController *ConfigEntryController
