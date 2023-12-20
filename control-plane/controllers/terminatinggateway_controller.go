f5f4c1144bbdec8b426c407a8d85ce118c7c2ac3
Nitya Dhanushkodi <nitya@hashicorp.com>
2023-12-19 17:12:46 -0800

    [Net-5510][Net-5455]: CRD controller should only patch the finalizer in the metadata of a CRD, rather than the whole object (#3362)
    Finalizer patcher
    
    Co-authored-by: Derek Menteer <derek.menteer@hashicorp.com>

--- control-plane/controllers/terminatinggateway_controller.go
+++ control-plane/controllers/terminatinggateway_controller.go
@@ -15,9 +15,12 @@ import (
 	consulv1alpha1 "github.com/hashicorp/consul-k8s/control-plane/api/v1alpha1"
 )
 
+var _ Controller = (*TerminatingGatewayController)(nil)
+
 // TerminatingGatewayController is the controller for TerminatingGateway resources.
 type TerminatingGatewayController struct {
 	client.Client
+	FinalizerPatcher
 	Log                   logr.Logger
 	Scheme                *runtime.Scheme
 	ConfigEntryController *ConfigEntryController
