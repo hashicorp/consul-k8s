86d6cbe715c7c2f459ea13220a3cacbed85f1723
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2024-01-15 14:51:12 -0800

    One node

--- acceptance/tests/server/server_test.go
+++ acceptance/tests/server/server_test.go
@@ -27,6 +27,7 @@ func TestServerRestart(t *testing.T) {
 		"connectInject.enabled": "false",
 		"server.enabled":        "true",
 		"server.replicas":       fmt.Sprintf("%d", replicas),
+		"server.affinity":       "null", // Allow >1 pods per node so we can test in minikube with one node.
 	}
 	consulCluster := consul.NewHelmCluster(t, helmValues, suite.Environment().DefaultContext(t), suite.Config(), releaseName)
 	consulCluster.Create(t)
