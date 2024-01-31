678ccd52c7334a2ea19113b2f9b654f6564998f4
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2024-01-31 14:49:18 -0600

    add status structs

--- control-plane/api/mesh/v2beta1/api_gateway_types.go
+++ control-plane/api/mesh/v2beta1/api_gateway_types.go
@@ -60,6 +60,24 @@ type GatewayAddress struct {
 	Value string `json:"value"`
 }
 
+type APIGatewayStatus struct {
+	Status
+	Addresses []GatewayAddress
+	Listeners []ListenerStatus
+}
+
+type ListenerStatus struct {
+	Status
+	Name           string `json:"name"`
+	AttachedRoutes int32  `json:"attachedRoutes"`
+}
+
+type GatewayAddress struct {
+	// +kubebuilder:default=IPAddress
+	Type  string `json:"type"`
+	Value string `json:"value"`
+}
+
 // +kubebuilder:object:root=true
 
 // APIGatewayList contains a list of APIGateway.
