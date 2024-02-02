32e24ef37d13a542ad728c6c3df585f39f38f2a6
Michael Wilkerson <mwilkerson@hashicorp.com>
2024-02-02 19:41:59 +0000

    backport of commit 34fae6febcd5def80f237ca094d737a41d13a6ee

--- control-plane/build-support/scripts/consul-enterprise-version.sh
+++ control-plane/build-support/scripts/consul-enterprise-version.sh
@@ -6,8 +6,6 @@ VERSION=$(yq .global.image $FILE)
 
 if [[ !"${VERSION}" == *"hashicorp/consul:"* ]]; then
 	VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
-elif [[ !"${VERSION}" == *"-rc"* ]]; then
-  VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
 else
   VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g")
 fi
