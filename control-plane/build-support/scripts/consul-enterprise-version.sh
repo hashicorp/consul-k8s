3fac68561247cc5122b75cc7976cce83c6165956
Michael Wilkerson <mwilkerson@hashicorp.com>
2024-02-02 20:05:59 +0000

    backport of commit c69f584adf7c238525bd0ceeb0fa09fdd92c4815

--- control-plane/build-support/scripts/consul-enterprise-version.sh
+++ control-plane/build-support/scripts/consul-enterprise-version.sh
@@ -4,9 +4,11 @@
 FILE=$1
 VERSION=$(yq .global.image $FILE)
 
-if [[ !"${VERSION}" == *"hashicorp/consul:"* ]]; then
+if [[ "${VERSION}" == *"hashicorp/consul:"* ]]; then
+  # for matching release image repos with a -ent label
 	VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g" | sed "s/$/-ent/g")
 else
+  # for matching preview image repos
   VERSION=$(echo ${VERSION} | sed "s/consul:/consul-enterprise:/g")
 fi
 
