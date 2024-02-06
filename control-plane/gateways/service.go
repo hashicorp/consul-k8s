7e38df4feead2bfe05b333ffa73862178d0e9bbe
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2024-02-06 14:03:38 -0800

    Backport of [NET-7656] Add GatewayClassConfig watch for MeshGateway controller into release/1.4.x (#3552)
    [NET-7656] Add GatewayClassConfig watch for MeshGateway controller (#3537)
    
    * Add GatewayClass[Config] watches for MeshGateway controller
    
    * Update merge logic for deployment + service
    
    * Add test coverage for MergeDeployment
    
    * Add test coverage for MergeService
    
    * Copy over owner references to new Service + Deployment
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

--- control-plane/gateways/service.go
+++ control-plane/gateways/service.go
@@ -73,3 +73,27 @@ func (b *meshGatewayBuilder) Ports(portModifier int32) []corev1.ServicePort {
 	}
 	return ports
 }
+
+// MergeService is used to update a corev1.Service without overwriting any
+// existing annotations or labels that were placed there by other vendors.
+//
+// based on https://github.com/kubernetes-sigs/controller-runtime/blob/4000e996a202917ad7d40f02ed8a2079a9ce25e9/pkg/controller/controllerutil/example_test.go
+func MergeService(existing, desired *corev1.Service) {
+	existing.Spec = desired.Spec
+
+	// Only overwrite fields if the Service doesn't exist yet
+	if existing.ObjectMeta.CreationTimestamp.IsZero() {
+		existing.ObjectMeta.OwnerReferences = desired.ObjectMeta.OwnerReferences
+		existing.Annotations = desired.Annotations
+		existing.Labels = desired.Labels
+		return
+	}
+
+	// If the Service already exists, add any desired annotations + labels to existing set
+	for k, v := range desired.ObjectMeta.Annotations {
+		existing.ObjectMeta.Annotations[k] = v
+	}
+	for k, v := range desired.ObjectMeta.Labels {
+		existing.ObjectMeta.Labels[k] = v
+	}
+}
