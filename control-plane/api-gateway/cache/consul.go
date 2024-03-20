446c1b3df2ca962519a595f4d99f0f4a9d9332c6
jm96441n <john.maguire@hashicorp.com>
2024-03-19 20:29:10 -0400

    fix spacing

--- control-plane/api-gateway/cache/consul.go
+++ control-plane/api-gateway/cache/consul.go
@@ -374,6 +374,7 @@ func (c *Cache) ensurePolicy(client *api.Client, gatewayName string) (string, er
 	}
 
 	cachedPolicy, found := c.gatewayNameToPolicy[gatewayName]
+
 	if !found {
 		return createPolicy()
 	}
