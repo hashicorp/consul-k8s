7170bfdcb1bb4c1e3a6db7c073d405acb2736660
Nathan Coleman <nathan.coleman@hashicorp.com>
2024-04-22 14:24:16 -0400

    Fix linter errors

--- control-plane/connect-inject/webhookv2/redirect_traffic_test.go
+++ control-plane/connect-inject/webhookv2/redirect_traffic_test.go
@@ -389,7 +389,7 @@ func TestAddRedirectTrafficConfig(t *testing.T) {
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			err = c.webhook.addRedirectTrafficConfigAnnotation(c.pod, c.namespace)
+			err := c.webhook.addRedirectTrafficConfigAnnotation(c.pod, c.namespace)
 
 			// Only compare annotation and iptables config on successful runs
 			if c.expErr == nil {
