cf59df85923b1acb97b507f8176621d4dc328522
jm96441n <john.maguire@hashicorp.com>
2024-05-31 16:34:02 -0400

    fix issue where pods would constantly recycle due to random ordering of volumes/mounts

--- control-plane/api-gateway/gatekeeper/volumes.go
+++ control-plane/api-gateway/gatekeeper/volumes.go
@@ -5,6 +5,9 @@ package gatekeeper
 
 import (
 	"fmt"
+	"slices"
+	"strings"
+
 	"github.com/hashicorp/consul-k8s/control-plane/api-gateway/common"
 	"golang.org/x/exp/maps"
 	corev1 "k8s.io/api/core/v1"
@@ -60,5 +63,15 @@ func volumesAndMounts(gateway v1beta1.Gateway) ([]corev1.Volume, []corev1.Volume
 		}
 	}
 
-	return maps.Values(volumes), maps.Values(mounts)
+	vols := maps.Values(volumes)
+	slices.SortFunc(vols, func(a, b corev1.Volume) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	mts := maps.Values(mounts)
+	slices.SortFunc(mts, func(a, b corev1.VolumeMount) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	return vols, mts
 }
