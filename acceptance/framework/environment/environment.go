9016d6f718f75d7013ac5d0486f0a54d631a6920
jm96441n <john.maguire@hashicorp.com>
2024-06-03 12:30:21 -0400

    fix set logger call

--- acceptance/framework/environment/environment.go
+++ acceptance/framework/environment/environment.go
@@ -5,8 +5,8 @@ package environment
 
 import (
 	"fmt"
-	"os"
 
+	"github.com/go-logr/logr"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/hashicorp/consul-k8s/acceptance/framework/config"
 	"github.com/hashicorp/consul-k8s/control-plane/api/v1alpha1"
@@ -18,7 +18,6 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	gwv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 )
@@ -191,7 +190,7 @@ func (k kubernetesContext) ControllerRuntimeClient(t testutil.TestingTB) client.
 
 	client, err := client.New(config, client.Options{Scheme: s})
 	require.NoError(t, err)
-	logf.SetLogger(zap.New(zap.WriteTo(os.Stdout), zap.UseDevMode(true)))
+	logf.SetLogger(logr.New(nil))
 
 	k.runtimeClient = client
 
