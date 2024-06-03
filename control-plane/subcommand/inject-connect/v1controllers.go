57da609a24cfb47b787eed71629d727941ea7041
jm96441n <john.maguire@hashicorp.com>
2024-06-03 20:29:02 +0000

    backport of commit 74ee754d3b17f0729d59af60696db0a28c053878

--- control-plane/subcommand/inject-connect/v1controllers.go
+++ control-plane/subcommand/inject-connect/v1controllers.go
@@ -108,7 +108,7 @@ func (c *Command) configureV1Controllers(ctx context.Context, mgr manager.Manage
 		return err
 	}
 
-	cache, err := gatewaycontrollers.SetupGatewayControllerWithManager(ctx, mgr, gatewaycontrollers.GatewayControllerConfig{
+	cache, cleaner, err := gatewaycontrollers.SetupGatewayControllerWithManager(ctx, mgr, gatewaycontrollers.GatewayControllerConfig{
 		HelmConfig: gatewaycommon.HelmConfig{
 			ConsulConfig: gatewaycommon.ConsulConfig{
 				Address:    c.consul.Addresses,
@@ -152,6 +152,7 @@ func (c *Command) configureV1Controllers(ctx context.Context, mgr manager.Manage
 	}
 
 	go cache.Run(ctx)
+	go cleaner.Run(ctx)
 
 	// wait for the cache to fill
 	setupLog.Info("waiting for Consul cache sync")
