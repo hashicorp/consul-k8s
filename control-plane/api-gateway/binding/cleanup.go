03a4dd466124891754aa923f38d5361e2bf2a072
jm96441n <john.maguire@hashicorp.com>
2024-06-03 20:29:02 +0000

    backport of commit 74ee754d3b17f0729d59af60696db0a28c053878

---
+++ control-plane/api-gateway/binding/cleanup.go
@@ -0,0 +1,174 @@
+package binding
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/go-logr/logr"
+	"github.com/hashicorp/consul-k8s/control-plane/consul"
+	"github.com/hashicorp/consul/api"
+)
+
+const (
+	oldACLRoleName   = "managed-gateway-acl-role"
+	oldACLPolicyName = "api-gateway-token-policy"
+)
+
+type Cleaner struct {
+	Logger       logr.Logger
+	ConsulConfig *consul.Config
+	ServerMgr    consul.ServerConnectionManager
+	AuthMethod   string
+}
+
+// Run periodically cleans up old ACL roles and policies as well as orphaned inline certs
+// when it detects that there are no more inline certs and the old ACL role and policy are not in use then it exits
+func (c Cleaner) Run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(60 * time.Second):
+		}
+
+		client, err := consul.NewClientFromConnMgr(c.ConsulConfig, c.ServerMgr)
+		if err != nil {
+			c.Logger.Error(err, "failed to create Consul client")
+			continue
+		}
+
+		aclsCleanedUp, err := c.cleanupACLRoleAndPolicy(client)
+		if err != nil {
+			c.Logger.Error(err, "failed to cleanup old ACL role and policy")
+		}
+
+		inlineCertsAllCleanedUp, err := cleanupInlineCerts(client)
+		if err != nil {
+			c.Logger.Error(err, "failed to cleanup inlineCerts")
+		}
+
+		if aclsCleanedUp && inlineCertsAllCleanedUp {
+			return
+		}
+	}
+}
+
+// cleanupACLRoleAndPolicy deletes the old shared gateway ACL role and policy if they exist
+func (c Cleaner) cleanupACLRoleAndPolicy(client *api.Client) (bool, error) {
+	existingRules, _, err := client.ACL().BindingRuleList(c.AuthMethod, &api.QueryOptions{})
+	if err != nil {
+		if err.Error() == "Unexpected response code: 401 (ACL support disabled)" {
+			return true, nil
+		}
+		return false, fmt.Errorf("failed to list binding rules: %w", err)
+	}
+
+	for _, rule := range existingRules {
+		if rule.BindName == oldACLRoleName {
+			return false, nil
+		}
+	}
+
+	role, _, err := client.ACL().RoleReadByName(oldACLRoleName, &api.QueryOptions{})
+	if ignoreNotFoundError(err) != nil {
+		return false, fmt.Errorf("failed to get role: %w", err)
+	}
+
+	if role != nil {
+		_, err = client.ACL().RoleDelete(role.ID, &api.WriteOptions{})
+		if err != nil {
+			return false, fmt.Errorf("failed to delete role: %w", err)
+		}
+	}
+
+	policy, _, err := client.ACL().PolicyReadByName(oldACLPolicyName, &api.QueryOptions{})
+	if ignoreNotFoundError(err) != nil {
+		return false, fmt.Errorf("failed to get policy: %w", err)
+	}
+
+	if policy != nil {
+		_, err = client.ACL().PolicyDelete(policy.ID, &api.WriteOptions{})
+		if err != nil {
+			return false, fmt.Errorf("failed to delete policy: %w", err)
+		}
+	}
+
+	return true, nil
+}
+
+// cleanupInlineCerts deletes all inline certs that are not used by any gateway
+func cleanupInlineCerts(client *api.Client) (bool, error) {
+	certs, _, err := client.ConfigEntries().List(api.InlineCertificate, &api.QueryOptions{})
+	if err != nil {
+		return false, fmt.Errorf("failed to list the inline certs: %w", err)
+	}
+
+	gateways, _, err := client.ConfigEntries().List(api.APIGateway, &api.QueryOptions{})
+	if err != nil {
+		return false, fmt.Errorf("failed to list the gateways: %w", err)
+	}
+
+	if len(certs) == 0 {
+		return true, nil
+	}
+
+	certSet := mapset.NewSet[string]()
+	certsToKeep := mapset.NewSet[string]()
+	for _, cert := range certs {
+		certSet.Add(cert.GetName())
+	}
+
+	for _, gateway := range gateways {
+		gtw := gateway.(*api.APIGatewayConfigEntry)
+		for _, listener := range gtw.Listeners {
+			if len(listener.TLS.Certificates) == 0 {
+				continue
+			}
+
+			for _, cert := range listener.TLS.Certificates {
+				if cert.Kind == api.InlineCertificate && certSet.Contains(cert.Name) {
+					certsToKeep.Add(cert.Name)
+				}
+			}
+		}
+	}
+
+	certsToDelete := certSet.Difference(certsToKeep)
+	var mErr error
+	deletedCerts := 0
+	for cert := range certsToDelete.Iter() {
+		_, err := client.ConfigEntries().Delete(api.InlineCertificate, cert, &api.WriteOptions{})
+		if err != nil {
+			mErr = errors.Join(mErr, fmt.Errorf("failed to delete the inline cert %s: %w", cert, err))
+			continue
+		}
+		deletedCerts++
+	}
+
+	return certSet.Cardinality() == deletedCerts, mErr
+}
+
+func ignoreNotFoundError(err error) error {
+	if err == nil {
+		return nil
+	}
+	if strings.Contains(err.Error(), "Unexpected response code: 404") {
+		return nil
+	}
+
+	return err
+}
+
+func ignoreACLsDisabled(err error) error {
+	if err == nil {
+		return nil
+	}
+	if err.Error() == "Unexpected response code: 401 (ACL support disabled)" {
+		return nil
+	}
+	return err
+}
