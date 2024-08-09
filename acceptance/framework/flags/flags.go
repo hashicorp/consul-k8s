34a878eda95cf3b87dfc484751ce208a4f7af220
John Murret <john.murret@hashicorp.com>
2024-08-09 16:22:57 -0600

    test v1 dns

--- acceptance/framework/flags/flags.go
+++ acceptance/framework/flags/flags.go
@@ -226,7 +226,7 @@ func (t *TestFlags) TestConfigFromFlags() *config.TestConfig {
 		DisablePeering: t.flagDisablePeering,
 
 		HelmChartVersion:       t.flagHelmChartVersion,
-		ConsulImage:            t.flagConsulImage,
+		ConsulImage:            "jmurrethc/consul-ent-dev", // t.flagConsulImage,
 		ConsulK8SImage:         t.flagConsulK8sImage,
 		ConsulDataplaneImage:   t.flagConsulDataplaneImage,
 		ConsulVersion:          consulVersion,
