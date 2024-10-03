e7e743af960b95c92f9ff23355017c4e90e9c6f8
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2024-10-03 11:56:53 -0400

    Backport of [NET-10985] Fix bug where imagePullSecrets were not set up for Gateways into release/1.4.x (#4373)
    [NET-10985] Fix bug where imagePullSecrets were not set up for Gateways (#4316)
    
    * Plumb global.imagePullSecrets through to Gateway's ServiceAccount
    
    Since pull secrets are a list of structured objects that cannot easily be passed as a flag value to the container, this approach uses a JSON config file that is created as a ConfigMap and then mounted into the connect-injector Pod and parsed on startup.
    
    * Leave camp cleaner than I found it
    
    * Make path to config file configurable
    
    * Add changelog entry
    
    * Add note to changelog entry
    
    * Ensure ServiceAccount is created if any image pull secrets are provided
    
    * Add test coverage for image pull secret inclusion on gateway ServiceAccount
    
    * Adjust note in changelog
    
    * Add a helpful comment explaining when/why we create a ServiceAccount
    
    * Update .changelog/4316.txt
    
    
    
    * Return ServiceAccount name when image pull secrets warrant it
    
    * Improve unit tests to assert presence of ServiceAccount name on Deployment
    
    * Copy helpful comment added elsewhere
    
    ---------
    
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>
    Co-authored-by: Blake Covarrubias <blake@covarrubi.as>

--- control-plane/api-gateway/gatekeeper/init.go
+++ control-plane/api-gateway/gatekeeper/init.go
@@ -36,7 +36,7 @@ type initContainerCommandData struct {
 	LogJSON  bool
 }
 
-// containerInit returns the init container spec for connect-init that polls for the service and the connect proxy service to be registered
+// initContainer returns the init container spec for connect-init that polls for the service and the connect proxy service to be registered
 // so that it can save the proxy service id to the shared volume and boostrap Envoy with the proxy-id.
 func (g Gatekeeper) initContainer(config common.HelmConfig, name, namespace string) (corev1.Container, error) {
 	data := initContainerCommandData{
