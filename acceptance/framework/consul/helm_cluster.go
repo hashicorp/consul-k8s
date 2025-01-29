b7efa1646d7e180407c5227e09834ee5aa236ecf
sarahalsmiller <100602640+sarahalsmiller@users.noreply.github.com>
2025-01-29 16:18:19 -0600

    Update acceptance/framework/consul/helm_cluster.go

--- acceptance/framework/consul/helm_cluster.go
+++ acceptance/framework/consul/helm_cluster.go
@@ -156,7 +156,7 @@ func (h *HelmCluster) Create(t *testing.T) {
 		chartName = h.ChartPath
 	}
 	// Retry the install in case previous tests have not finished cleaning up.
-	retry.RunWith(&retry.Counter{Wait: 2 * time.Second, Count: 1}, t, func(r *retry.R) {
+	retry.RunWith(&retry.Counter{Wait: 2 * time.Second, Count: 30}, t, func(r *retry.R) {
 		err := helm.InstallE(r, h.helmOptions, chartName, h.releaseName)
 		require.NoError(r, err)
 	})
