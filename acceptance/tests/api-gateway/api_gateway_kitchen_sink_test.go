88044fe9fe1243bb309430a306bef0609e68f324
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2025-03-04 17:07:36 -0600

    fix targetAddress

--- acceptance/tests/api-gateway/api_gateway_kitchen_sink_test.go
+++ acceptance/tests/api-gateway/api_gateway_kitchen_sink_test.go
@@ -187,7 +187,7 @@ func TestAPIGateway_KitchenSink(t *testing.T) {
 
 	// finally we check that we can actually route to the service(s) via the gateway
 	k8sOptions := ctx.KubectlOptions(t)
-	targetHTTPAddress := fmt.Sprintf("http://%s/v1", gatewayAddress)
+	targetHTTPAddress := fmt.Sprintf("http://%s:8080/v1", gatewayAddress)
 
 	// Now we create the allow intention.
 	_, _, err = consulClient.ConfigEntries().Set(&api.ServiceIntentionsConfigEntry{
