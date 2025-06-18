ffdbeea722b399e807835cabe2c514626272aa5c
P Ajay Rao <pajay.rao@hashicorp.com>
2025-06-18 09:53:19 +0530

    Updating gke cluster cidr

--- charts/consul/test/terraform/gke/main.tf
+++ charts/consul/test/terraform/gke/main.tf
@@ -47,7 +47,7 @@ resource "google_container_cluster" "cluster" {
     machine_type = "e2-standard-8"
   }
   subnetwork          = data.google_compute_subnetwork.subnet.name
-  cluster_ipv4_cidr   = "10.0.0.0/6"
+  cluster_ipv4_cidr   = "8.0.0.0/8"
   resource_labels     = var.labels
   deletion_protection = false
 }
