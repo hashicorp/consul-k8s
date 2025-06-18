7c8eed2d0cd552a586190bf4b44132b81eb5764b
P Ajay Rao <pajay.rao@hashicorp.com>
2025-06-18 11:30:40 +0530

    Addition of new random variable to all resources for gke cluster creation

--- charts/consul/test/terraform/gke/main.tf
+++ charts/consul/test/terraform/gke/main.tf
@@ -19,44 +19,50 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
+resource "random_string" "cluster_prefix" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
 data "google_container_engine_versions" "main" {
   location       = var.zone
   version_prefix = var.kubernetes_version_prefix
 
 }
 
-# We assume that the subnets are already created to save time.
-data "google_compute_subnetwork" "subnet" {
-  name = var.subnet
+resource "google_compute_subnetwork" "subnet" {
+  count          = var.cluster_count
+  name           = "${random_string.cluster_prefix.result}-subnet-${count.index}"
+  ip_cidr_range  = cidrsubnet("10.0.0.0/8", 8, count.index)
+  network        = google_compute_network.custom_network.name
 }
 
 resource "google_container_cluster" "cluster" {
   provider = "google"
   count    = var.cluster_count
 
-  name               = "consul-k8s-${random_id.suffix[count.index].dec}"
+  name               = "consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"
   project            = var.project
   initial_node_count = 3
   location           = var.zone
-  # 2023-10-30 - There is a bug with the terraform provider where lastest_master_version is not being returned by the
-  # api. Hardcode GKE version for now.
   min_master_version = data.google_container_engine_versions.main.latest_master_version
   node_version       = data.google_container_engine_versions.main.latest_master_version
   node_config {
-    tags         = ["consul-k8s-${random_id.suffix[count.index].dec}"]
+    tags         = ["consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"]
     machine_type = "e2-standard-8"
   }
-  subnetwork          = data.google_compute_subnetwork.subnet.name
-  cluster_ipv4_cidr   = "10.0.0.0/10"
+  subnetwork          = google_compute_subnetwork.subnet[count.index].name
+  cluster_ipv4_cidr   = cidrsubnet("10.0.0.0/8", 8, count.index)
   resource_labels     = var.labels
   deletion_protection = false
 }
 
 resource "google_compute_firewall" "firewall-rules" {
   project     = var.project
-  name        = "consul-k8s-acceptance-firewall-${random_id.suffix[count.index].dec}"
+  name        = "${random_string.cluster_prefix.result}-firewall-${random_id.suffix[count.index].dec}"
   network     = "default"
-  description = "Creates firewall rule allowing traffic from nodes and pods of the ${random_id.suffix[count.index == 0 ? 1 : 0].dec} Kubernetes cluster."
+  description = "Firewall rule for cluster ${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}."
 
   count = var.cluster_count > 1 ? var.cluster_count : 0
 
@@ -65,8 +71,8 @@ resource "google_compute_firewall" "firewall-rules" {
   }
 
   source_ranges = [google_container_cluster.cluster[count.index == 0 ? 1 : 0].cluster_ipv4_cidr]
-  source_tags   = ["consul-k8s-${random_id.suffix[count.index == 0 ? 1 : 0].dec}"]
-  target_tags   = ["consul-k8s-${random_id.suffix[count.index].dec}"]
+  source_tags   = ["${random_string.cluster_prefix.result}-${random_id.suffix[count.index == 0 ? 1 : 0].dec}"]
+  target_tags   = ["${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"]
 }
 
 resource "null_resource" "kubectl" {
