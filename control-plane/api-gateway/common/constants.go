940441371492ef5df99e43980e85713b368fffb2
kruti <kruti.baraiya@hashicorp.com>
2025-08-28 17:16:13 +0530

    Dummy commit to test flaky tests

--- control-plane/api-gateway/common/constants.go
+++ control-plane/api-gateway/common/constants.go
@@ -8,7 +8,7 @@ const (
 
 	AnnotationGatewayClassConfig = "consul.hashicorp.com/gateway-class-config"
 
-	// The following annotation keys are used in the v1beta1.GatewayTLSConfig's Options on a v1beta1.Listener.
+	// These annotation keys are used in the v1beta1.GatewayTLSConfig's Options on a v1beta1.Listener.
 	TLSCipherSuitesAnnotationKey = "api-gateway.consul.hashicorp.com/tls_cipher_suites"
 	TLSMaxVersionAnnotationKey   = "api-gateway.consul.hashicorp.com/tls_max_version"
 	TLSMinVersionAnnotationKey   = "api-gateway.consul.hashicorp.com/tls_min_version"
