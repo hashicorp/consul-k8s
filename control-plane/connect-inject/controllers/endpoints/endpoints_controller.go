0a6cb63437e851d9bb8c3b6196af14018e0e263d
Sai Shashank Nandigama <saishashank.nandigama@hashicorp.com>
2025-10-07 11:24:53 +0530

    cleanup comments and update changelog

--- control-plane/connect-inject/controllers/endpoints/endpoints_controller.go
+++ control-plane/connect-inject/controllers/endpoints/endpoints_controller.go
@@ -216,34 +216,6 @@ func (r *Controller) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 				}
 
 				// Skip registration if node information is incomplete to prevent duplicate registrations.
-				// Retry for 5 seconds because the pod IP may not be immediately available after pod creation.
-				// retries := 5
-				// for range retries {
-				// 	if pod.Spec.NodeName == "" || pod.Status.PodIP == "" || pod.Status.HostIP == "" {
-				// 		r.Log.Info("waiting for pod to have complete node information before registering",
-				// 			"pod", pod.Name, "namespace", pod.Namespace,
-				// 			"nodeName", pod.Spec.NodeName, "podIP", pod.Status.PodIP, "hostIP", pod.Status.HostIP)
-				// 		time.Sleep(1 * time.Second)
-				// 		if err = r.Client.Get(ctx, objectKey, &pod); err != nil {
-				// 			// If the pod doesn't exist anymore, set up the deregisterEndpointAddress map to deregister it.
-				// 			if k8serrors.IsNotFound(err) {
-				// 				deregisterEndpointAddress[address.IP] = true
-				// 				r.Log.Info("pod not found", "name", address.TargetRef.Name)
-				// 			} else {
-				// 				// If there was a different error fetching the pod, then log the error but don't deregister it
-				// 				// since this could be a K8s API blip and we don't want to prematurely deregister.
-				// 				deregisterEndpointAddress[address.IP] = false
-				// 				r.Log.Error(err, "failed to get pod", "name", address.TargetRef.Name)
-				// 				errs = multierror.Append(errs, err)
-				// 			}
-				// 			break
-				// 		}
-				// 	} else {
-				// 		break
-				// 	}
-				// }
-				// If after retries the node information is still incomplete, skip registration.
-				// This could happen if the pod is stuck in Pending state for a long time.
 				if pod.Spec.NodeName == "" || address.IP == "" || pod.Status.HostIP == "" {
 					r.Log.Info("skipping pod registration due to incomplete node information",
 						"pod", pod.Name, "namespace", pod.Namespace,
