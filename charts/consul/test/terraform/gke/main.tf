0d3c51ab686741cdde95c34dbe500fe08d629f02
shani34 <shani.kumar@hashicorp.com>
2025-10-23 17:52:24 +0530

    used shared network to handled the network exhaustion issue

--- charts/consul/test/terraform/gke/main.tf
+++ charts/consul/test/terraform/gke/main.tf
@@ -14,6 +14,9 @@ provider "google" {
   zone    = var.zone
 }
 
+# -----------------------------
+# Random Identifiers
+# -----------------------------
 resource "random_id" "suffix" {
   count       = var.cluster_count
   byte_length = 4
@@ -25,56 +28,79 @@ resource "random_string" "cluster_prefix" {
   special = false
 }
 
+# -----------------------------
+# Get Available GKE Versions
+# -----------------------------
 data "google_container_engine_versions" "main" {
   location       = var.zone
   version_prefix = var.kubernetes_version_prefix
-
 }
 
+# -----------------------------
+# Network and Subnet Logic
+# -----------------------------
+# Create a network only if create_network = true
 resource "google_compute_network" "custom_network" {
+  count                   = var.create_network ? 1 : 0
   name                    = "network-${random_string.cluster_prefix.result}"
   auto_create_subnetworks = false
+
   lifecycle {
-  prevent_destroy = true
-  ignore_changes  = [name]
-}
+    prevent_destroy = false
+    ignore_changes  = [name]
+  }
 }
 
+# Determine which network name to use (created or existing)
+locals {
+  active_network_name = var.create_network ? google_compute_network.custom_network[0].name : var.shared_network_name
+}
+
+# Create subnets only when new network is created
 resource "google_compute_subnetwork" "subnet" {
-  count         = var.cluster_count
-  name          = "subnet-${random_string.cluster_prefix.result}-${count.index}" // Ensure valid name
-  ip_cidr_range = cidrsubnet("10.0.0.0/8", 8, count.index)
-  network       = google_compute_network.custom_network.name
+  count         = var.create_network ? var.cluster_count : 0
+  name          = "subnet-${random_string.cluster_prefix.result}-${count.index}"
+  ip_cidr_range = cidrsubnet(var.shared_network_cidr, 8, count.index)
+  region        = var.subnet_region
+  network       = local.active_network_name
 }
 
+# -----------------------------
+# GKE Cluster(s)
+# -----------------------------
 resource "google_container_cluster" "cluster" {
-  provider = google
   count    = var.cluster_count
+  provider = google
 
   name               = "consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"
   project            = var.project
-  initial_node_count = 3
   location           = var.zone
+  initial_node_count = 3
+
   min_master_version = data.google_container_engine_versions.main.latest_master_version
   node_version       = data.google_container_engine_versions.main.latest_master_version
-  network            = google_compute_network.custom_network.name
+
+  network    = local.active_network_name
+  subnetwork = var.create_network ? google_compute_subnetwork.subnet[count.index].self_link : var.subnet
+
   node_config {
-    tags         = ["consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"]
     machine_type = "e2-standard-8"
+    tags         = ["consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"]
   }
-  subnetwork          = google_compute_subnetwork.subnet[count.index].self_link
+
   resource_labels     = var.labels
   deletion_protection = false
 }
 
-
-resource "google_compute_firewall" "firewall-rules" {
+# -----------------------------
+# Firewall Rules (only when new network created)
+# -----------------------------
+resource "google_compute_firewall" "firewall_rules" {
+  count       = var.create_network && var.cluster_count > 1 ? var.cluster_count : 0
   project     = var.project
   name        = format("firewall-%s-%d", substr(random_string.cluster_prefix.result, 0, 8), count.index)
-  network     = google_compute_network.custom_network.name
+  network     = local.active_network_name
   description = "Firewall rule for cluster ${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}."
-
-  count = var.cluster_count > 1 ? var.cluster_count : 0
 
   allow {
     protocol = "all"
@@ -85,6 +111,9 @@ resource "google_compute_firewall" "firewall-rules" {
   target_tags   = ["cluster-${random_string.cluster_prefix.result}-${count.index}"]
 }
 
+# -----------------------------
+# Optional kubectl Initialization
+# -----------------------------
 resource "null_resource" "kubectl" {
   count = var.init_cli ? var.cluster_count : 0
 
@@ -92,16 +121,10 @@ resource "null_resource" "kubectl" {
     cluster = google_container_cluster.cluster[count.index].id
   }
 
-  # On creation, we want to setup the kubectl credentials. The easiest way
-  # to do this is to shell out to gcloud.
   provisioner "local-exec" {
     command = "KUBECONFIG=$HOME/.kube/${google_container_cluster.cluster[count.index].name} gcloud container clusters get-credentials --zone=${var.zone} ${google_container_cluster.cluster[count.index].name}"
   }
 
-  # On destroy we want to try to clean up the kubectl credentials. This
-  # might fail if the credentials are already cleaned up or something so we
-  # want this to continue on failure. Generally, this works just fine since
-  # it only operates on local data.
   provisioner "local-exec" {
     when       = destroy
     on_failure = continue
--- charts/consul/test/terraform/gke/variables.tf
+++ charts/consul/test/terraform/gke/variables.tf
@@ -23,9 +23,6 @@ variable "cluster_count" {
   default     = 1
   description = "The number of Kubernetes clusters to create."
 
-  // We currently cannot support more than 2 cluster
-  // because setting up peering is more complicated if cluster count is
-  // more than two.
   validation {
     condition     = var.cluster_count < 3 && var.cluster_count > 0
     error_message = "The cluster_count value must be 1 or 2."
@@ -47,4 +44,32 @@ variable "subnet" {
 variable "kubernetes_version_prefix" {
   default     = "1.32."
   description = "Kubernetes version supported on EKS"
+}
+
+#########################################
+# Newly Added Variables for Optimized Network Usage
+#########################################
+
+variable "create_network" {
+  type        = bool
+  default     = false
+  description = "Whether to create a new shared network if not existing. Set to true only for the first run."
+}
+
+variable "shared_network_name" {
+  type        = string
+  default     = ""
+  description = "Name of an existing shared network to reuse. If set, Terraform will not create a new network."
+}
+
+variable "shared_network_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "Base CIDR range for creating subnets for each cluster when using a shared network."
+}
+
+variable "subnet_region" {
+  type        = string
+  default     = "us-central1"
+  description = "Region where subnets will be created inside the shared network."
 }
