hashicorp/consul-k8s

commit a189f6e3bc231f641923180fd6684b037bcdd8c9

Enabling dual-stack testing (#4895)
* Enabling dual-stack testing

* Reverting branch to main

.github/workflows/pr.yml
@@ -30,7 +30,7 @@ jobs:
         repo: hashicorp/consul-k8s-workflows
         ref: main
         token: ${{ secrets.ELEVATED_GITHUB_TOKEN }}
-        inputs: '{ "context":"${{ env.CONTEXT }}-${{ github.event.pull_request.number }}", "actor":"${{ github.actor }}", "repository":"${{ github.repository }}", "branch":"${{ env.BRANCH }}", "sha":"${{ env.SHA }}", "token":"${{ secrets.ELEVATED_GITHUB_TOKEN }}" }'
+        inputs: '{ "context":"${{ env.CONTEXT }}-${{ github.event.pull_request.number }}", "actor":"${{ github.actor }}", "repository":"${{ github.repository }}", "branch":"${{ env.BRANCH }}", "sha":"${{ env.SHA }}", "token":"${{ secrets.ELEVATED_GITHUB_TOKEN }}", "json_params":"{\"gotestsum-version\":\"1.12.3\", \"terraform-version\":\"latest\", \"test-ce\":true, \"test-type\":\"short\", \"dual-stack\":true}" }'
 
   pass-required-checks-on-skip:
     needs: [ conditional-skip ]

commit 527bd67d280c17a6f3f69c66c20baf55324c9ad9

Add IPv6 dual-stack support to EKS terraform configuration
- Enable IPv6 on VPC to support dual-stack networking tests
- Configure IPv6 prefixes for public and private subnets
- Fixes acceptance-dual-stack test failures introduced in PR #4895
- Required for dual-stack testing enabled on Oct 22, 2025

charts/consul/test/terraform/eks/main.tf
@@ -42,13 +42,18 @@ module "vpc" {
 
   name = "consul-k8s-${random_id.suffix[count.index].dec}"
   # The cidr range needs to be unique in each VPC to allow setting up a peering connection.
-  cidr                 = format("10.%s.0.0/16", count.index)
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = [format("10.%s.1.0/24", count.index), format("10.%s.2.0/24", count.index), format("10.%s.3.0/24", count.index)]
-  public_subnets       = [format("10.%s.4.0/24", count.index), format("10.%s.5.0/24", count.index), format("10.%s.6.0/24", count.index)]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  cidr               = format("10.%s.0.0/16", count.index)
+  azs                = data.aws_availability_zones.available.names
+  private_subnets    = [format("10.%s.1.0/24", count.index), format("10.%s.2.0/24", count.index), format("10.%s.3.0/24", count.index)]
+  public_subnets     = [format("10.%s.4.0/24", count.index), format("10.%s.5.0/24", count.index), format("10.%s.6.0/24", count.index)]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # Enable dual-stack (IPv4 + IPv6) support for acceptance tests
+  enable_ipv6                  = true
+  public_subnet_ipv6_prefixes  = [0, 1, 2]
+  private_subnet_ipv6_prefixes = [3, 4, 5]
+  enable_dns_hostnames         = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/consul-k8s-${random_id.suffix[count.index].dec}" = "shared"