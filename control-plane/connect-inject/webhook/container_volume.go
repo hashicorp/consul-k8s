b0f884c84214907f71f692543b7a078ceb9b3fd8
anilvpatel <anilkumar.vinodbhai.patel@hashicorp.com>
2025-11-23 21:27:48 +0530

    volume mount for file type access log in envoy sidecar

--- control-plane/connect-inject/webhook/container_volume.go
+++ control-plane/connect-inject/webhook/container_volume.go
@@ -4,12 +4,15 @@
 package webhook
 
 import (
+	"path/filepath"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
 // volumeName is the name of the volume that is created to store the
 // Consul Connect injection data.
 const volumeName = "consul-connect-inject-data"
+const accessLogVolumeName = "envoy-access-logs"
 
 // containerVolume returns the volume data to add to the pod. This volume
 // is used for shared data between containers.
@@ -19,5 +22,21 @@ func (w *MeshWebhook) containerVolume() corev1.Volume {
 		VolumeSource: corev1.VolumeSource{
 			EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory},
 		},
+	}
+}
+
+func accessLogVolume() corev1.Volume {
+	return corev1.Volume{
+		Name: accessLogVolumeName,
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumDefault},
+		},
+	}
+}
+
+func accessLogVolumeMount(path string) corev1.VolumeMount {
+	return corev1.VolumeMount{
+		Name:      accessLogVolumeName,
+		MountPath: filepath.Dir(path),
 	}
 }
