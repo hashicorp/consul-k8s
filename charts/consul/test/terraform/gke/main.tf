f53428d67a8cdc775398ad4dfbfc0972be845624
shani34 <shani.kumar@hashicorp.com>
2025-12-05 23:24:18 +0530

    update cluster creation

--- charts/consul/test/terraform/gke/main.tf
+++ charts/consul/test/terraform/gke/main.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     google = {
@@ -19,74 +16,63 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-data "google_container_engine_versions" "main" {
-  location       = var.zone
-  version_prefix = "1.27."
+resource "random_string" "cluster_prefix" {
+  length  = 8
+  upper   = false
+  special = false
 }
 
-# We assume that the subnets are already created to save time.
-data "google_compute_subnetwork" "subnet" {
-  name = var.subnet
+
+resource "google_compute_network" "custom_network" {
+  name                    = "network-${random_string.cluster_prefix.result}"
+  auto_create_subnetworks = false
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = [name]
+  }
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  count         = var.cluster_count
+  name          = "subnet-${random_string.cluster_prefix.result}-${count.index}"
+  ip_cidr_range = cidrsubnet("10.0.0.0/8", 8, count.index)
+  network       = google_compute_network.custom_network.name
 }
 
 resource "google_container_cluster" "cluster" {
-  provider = "google"
+  provider = google
   count    = var.cluster_count
 
-  name               = "consul-k8s-${random_id.suffix[count.index].dec}"
+  name               = "consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"
   project            = var.project
   initial_node_count = 3
   location           = var.zone
-  # 2023-10-30 - There is a bug with the terraform provider where lastest_master_version is not being returned by the
-  # api. Hardcode GKE version for now.
-  min_master_version = data.google_container_engine_versions.main.latest_master_version
-  node_version       = data.google_container_engine_versions.main.latest_master_version
+
+  network    = google_compute_network.custom_network.name
+  subnetwork = google_compute_subnetwork.subnet[count.index].self_link
+
   node_config {
-    tags         = ["consul-k8s-${random_id.suffix[count.index].dec}"]
+    tags         = ["consul-k8s-${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}"]
     machine_type = "e2-standard-8"
   }
-  subnetwork          = data.google_compute_subnetwork.subnet.name
+
   resource_labels     = var.labels
   deletion_protection = false
 }
 
 resource "google_compute_firewall" "firewall-rules" {
+  count       = var.cluster_count > 1 ? var.cluster_count : 0
   project     = var.project
-  name        = "consul-k8s-acceptance-firewall-${random_id.suffix[count.index].dec}"
-  network     = "default"
-  description = "Creates firewall rule allowing traffic from nodes and pods of the ${random_id.suffix[count.index == 0 ? 1 : 0].dec} Kubernetes cluster."
-
-  count = var.cluster_count > 1 ? var.cluster_count : 0
+  name        = format("firewall-%s-%d", substr(random_string.cluster_prefix.result, 0, 8), count.index)
+  network     = google_compute_network.custom_network.name
+  description = "Firewall rule for cluster ${random_string.cluster_prefix.result}-${random_id.suffix[count.index].dec}."
 
   allow {
     protocol = "all"
   }
 
-  source_ranges = [google_container_cluster.cluster[count.index == 0 ? 1 : 0].cluster_ipv4_cidr]
-  source_tags   = ["consul-k8s-${random_id.suffix[count.index == 0 ? 1 : 0].dec}"]
-  target_tags   = ["consul-k8s-${random_id.suffix[count.index].dec}"]
-}
-
-resource "null_resource" "kubectl" {
-  count = var.init_cli ? var.cluster_count : 0
-
-  triggers = {
-    cluster = google_container_cluster.cluster[count.index].id
-  }
-
-  # On creation, we want to setup the kubectl credentials. The easiest way
-  # to do this is to shell out to gcloud.
-  provisioner "local-exec" {
-    command = "KUBECONFIG=$HOME/.kube/${google_container_cluster.cluster[count.index].name} gcloud container clusters get-credentials --zone=${var.zone} ${google_container_cluster.cluster[count.index].name}"
-  }
-
-  # On destroy we want to try to clean up the kubectl credentials. This
-  # might fail if the credentials are already cleaned up or something so we
-  # want this to continue on failure. Generally, this works just fine since
-  # it only operates on local data.
-  provisioner "local-exec" {
-    when       = destroy
-    on_failure = continue
-    command    = "rm $HOME/.kube/consul-k8s*"
-  }
+  source_ranges = [
+    google_container_cluster.cluster[count.index == 0 ? 1 : 0].cluster_ipv4_cidr
+  ]
 }
