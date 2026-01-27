ae504968b1d2491e39f15bf68f8aa6d7b7a918dc
anilvpatel <anilkumar.vinodbhai.patel@hashicorp.com>
2026-01-27 17:04:32 +0530

    checking login

--- acceptance/tests/openshift/basic_openshift_test.go
+++ acceptance/tests/openshift/basic_openshift_test.go
@@ -28,7 +28,6 @@ import (
 func TestOpenshift_Basic(t *testing.T) {
 	cfg := suite.Config()
 
-	//its install helm chart
 	logger.Log(t, "use openshift", cfg.UseOpenshift)
 	newOpenshiftCluster(t, cfg, true, false)
 
