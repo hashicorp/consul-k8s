d7dcac54109b79139c83c00cc8c7e2d7b8b6088b
vdinesh4738 <dinesh.vadlamudi@hashicorp.com>
2026-02-10 12:57:53 +0530

    Backport of CSL-13047: fix for segmentation fault or SIGSEGV with consul-cni network plugin's code on 1.9.1 into release/1.8.x (#5073)
    * no-op commit due to failed cherry-picking
    
    * CSL-13047: fix for segmentation fault or SIGSEGV with consul-cni network plugin's code on 1.9.1  (#5062)
    
    * looks for kubeconfig created for cni plugin
    
    * added tcs
    
    * added leader Renewal params
    
    * added changelog
    
    * added timeout for leader renewal--mainly on ocp
    
    * renamed file name
    
    * changelog file name
    
    * updated code to reduce complexity to O(n)
    
    ---------
    
    Co-authored-by: temp <temp@hashicorp.com>
    Co-authored-by: Bharath <kallamadibharath.reddy@hashicorp.com>

--- acceptance/tests/consul-dns/consul_dns_partitions_test.go
+++ acceptance/tests/consul-dns/consul_dns_partitions_test.go
@@ -64,22 +64,22 @@ func TestConsulDNSProxy_WithPartitionsAndCatalogSync(t *testing.T) {
 
 	cases := []dnsWithPartitionsTestCase{
 		{
-			name:   "not secure - ACLs and auto-encrypt not enabled",
+			name:   "not secure - ACLs and auto-encrypt not enabled privileged port",
 			secure: false,
 			port:   privilegedPort,
 		},
 		{
-			name:   "secure - ACLs and auto-encrypt enabled",
+			name:   "secure - ACLs and auto-encrypt enabled privileged port",
 			secure: true,
 			port:   privilegedPort,
 		},
 		{
-			name:   "not secure - ACLs and auto-encrypt not enabled",
+			name:   "not secure - ACLs and auto-encrypt not enabled non-privileged port",
 			secure: false,
 			port:   nonPrivilegedPort,
 		},
 		{
-			name:   "secure - ACLs and auto-encrypt enabled",
+			name:   "secure - ACLs and auto-encrypt enabled non-privileged port",
 			secure: true,
 			port:   nonPrivilegedPort,
 		},
@@ -125,7 +125,7 @@ func TestConsulDNSProxy_WithPartitionsAndCatalogSync(t *testing.T) {
 					if v.preProcessingFunc != nil {
 						v.preProcessingFunc(t)
 					}
-					verifyDNS(t, releaseName, staticServerNamespace, v.requestingCtx, v.svcContext,
+					verifyDNS(t, cfg, releaseName, staticServerNamespace, v.requestingCtx, v.svcContext,
 						podLabelSelector, v.svcName, v.shouldResolveDNS, dnsUtilsPodIndex)
 					dnsUtilsPodIndex++
 				})
