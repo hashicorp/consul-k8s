1aa72f66f99a6e77cb0c94cf5e48fa82b35459d4
Bharath <kallamadibharath.reddy@hashicorp.com>
2026-05-13 15:56:16 +0530

    [Deprecate Ingress Gateway]sds support for api-gateway (#5186)
    * initial code chages for sds support to api gateway
    
    * initial code chages for sds support to api gateway
    
    * initial commit
    
    * Add tls.sds at default, listner and service levels to allow overrides
    
    * go.mod update
    
    * update api submodule
    
    * add changelog
    
    * updating go mod
    
    * fixing build failures
    
    * refactor: replace gwv1beta1 with gwv1 in gateway API references
    
    * refactor: update comment to clarify gwv1beta1 replacement with gwv1
    
    * refactor: improve error handling and comments in gateway controller integration tests
    
    * refactor: enhance SDS configuration resolution logic and add comprehensive tests
    
    * refactor: enhance ReferenceGrant handling by adding support for v1beta1 and improving error management
    
    * refactor: simplify conditional logic in tls_sds.go for listener key validation
    
    * refactor: improve HTTPRoute inheritance logic from listeners and add unit tests

--- control-plane/api-gateway/common/constants.go
+++ control-plane/api-gateway/common/constants.go
@@ -9,7 +9,9 @@ const (
 	AnnotationGatewayClassConfig = "consul.hashicorp.com/gateway-class-config"
 
 	// The following annotation keys are used in the v1beta1.GatewayTLSConfig's Options on a v1beta1.Listener.
-	TLSCipherSuitesAnnotationKey = "api-gateway.consul.hashicorp.com/tls_cipher_suites"
-	TLSMaxVersionAnnotationKey   = "api-gateway.consul.hashicorp.com/tls_max_version"
-	TLSMinVersionAnnotationKey   = "api-gateway.consul.hashicorp.com/tls_min_version"
+	TLSCipherSuitesAnnotationKey    = "api-gateway.consul.hashicorp.com/tls_cipher_suites"
+	TLSMaxVersionAnnotationKey      = "api-gateway.consul.hashicorp.com/tls_max_version"
+	TLSMinVersionAnnotationKey      = "api-gateway.consul.hashicorp.com/tls_min_version"
+	TLSSDSClusterNameAnnotationKey  = "api-gateway.consul.hashicorp.com/tls_sds_cluster_name"
+	TLSSDSCertResourceAnnotationKey = "api-gateway.consul.hashicorp.com/tls_sds_cert_resource"
 )
